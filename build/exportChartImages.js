1139fbb8f49699b379cc6f74a5606636e0e86446
Jonas Bieri <jonas.bieri@gmail.com>
2017-01-13 12:55:25 +0000

    Try out saving pngs

--- build/exportChartImages.js
+++ build/exportChartImages.js
@@ -1,7 +1,7 @@
 //Include the exporter module
 const exporter = require('highcharts-export-server');
 var path = require('path');
-var chartId=4127;
+var chartId=4716;
 var configPath = 'charts/configs/portal/';
 var infilePath = path.join(__dirname, '../' + configPath + chartId + '.json');
 console.log(infilePath);
@@ -9,6 +9,8 @@ console.log(infilePath);
 var imagePath = 'images/portal/';
 var outfilePath = path.join(__dirname, '../' + imagePath + chartId + '.svg');
 console.log(outfilePath);
+
+var fs = require('fs');
 
 //Export settings 
 var exportSettings = {
@@ -31,6 +33,7 @@ exporter.export(exportSettings, function (err, res) {
     //If the output is not PDF or SVG, it will be base64 encoded (res.data).
     //If the output is a PDF or SVG, it will contain a filename (res.filename).
     console.log(res);
+    //fs.writeFileSync(outfilePath, res.data, 'base64');
     //Kill the pool when we're done with it, and exit the application
     exporter.killPool();
     process.exit(1);
