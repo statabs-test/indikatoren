39e56ebeae901229755fa93894b6ddda5ec5f003
larafaelivrin <lara.faelivrin@gmail.com>
2017-03-22 12:18:44 +0000

    legislaturplan

--- charts/templates/5064.js
+++ charts/templates/5064.js
@@ -19,7 +19,7 @@
     }
   ],
   "tooltip": {
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.0f}</b><br/>',
     "shared": false
   },
     "plotOptions": {
