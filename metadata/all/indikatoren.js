9df62a231df8973a860c2b69abcbc104513214b5
larafaelivrin <lara.faelivrin@gmail.com>
2017-07-19 10:43:54 +0000

    npm run reinstall

--- metadata/all/indikatoren.js
+++ metadata/all/indikatoren.js
@@ -6106,44 +6106,6 @@ var indikatoren = [
 		]
 	},
 	{
-		"title": "Anteil Fläche nach Art der Bodenbedeckung",
-		"subtitle": "",
-		"kuerzel": "I.02.1.05807",
-		"kuerzelKunde": "ST 2-1",
-		"description": "Anteil Fläche nach Art der Bodenbedeckung",
-		"id": 9998,
-		"thema": "02 Raum, Umwelt",
-		"unterthema": "Räumliche Gliederung und Bodennutzung",
-		"kennzahlenset": "Test",
-		"raeumlicheGliederung": [
-			"Kanton"
-		],
-		"aktualisierungsdatum": "",
-		"schlagwort": [
-			"Boden"
-		],
-		"stufe1": "Strategie 2",
-		"stufe2": "",
-		"stufe3": "",
-		"stufe4": "",
-		"stufe5": "",
-		"lesehilfe": "",
-		"erlaeuterungen": "",
-		"template": "pie001",
-		"option": "options001",
-		"visible": true,
-		"visibleInPortal": false,
-		"parentId": null,
-		"children": [
-			"5869"
-		],
-		"renderLink": [],
-		"quellenangabe": [],
-		"externalLinks": [
-			""
-		]
-	},
-	{
 		"title": "Personenwagen pro Haushalt",
 		"subtitle": "Stand: 2015",
 		"kuerzel": "I.11.1.9999",
