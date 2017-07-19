8f2488946d5897690cdc1de66b75b319e8fad6d8
Jonas Bieri <jonas.bieri@gmail.com>
2017-07-19 10:07:57 +0000

    Hide slices of all series when legend item is clicked

--- charts/templates/9998.js
+++ charts/templates/9998.js
@@ -1,3 +1,7 @@
+/* 
+    global $
+*/
+
 (function(){
     return {
       "data":{
@@ -6,12 +10,12 @@
       "series": [ 
           {
               innerSize: '20%',
-              id: 0,
-              showInLegend: false
+              id: 0
           }, 
           {
               innerSize: '60%',
-              linkedTo: ':previous'
+              linkedTo: ':previous',
+              showInLegend: false
           }
       ],
     	"tooltip": {
@@ -32,6 +36,77 @@
                '#6AF9C4'
              ]
 	      }
-      }
+      },
+     chart: {		
+        events:{
+              load: function() {
+                  this.credits.element.onclick = function() {};
+        
+                // Remove all pie slices with same color when clicking onto legend item
+                // see https://www.bountysource.com/issues/1055141-linkedto-on-pie-chart-s-data-points-for-e-g-setvisible
+                // or http://jsfiddle.net/highcharts/u7FQS/278/
+                  
+                var chart = this;
+                
+                $(chart.series[0].data).each(function(i, e) {
+                    e.legendItem.on('click', function(event) {
+                        var legendItem=e.name;
+                        
+                        event.stopPropagation();
+                        
+                        $(chart.series).each(function(j,f){
+                               $(this.data).each(function(k,z){
+                                   if(z.name==legendItem)
+                                   {
+                                       if(z.visible)
+                                       {
+                                           z.setVisible(false);
+                                       }
+                                       else
+                                       {
+                                           z.setVisible(true);
+                                       }
+                                   }
+                               });
+                        });
+                    });
+                    e.legendItem.on('mouseover', function(event) {
+                        var legendItem=e.name;
+                        
+                        event.stopPropagation();
+                        
+                        $(chart.series).each(function(j,f){
+                               $(this.data).each(function(k,z){
+                                   if(z.name==legendItem)
+                                   {
+                                       z.setState('hover');
+                                   }
+                               });
+                        });
+                    });
+                    e.legendItem.on('mouseout', function(event) {
+                        var legendItem=e.name;
+                        
+                        event.stopPropagation();
+                        
+                        $(chart.series).each(function(j,f){
+                               $(this.data).each(function(k,z){
+                                   if(z.name==legendItem)
+                                   {
+                                       z.setState('');
+                                   }
+                               });
+                        });
+                    });
+                    
+                });
+        
+                
+
+                  
+                  
+              }
+          }
+    }
     };
 }());
