b7829df9a83d4fd29c44bd230cc22fdd602d2c0b
Jonas Bieri <jonas.bieri@gmail.com>
2017-07-27 10:23:55 +0000

    Change fontWeight of dataLabels

--- charts/templates/5869.js
+++ charts/templates/5869.js
@@ -3,12 +3,7 @@
       "data":{
       	"switchRowsAndColumns": true
       },
-       legend:{
-      	itemStyle: {
-		fontSize: "12px"
-		}
-      },
-    	"tooltip": {
+      "tooltip": {
     		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f} km² ({point.percentage:.1f}%)</b><br/>',
         "shared": false
       },
@@ -21,29 +16,31 @@
 		//"width": 340,
 		"itemWidth": 135,
 	    "itemStyle": {
-	    "fontWeight": "normal"
+    	    "fontWeight": "normal", 
+    	    fontSize: "12px"
 	    }
 	  },
       plotOptions: {
         pie: {
          colors: [
-           '#007A2F',  // Wald
+           '#007A2F',   // Wald
            '#68AB2B ',  // Acker, Wiese, Weide
-           '#73B97C',  // Übrige Grünfläche
-           '#990300',  // Gebäude
-           '#6F6F6F',  // Verkehrsflächen
-           '#3C3C3C',  // Übrige versiegelte Fläche
-           '#008AC3' // Gewässer
+           '#73B97C',   // Übrige Grünfläche
+           '#990300',   // Gebäude
+           '#6F6F6F',   // Verkehrsflächen
+           '#3C3C3C',   // Übrige versiegelte Fläche
+           '#008AC3'    // Gewässer
          ],
         size: "115%",
-	dataLabels: {
-		                enabled: true,
-		                format: '{point.percentage:.1f} %',
-		                distance: 5,
-		                style: {
-		                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black',
-		                    fontSize: '12px' 
-		                }
+	    dataLabels: {
+            enabled: true,
+            format: '{point.percentage:.1f} %',
+            distance: 5,
+            style: {
+                color: 'black',
+                fontSize: '12px', 
+                fontWeight: 'normal'
+            }
 	      }
       }
     }
