ab2ad39681c6c07d4ef0ede55648dd66c4238f2c
Jonas Bieri <jonas.bieri@gmail.com>
2017-07-27 11:52:27 +0000

    Change template to befragungenProzent001

--- charts/templates/5819.js
+++ charts/templates/5819.js
@@ -1,54 +1,17 @@
 (function(){
     return {
-  "plotOptions": {
-    "series": {
-      "pointWidth": 20,
-      "dataLabels": {
-        "style": {
-          "fontSize": "10px"
-        }
-      },
-      "stacking": "normal"
-    }
-  },
-  "xAxis": {
-    "type": "category",
-    "tickInterval": 1
-  },  
-  "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}%"
-    },
-	"max": 100
-  },
-  "tooltip": {
-    "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
-    "shared": true
-  },  
-  "legend": {
-	"enabled": true,
-    "layout": "vertical",
-    "verticalAlign": "middle",
-    "itemMarginBottom": 5,
-    "align": "right",
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },
-  "series": [
-  {"color": "#C8C8C8"}, /*grau*/
-  {"color": "#CD9C00"}, /*dunkelrot*/
-  {"color": "#FABD24"}, /*hellrot*/
-  {"color": "#73B97C"}, /*hellgrün*/
-  {"color": "#007A2F"} /*dunkelgrün*/
-
-  ],
-  "chart": {
-    "renderTo": 'container-I.01.1.0016', 
-    "marginBottom": 75,
-    "marginTop": 75,
-    "type": "column",
-    "inverted": true
-  }
-}
+      "xAxis": {
+        "type": "category"
+      },  
+      "tooltip": {
+        valueDecimals: 1
+      },  
+      "series": [
+        {"color": "#C8C8C8"}, /*grau*/
+        {"color": "#CD9C00"}, /*dunkelrot*/
+        {"color": "#FABD24"}, /*hellrot*/
+        {"color": "#73B97C"}, /*hellgrün*/
+        {"color": "#007A2F"} /*dunkelgrün*/
+      ]
+    };
 }());
