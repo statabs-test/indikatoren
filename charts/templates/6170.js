d0310322d85d1352411107a14c0a7c491c54fda3
larafaelivrin <lara.faelivrin@gmail.com>
2017-09-12 15:50:28 +0000

    Configure chart

---
+++ charts/templates/6170.js
@@ -0,0 +1,71 @@
+(function(){
+    return {
+  "xAxis": {
+    "tickInterval": 3
+  },
+  "yAxis": {
+	"labels": {
+		"format": "{value:,.0f}"
+	}
+  },	
+  "tooltip": {
+    "shared": false, 
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+  },
+ "series": [
+	{"color": "#256370",
+  index: 10, "visible":  false}, /*blau*/
+	{"color": "#a8c4cb",
+  index: 9, "visible":  false}, /*hellblau*/
+	{"color": "#ffbb58",
+  index: 8, "visible":  false}, /*hellorange*/
+	{"color": "#b475ab",
+  index: 7, "visible":  true}, /*hellviolett*/
+	{"color": "#44ab2b",
+  index: 6, "visible":  false}, /*grün*/
+  {"color": "#007a2f",
+  index: 5, "visible":  false},  /*dunkelgrün*/
+	{"color": "#672773",
+  index: 4, "visible":  false}, /*violett*/
+	{"color": "#b00000",
+  index: 3, "visible":  false}, /*rot*/
+	{"color": "#ff8028",
+  index: 2, "visible":  false}, /*orange*/
+	{"color": "#cd9c00",
+  index: 1, "visible":  false}, /*hellbraun*/
+      {
+	  "color": "#E6E6E6",
+	  "index": 0,
+      "legendIndex": 12,
+      "type": "column",
+      "borderWidth": 0,
+      /*"tooltip": {
+    		"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+       }*/
+    }, // Blau
+  ],
+  "legend": {
+    "enabled": true,
+    "x": 45,
+    "y": 35,
+    "itemWidth": 208,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  "plotOptions": {
+    "line": {
+	  "connectNulls": true,
+      "marker":{
+        "enabled": false,
+        "symbol": "circle",
+      }
+    }
+  }
+}
+}());
+ 
--- metadata/sets/Legislaturplan.js
+++ metadata/sets/Legislaturplan.js
@@ -202,5 +202,420 @@ var indikatorensetData = [
 		"externalLinks": [
 			""
 		]
+	},
+	{
+		"title": "Summe der Steuereinnahmen natürlicher und juristischer Personen in Mio. Franken",
+		"subtitle": "",
+		"kuerzel": "I.04.1.06132",
+		"kuerzelKunde": "2.4",
+		"description": "Summe der Steuereinnahmen natürlicher und juristischer Personen in Mio. Franken",
+		"id": 6132,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Steuer"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Folgen der Finanz- und Wirtschaftskrise, die 2008 begann,<br>wurden vor allem bei der Gewinn- und Kapitalsteuer deutlich. Die<br>Steuereinnahmen aus Einkommens- und Vermögenssteuer<br>betrugen 2015 rund 1,86 Mrd. Franken.",
+		"erlaeuterungen": "Steuereinnahmen gemäss Jahresrechung des Kantons Basel-Stadt; direkte Steuern natürlicher und juristischer Personen. Im Gegensatz zur Steuerstatistik auf das Kalenderjahr bezogen, nicht auf das Steuerjahr.",
+		"template": "line001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "08_00_00_00_00_001",
+		"parentId": 5162,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Jahresbericht des Kantons Basel-Stadt"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Beurteilung der kantonalen Steuern",
+		"subtitle": "«Finden Sie, dass die kantonalen Steuern und Gebühren erhöht, gesenkt oder beibehalten werden sollen, wie sie heute sind?»",
+		"kuerzel": "I.50.1.06133",
+		"kuerzelKunde": "2.5",
+		"description": "Beurteilung der kantonalen Steuern",
+		"id": 6133,
+		"thema": "50 Befragungen",
+		"unterthema": "Bevölkerungsbefragung",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Verwaltung",
+			"Beurteilung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "2015 gaben mit 56,4% mehr als die Hälfte der Befragten an, dass die kantonalen Steuern und Gebühren beibehalten werden sollten. Dieser Anteil fällt im Vergleich zu den Vorjahren deutlich höher aus (2011: 45,6%, 2007: 30,0%).",
+		"erlaeuterungen": "",
+		"template": "befragungenProzent001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "08_00_00_00_00_001",
+		"parentId": 4860,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, kantonale Bevölkerungsbefragung: F8"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Schulabgänger/-innen ohne Anschlusslösung nach Geschlecht und Schultyp",
+		"subtitle": "",
+		"kuerzel": "I.15.7.06135",
+		"kuerzelKunde": "3.1",
+		"description": "Schulabgänger/-innen ohne Anschlusslösung nach Geschlecht und Schultyp",
+		"id": 6135,
+		"thema": "15 Bildung",
+		"unterthema": "Bildungsabschlüsse",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Geschlecht",
+			"Bildung",
+			"Schule"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Jugendlichen der Abschlussklassen der Sekundarstufe I werden jeweils Ende Schuljahr schriftlich über ihren nächsten Ausbildungsschritt befragt. Nachdem sich die Mädchen bei der Suche nach einer Anschlusslösung bis 2010 vergleichsweise schwer getan haben, gelingt ihnen dies seit 2011 besser als den Knaben (Ausnahme 2012). Die Werte 2016 deuten darauf hin, dass die Jugendlichen wieder mehr Mühe haben, eine für sie passende Anschlusslösung zu finden.",
+		"erlaeuterungen": "Die Weiterbildungsschule läuft Ende Schuljahr 2017/18 aus.",
+		"template": "template001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "08_00_00_00_00_001",
+		"parentId": 3051,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Erziehungsdepartement Basel-Stadt, Berufs- und Schulwahlerhebung"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Höchste abgeschlossene Ausbildung nach Geschlecht, Herkunft und Altersgruppe",
+		"subtitle": "",
+		"kuerzel": "I.15.6.06136",
+		"kuerzelKunde": "3.2",
+		"description": "Höchste abgeschlossene Ausbildung nach Geschlecht, Herkunft und Altersgruppe",
+		"id": 6136,
+		"thema": "15 Bildung",
+		"unterthema": "Bildungsniveau",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Geschlecht",
+			"Alter",
+			"Bildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "08_00_00_00_00_001",
+		"parentId": 5191,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": " Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"subtitle": "",
+		"kuerzel": "I.04.1.06138",
+		"kuerzelKunde": "3.4",
+		"description": " Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"id": 6138,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Einkommen",
+			"Staatspersonal",
+			"Angestellte",
+			"Konsum",
+			"Schulden"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Gesamtausgaben pro Kopf stiegen von 20 817 Fr. im Jahr 2003 auf 23 335 Fr. im Jahr 2013. Der höchste Wert wird mit 7 068 Fr. pro<br>Kopf für den Bereich Bildung notiert, auf den weiteren Rängen folgen Soziale Sicherheit, Gesundheit und Öffentliche Ordnung und<br>Sicherheit. Der Anstieg der Ausgaben beim Posten",
+		"erlaeuterungen": "Aufgrund einer Änderung in der Finanzstatistik der Eidgenössischen Finanzverwaltung (EFV) ist der Vergleich der Daten ab 2008 mit den Jahren 2000-2007 nur von bedingter Aussagekraft. Seit der Einführung des Neuen Finanzausgleichs 2008 wendet die EFV in der Finanzstatistik ein neues Sektorisierungskonzept an, in dem die Ausgaben für Spitäler aus der Staatsrechnung ausgebucht werden.",
+		"template": "line001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "",
+		"parentId": 5161,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Eidgenössische Finanzverwaltung"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Transnationale Patentanmeldungen pro 1000 Einwohner nach Technologiefeld",
+		"subtitle": "nach ausgewählten Wirtschaftsbranchen",
+		"kuerzel": "I.04.1.06139",
+		"kuerzelKunde": "3.5",
+		"description": "Transnationale Patentanmeldungen pro 1000 Einwohner nach Technologiefeld",
+		"id": 6139,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bewilligung",
+			"Schule",
+			"Standort",
+			"Kantonsvergleich"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Es werden die Patentanmeldungen pro Einwohner im Mittel über mehrere Jahre gezeigt. In Basel-Stadt werden am meisten Patente in der Pharmazeutischen Industrie angemeldet.",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "",
+		"parentId": 3087,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"BAK Basel Economics"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Aufwendungen für Forschung und Entwicklung in der Privatwirtschaft in der NWCH und anderen Grossregionen, Anteil am BIP",
+		"subtitle": "",
+		"kuerzel": "I.04.2.06140",
+		"kuerzelKunde": "3.6",
+		"description": "Aufwendungen für Forschung und Entwicklung in der Privatwirtschaft in der NWCH und anderen Grossregionen, Anteil am BIP",
+		"id": 6140,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Konjunktur",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bildung",
+			"BIP",
+			"Standort",
+			"Branche"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "In der Nordwestschweiz machten 2012 die Aufwendungen für<br>Forschung und Entwicklung in der Privatwirtschaft 5,8% des<br>Bruttoinlandprodukts aus. Dieser Anteil ist tiefer als 2008, als er<br>noch bei 6,6% lag.",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "",
+		"parentId": 6049,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Bundesamt für Statistik (BFS)"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"subtitle": "",
+		"kuerzel": "I.04.1.06170",
+		"kuerzelKunde": "8.4",
+		"description": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"id": 6170,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Einkommen",
+			"Staatspersonal",
+			"Angestellte",
+			"Konsum",
+			"Schulden"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Gesamtausgaben pro Kopf stiegen von 20 817 Fr. im Jahr 2003 auf 23 335 Fr. im Jahr 2013. Der höchste Wert wird mit 7 068 Fr. pro<br>Kopf für den Bereich Bildung notiert, auf den weiteren Rängen folgen Soziale Sicherheit, Gesundheit und Öffentliche Ordnung und<br>Sicherheit. Der Anstieg der Ausgaben beim Posten",
+		"erlaeuterungen": "Aufgrund einer Änderung in der Finanzstatistik der Eidgenössischen Finanzverwaltung (EFV) ist der Vergleich der Daten ab 2008 mit den Jahren 2000-2007 nur von bedingter Aussagekraft. Seit der Einführung des Neuen Finanzausgleichs 2008 wendet die EFV in der Finanzstatistik ein neues Sektorisierungskonzept an, in dem die Ausgaben für Spitäler aus der Staatsrechnung ausgebucht werden.",
+		"template": "line001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "",
+		"parentId": 5161,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Eidgenössische Finanzverwaltung"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"subtitle": "",
+		"kuerzel": "I.04.1.06172",
+		"kuerzelKunde": "9.2",
+		"description": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"id": 6172,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Einkommen",
+			"Staatspersonal",
+			"Angestellte",
+			"Konsum",
+			"Schulden"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Gesamtausgaben pro Kopf stiegen von 20 817 Fr. im Jahr 2003 auf 23 335 Fr. im Jahr 2013. Der höchste Wert wird mit 7 068 Fr. pro<br>Kopf für den Bereich Bildung notiert, auf den weiteren Rängen folgen Soziale Sicherheit, Gesundheit und Öffentliche Ordnung und<br>Sicherheit. Der Anstieg der Ausgaben beim Posten",
+		"erlaeuterungen": "Aufgrund einer Änderung in der Finanzstatistik der Eidgenössischen Finanzverwaltung (EFV) ist der Vergleich der Daten ab 2008 mit den Jahren 2000-2007 nur von bedingter Aussagekraft. Seit der Einführung des Neuen Finanzausgleichs 2008 wendet die EFV in der Finanzstatistik ein neues Sektorisierungskonzept an, in dem die Ausgaben für Spitäler aus der Staatsrechnung ausgebucht werden.",
+		"template": "line001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": false,
+		"orderKey": "",
+		"parentId": 5161,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Eidgenössische Finanzverwaltung"
+		],
+		"externalLinks": [
+			""
+		]
+	},
+	{
+		"title": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"subtitle": "",
+		"kuerzel": "I.04.1.06184",
+		"kuerzelKunde": "11.5",
+		"description": "Entwicklung der Staatsausgaben nach Funktion in Franken pro Kopf",
+		"id": 6184,
+		"thema": "04 Volkswirtschaft",
+		"unterthema": "Volkseinkommen",
+		"kennzahlenset": "Legislaturplan",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Einkommen",
+			"Staatspersonal",
+			"Angestellte",
+			"Konsum",
+			"Schulden"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Gesamtausgaben pro Kopf stiegen von 20 817 Fr. im Jahr 2003 auf 23 335 Fr. im Jahr 2013. Der höchste Wert wird mit 7 068 Fr. pro<br>Kopf für den Bereich Bildung notiert, auf den weiteren Rängen folgen Soziale Sicherheit, Gesundheit und Öffentliche Ordnung und<br>Sicherheit. Der Anstieg der Ausgaben beim Posten",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"option": "options001",
+		"visible": true,
+		"visibleInPortal": true,
+		"orderKey": "",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Eidgenössische Finanzverwaltung"
+		],
+		"externalLinks": [
+			""
+		]
 	}
 ];
