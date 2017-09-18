8e816be5ecd65f9dd71b6113d388f993e3ad4ded
larafaelivrin <lara.faelivrin@gmail.com>
2017-09-18 15:58:21 +0000

    Configure chart

---
+++ charts/templates/6173.js
@@ -0,0 +1,38 @@
+(function(){
+    return {
+  
+  "xAxis": {
+    "type": "category",
+  },
+  "yAxis": {
+	//"max": 100,
+    "labels": {
+      "format": "{value:,.2f}"
+    }    
+  },
+  "series": [
+  {"color": "#83522E", "visible": true}, /* dunkelbraun*/
+  //{"color": "#007A2F", "visible": true} /* ... */
+  ],
+  "legend": {
+    "enabled": false,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "align": "left",
+    "x": 35,
+	"y": 35,    
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+   tooltip: {
+    "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
+    "shared": false
+  },
+  "chart": {      
+    "type": "column",
+    "inverted": false,
+    "spacingBottom": 40
+  }
+}
+}());
---
+++ charts/templates/6174.js
@@ -0,0 +1,34 @@
+(function(){
+    return {
+  "xAxis": {
+    "type": "category"
+  },
+  "series": [
+    {
+      "color": "#999999", // grau
+      "index": 2
+    },
+    {
+      "color": "#dc440e", // rot
+      "index": 0
+    },
+    {
+      "color": "#ffbb58", //orange
+      "index": 1
+    },
+    {
+      "color": "#68ab2b", //hellgrün
+      "index": 3
+    },
+    {
+      "color": "#007a2f", //dunkelgrün
+      "index": 4
+    }
+  ],
+  "plotOptions": {
+    "series": {
+      "pointWidth": 30,
+      },
+  }
+	}
+}());
