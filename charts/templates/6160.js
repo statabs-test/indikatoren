025c2f4113be112a4c166d3d0804d11ad1d662d7
larafaelivrin <lara.faelivrin@gmail.com>
2017-09-18 14:46:34 +0000

    Configure chart

---
+++ charts/templates/6160.js
@@ -0,0 +1,58 @@
+(function(){
+    return {
+"plotOptions": {
+    "series": {
+      pointPadding: 100,
+      "pointWidth": 15,
+      "dataLabels": {
+        "style": {
+          "fontSize": "10px"
+        }
+      },
+      "stacking": "normal"
+    }
+  },
+  "xAxis": {
+    "type": "category",
+    "tickInterval": 1
+  },  
+  "yAxis": {
+  	 //max: 120000,
+     //tickInterval: 60000,
+    "labels": {
+      "format": "{value:,.0f}",
+    },
+	"max": undefined
+  },
+  "legend": {
+    "enabled": true,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "align": "left",
+    "x": 45,
+	"y": 20,
+	"Itemwidth": 500,
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  "series": [
+  {"color": "#B00000", "index": 4}, /*rot */
+  {"color": "#FF8028", "index": 3}, /*dunkelorange */
+  {"color":"#E6E600", "index": 2}, /*dunkelgelb */
+  {"color":"#68AB2B ", "index": 1}, /*dunkelgrün */
+  ],
+  "chart": {
+    "renderTo": 'container-I.01.1.0016', 
+    "marginBottom": 75,
+    "marginTop": 75,
+    "type": "column",
+    "inverted": false
+  },
+  "tooltip": {
+  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
+  "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
+   "shared": true
+  },
+}
+}());
