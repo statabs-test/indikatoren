6b507353e9b3c076ccbe6ef103ea2cb5efd15e34
larafaelivrin <lara.faelivrin@gmail.com>
2017-10-04 10:34:16 +0000

    Configure chart

--- charts/templates/6133.js
+++ charts/templates/6133.js
@@ -1,18 +1,10 @@
 (function(){
     return {
     "series": [
-        {
-            "color": "#2B0099" // dunkelblau
-        },
-        {
-            "color": "#B6CFD7" // hellblau
-        },
-        {
-            "color": "#3C3C3C" // grau
-        },  
-        {
-            "color": "#E6E600" //hellorange
-        },
+        {"color": "#2B0099"}, // dunkelblau
+        {"color": "#B6CFD7"}, // hellblau,
+        {"color": "#3C3C3C"}, // grau
+        {"color": "#E6E600"}, //hellorange
 ],
   "xAxis": {
       "type": "category",
--- charts/templates/6146.js
+++ charts/templates/6146.js
@@ -1,31 +1,11 @@
 (function(){
     return {
         "series": [
-            {
-                "color": "#007A2F", // dark green
-                "index": 4,
-                "legendIndex": 4 
-            },
-            {
-                "color": "#73BA7C", // light green
-                "index": 3,
-                "legendIndex": 3 
-            },
-                 {
-                "color": "#C8C8C8", // grey
-                "index": 2,
-                "legendIndex":2 
-            },        
-            {
-                "color": "#FFBB58", // light orange
-                "index": 1,
-                "legendIndex": 1 
-            },
-            {
-                "color": "#FF8028", // dark orange
-                "index": 0,
-                "legendIndex": 0 
-            },
+            {"color": "#007A2F"},
+            {"color": "#73BA7C"}, // light green
+            {"color": "#C8C8C8"}, // grey
+            {"color": "#FFBB58"}, // light orange
+            {"color": "#FF8028"}, // dark orange
     ],
     
     "xAxis": {
