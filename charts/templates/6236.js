28dd0bc7f349acb3decdbe2feb7c2e9aaf67eb53
larafaelivrin <lara.faelivrin@gmail.com>
2017-10-11 12:28:38 +0000

    config chart

--- charts/templates/6236.js
+++ charts/templates/6236.js
@@ -33,15 +33,15 @@
     }
   },
  "series": [
-  {"color": "#007a2f", "index": 0, "legendIndex":0}, /*grün dunkel2*/
-  {"color": "#44ab2b", "index": 1, "legendIndex":1}, /*grün mittel*/
-  {"color": "#73ba7c", "index": 2, "legendIndex":2}, /*grün dunkel1*/
-  {"color": "#d7e9d2", "index": 3, "legendIndex":3}, /*grün hell*/
+  {"color": "#007a2f", "index": 8, "legendIndex":8}, /*grün dunkel2*/
+  {"color": "#44ab2b", "index": 7, "legendIndex":7}, /*grün mittel*/
+  {"color": "#73ba7c", "index": 6, "legendIndex":6}, /*grün dunkel1*/
+  {"color": "#d7e9d2", "index": 5, "legendIndex":5}, /*grün hell*/
   {"color": "#b9cfd7", "index": 4, "legendIndex":4}, /*blau hell */
-  {"color": "#71a3b5", "index": 5, "legendIndex":5}, /*blau dunkel */
-  {"color": "#ffbb58", "index": 6, "legendIndex":6}, /*orange hell */
-  {"color": "#ff8028", "index": 7, "legendIndex":7}, /*orange dunkel */
-  {"color": "#b00000", "index": 9, "legendIndex":9} /*rot dunkel */
+  {"color": "#71a3b5", "index": 3, "legendIndex":3}, /*blau dunkel */
+  {"color": "#ffbb58", "index": 2, "legendIndex":2}, /*orange hell */
+  {"color": "#ff8028", "index": 1, "legendIndex":1}, /*orange dunkel */
+  {"color": "#b00000", "index": 0, "legendIndex":0} /*rot dunkel */
   ],
   "tooltip": {
     "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
