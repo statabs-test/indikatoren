9fff3c06b9230e9c45eee4a33b92568c68bfb693
larafaelivrin <lara.faelivrin@gmail.com>
2017-11-17 14:43:30 +0000

    config chart

--- charts/templates/6156.js
+++ charts/templates/6156.js
@@ -1,46 +1,56 @@
 (function(){
     return {
-   "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}%",
-    }
-  },
   "xAxis": {
-	"tickInterval": 1,
+    "tickInterval": 1,
   },
-  "series": [
-    {
-      "marker": {
-        "symbol": "circle",
-        "enabled": true
-      },     
-      "color": "#8b2223"
-    }
-  ],
+  "yAxis": {
+    "min": 0, 
+	  "labels": {
+		  "format": "{value}%"
+	  }
+  },	
   "tooltip": {
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}%</b><br/>',
-    "shared": false
+    "shared": false, 
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b>%<br/>'
+  },
+ "series": [
+    {
+		"color": "#B00000",
+    	"legendIndex": 2
+    }, /* rot */
+    {
+		"color": "#662673",
+    	"legendIndex": 2
+    }, /* violett */
+    {
+		"color": "#008AC3",
+    	"legendIndex": 2
+    }, /* blau */
+    {
+    	"color": "#007A2F", 
+    	"legendIndex": 0
+    }, /* grün */
+  ],
+   "legend": {
+    "enabled": true,
+    //"x": 45,
+    "y": 35,
+    "itemWidth": 150,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
   },
   "plotOptions": {
-    "series": {
-      "dataLabels": {
-        "enabled": true,
-        "allowOverlap": true,
-          y: 40,
-          //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
-          formatter: function() {
-            var last = this.series.data[this.series.data.length - 1];
-            var first = this.series.data[0];
-            if (this.point.category === first.category && this.point.y === first.y || this.point.category === last.category && this.point.y === last.y ) {
-              return Highcharts.numberFormat(this.point.y, 0, ",", " ") + '%';
-            }
-            return "";
-          }
-      }
-    },
-    
-    
+    "line": {
+      "marker":{
+        "enabled": false,
+        "symbol": "circle",
+      } 
+    }
   }
 }
 }());
-
