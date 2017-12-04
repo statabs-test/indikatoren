20219081e0961db26cb4fd61e9e7f8c1982f8fc7
larafaelivrin <lara.faelivrin@gmail.com>
2017-12-04 11:26:35 +0000

    config chart

--- charts/templates/6011.js
+++ charts/templates/6011.js
@@ -48,7 +48,7 @@
   },
   "series": [
     {
-      "color": "#83038", // dunkelblau -Zuzug_Unbekannt
+      "color": "#2B0099", // dunkelblau -Zuzug_Unbekannt
       //"index": 0,
       "type": "column",
       "pointWidth": "15",
