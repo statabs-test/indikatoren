27ff2165163856ebff2a665a2f03c98cbbb27007
Jonas Bieri <jonas.bieri@gmail.com>
2017-12-08 14:30:11 +0000

    Add remote html2pdf server, see #494

--- charts/templates/options001.js
+++ charts/templates/options001.js
@@ -67,6 +67,13 @@ menuItems.push(
     });
 menuItems.push(
     {
+        "text": "PDF mit Details", 
+        "onclick": function(){            
+            window.open('http://www.html2pdf.it/?url=' /* + $.url('protocol') + '://' + $.url('hostname') + ':' + $.url('port') + '/' + $.url(1) +*/ + 'https://statabs.github.io/indikatoren/chart-details.html?id=' + this.renderTo.id.substring(10) + indikatorensetParameter + "&suppressNumberInTitle=true", '_blank'); 
+        }
+    });
+menuItems.push(
+    {
         "text": "Einbetten", 
         "onclick": function(){            
             window.open($.url('protocol') + '://' + $.url('hostname') + ':' + $.url('port') + '/' + $.url(1) + '/chart.html?id=' + this.renderTo.id.substring(10) + indikatorensetParameter + "&suppressNumberInTitle=true", '_blank'); 
