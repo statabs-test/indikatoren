56951c58edc42e4dc7e35778adf07f27281eaf25
Jonas Bieri <jb3-2@users.noreply.github.com>
2017-12-18 14:11:36 +0100

    Issue 325 (#169)
    * improved bubble zoom 6025 #550

--- charts/templates/6025.js
+++ charts/templates/6025.js
@@ -7,6 +7,8 @@
 
     return {
     	"legend": {
+    		"x": -10,
+			"y": 0,
 			"title": {
 				"text": ""
 			}
@@ -104,13 +106,14 @@
 						return {
 	                        sizeFormatter: function () {
 	                            var fn = this.chart.options.customFunctions;
-								return fn.pieSize(Math.abs(data.value), fn.getPointsExtremes(pieSizeSeries.points).maxAbsNumber, maxPieDiameter); 
+	                            var yAxis = chart.yAxis[0], zoom = (yAxis.dataMax - yAxis.dataMin) / (yAxis.max - yAxis.min);
+								return zoom * fn.pieSize(Math.abs(data.value), fn.getPointsExtremes(pieSizeSeries.points).maxAbsNumber, maxPieDiameter); 
 								//return fn.pieSizeCategorical(Math.abs(data.value), pieSizeCatConfig).diameter;
 	                        },
 	                        tooltip: {
 	                        	headerFormat: '<span style="color:{point.color}">\u25CF</span> <span style="font-size: 10px"> {point.key} </span><br/>',
 	                            pointFormatter: function () {
-	                            	return correspondingMapSeriesItem.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.v),1) + '</b><br/>';
+	                            	return correspondingMapSeriesItem.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.v),1) + ' % </b><br/>';
 	                            }
 	                        },
 	                    };
@@ -124,18 +127,18 @@
 	                var maxValueInLegend = 0.1; 
 	                
                 	//Add manually drawn legend	
-	                fn.addLegendTitle(chart,"Anteil Zugezogene nach Zuzugsland", 260, 240);
+	                fn.addLegendTitle(chart,"Anteil Zugezogene nach Zuzugsland", 255, 240);
 	                
 	                //fn.addLegendCircle(chart, 410, 275, 0.5*fn.pieSize(minValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), 'grey');
 	                //fn.addLegendLabel(chart, Highcharts.numberFormat((minValueInLegend),3,","," "), 430, 265);
 	                //fn.addLegendCircle(chart, 410, 300, 0.5*fn.pieSize(maxValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), 'grey');
 	                //fn.addLegendLabel(chart, Highcharts.numberFormat((maxValueInLegend),2,"."," "), 430, 290);
 
-					fn.addLegendSquare(chart, 270, 265, 10, 'red');
-					fn.addLegendLabel(chart, 'Schweiz', 290, 260);
-					fn.addLegendSquare(chart, 270, 285, 10, 'blue');
-					fn.addLegendLabel(chart, 'Ausland', 290, 280);
-					fn.addLegendLabelbold(chart, 'Anzahl Zugezogene pro 100 Einwohner', 260, 305);
+					fn.addLegendSquare(chart, 258, 265, 10, 'red');
+					fn.addLegendLabel(chart, 'Schweiz', 270, 260);
+					fn.addLegendSquare(chart, 258, 285, 10, 'blue');
+					fn.addLegendLabel(chart, 'Ausland', 270, 280);
+					fn.addLegendLabelbold(chart, 'Anzahl Zugezogene pro 100 Einwohner', 255, 305);
 					
 					//make sure pies are hidden upon click onto pie legend
 					fn.AddPieLegendClickHandler(chart);
