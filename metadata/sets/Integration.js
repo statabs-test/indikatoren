20ec5e8830f9a8ff9ef8a4298452b126681d8405
larafaelivrin <lara.faelivrin@gmail.com>
2017-12-22 14:58:41 +0000

    config chart

---
+++ metadata/sets/Integration.js
@@ -0,0 +1,461 @@
+var indikatorensetData = [
+	{
+		"title": "Ausländer nach Staatsangehörigkeit (Ländergruppen) 1997-2016",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06059",
+		"kuerzelKunde": "A1-4",
+		"description": "Ausländer nach Staatsangehörigkeit (Ländergruppen) 1997-2016",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6059
+	},
+	{
+		"title": "Bevölkerungspyramide Schweizer 1997",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06064",
+		"kuerzelKunde": "A2-2",
+		"description": "Bevölkerungspyramide Schweizer 1997",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "populationPyramid001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6064
+	},
+	{
+		"title": "Bevölkerungspyramide Ausländer 1997",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06065",
+		"kuerzelKunde": "A2-3",
+		"description": "Bevölkerungspyramide Ausländer 1997",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "populationPyramid001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Statistisches Amt Basel-Stadt, Bevölkerungsstatistik"
+		],
+		"externalLinks": [
+			""
+		],
+		"id": 6065
+	},
+	{
+		"title": "Bevölkerungspyramide Schweizer 2016",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06066",
+		"kuerzelKunde": "A2-4",
+		"description": "Bevölkerungspyramide Schweizer 2016",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "populationPyramid001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6066
+	},
+	{
+		"title": "Bevölkerungspyramide Ausländer 2016",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06067",
+		"kuerzelKunde": "A2-5",
+		"description": "Bevölkerungspyramide Ausländer 2016",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "populationPyramid001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6067
+	},
+	{
+		"title": "Anteil Ausländer nach Alter 1997, 2006, 2016",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06070",
+		"kuerzelKunde": "A2-8",
+		"description": "Anteil Ausländer nach Alter 1997, 2006, 2016",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Ausland",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6070
+	},
+	{
+		"title": "Zusammengefasste Geburtenziffer nach Staatsangehörigkeit (Ländergruppen) Mittel der Jahre 2006-2015",
+		"subtitle": "",
+		"kuerzel": "I.01.3.06077",
+		"kuerzelKunde": "A4-3",
+		"description": "Zusammengefasste Geburtenziffer nach Staatsangehörigkeit (Ländergruppen) Mittel der Jahre 2006-2015",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Geburten und Todesfälle",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6077
+	},
+	{
+		"title": "Segregationsindex nach Staatsangehörigkeit",
+		"subtitle": "",
+		"kuerzel": "I.01.1.06078",
+		"kuerzelKunde": "A5-1",
+		"description": "Segregationsindex nach Staatsangehörigkeit",
+		"thema": "01 Bevölkerung",
+		"unterthema": "Bestand und Struktur ",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Staatsangehörigkeit"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": 6152,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6078
+	},
+	{
+		"title": "Gymnasialquoten nach Staatsangehörigkeit (Ländergruppen) 2010-2016",
+		"subtitle": "",
+		"kuerzel": "I.15.1.06080",
+		"kuerzelKunde": "B1-1",
+		"description": "Gymnasialquoten nach Staatsangehörigkeit (Ländergruppen) 2010-2016",
+		"thema": "15 Bildung",
+		"unterthema": "Schulen und Schulpersonal",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Staatsangehörigkeit",
+			"Ausbildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6080
+	},
+	{
+		"title": "Gymnasialquoten nach Staatsangehörigkeit (CH/A) und Geschlecht 2010-2016",
+		"subtitle": "",
+		"kuerzel": "I.15.1.06081",
+		"kuerzelKunde": "B1-2",
+		"description": "Gymnasialquoten nach Staatsangehörigkeit (CH/A) und Geschlecht 2010-2016",
+		"thema": "15 Bildung",
+		"unterthema": "Schulen und Schulpersonal",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Geschlecht",
+			"Staatsangehörigkeit",
+			"Ausbildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6081
+	},
+	{
+		"title": "Hochschulquote nach Staatsangehörigkeit (Ländergruppen) 2002-2016",
+		"subtitle": "",
+		"kuerzel": "I.15.4.06082",
+		"kuerzelKunde": "B2-1",
+		"description": "Hochschulquote nach Staatsangehörigkeit (Ländergruppen) 2002-2016",
+		"thema": "15 Bildung",
+		"unterthema": "Hochschulen",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Staatsangehörigkeit",
+			"Ausbildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6082
+	},
+	{
+		"title": "Hochschulquote nach Staatsangehörigkeit (CH/A) und Geschlecht 1997-2016",
+		"subtitle": "",
+		"kuerzel": "I.15.4.06083",
+		"kuerzelKunde": "B2-2",
+		"description": "Hochschulquote nach Staatsangehörigkeit (CH/A) und Geschlecht 1997-2016",
+		"thema": "15 Bildung",
+		"unterthema": "Hochschulen",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Geschlecht",
+			"Staatsangehörigkeit",
+			"Ausbildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "line001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6083
+	},
+	{
+		"title": "Wohnbevölkerung ab 15 Jahren nach Staatsangehörigkeit (Ländergruppen) und höchster abgeschlossener Ausbildung, gepoolte Daten 2011-2015",
+		"subtitle": "",
+		"kuerzel": "I.15.7.06090",
+		"kuerzelKunde": "B4-2",
+		"description": "Wohnbevölkerung ab 15 Jahren nach Staatsangehörigkeit (Ländergruppen) und höchster abgeschlossener Ausbildung, gepoolte Daten 2011-2015",
+		"thema": "15 Bildung",
+		"unterthema": "Bildungsabschlüsse",
+		"kennzahlenset": "Integration",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"schlagwort": [
+			"Bevölkerung",
+			"Staatsangehörigkeit",
+			"Ausbildung"
+		],
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "",
+		"erlaeuterungen": "",
+		"template": "template001",
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [],
+		"externalLinks": [
+			""
+		],
+		"id": 6090
+	}
+];
--- metadata/sets/indikatorensetNames.js
+++ metadata/sets/indikatorensetNames.js
@@ -8,6 +8,7 @@ var indikatorensetNames = [
 	"Wohnviertel",
 	"Richtplan",
 	"Wanderungen",
+	"Integration",
 	"Richtplan (Print)",
 	"Legislaturplan (Print)",
 	"Test"
