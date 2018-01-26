71c6a3faece51a175575bd89d2833214d5e27c6d
larafaelivrin <lara.faelivrin@gmail.com>
2018-01-26 16:15:14 +0000

    config chart

--- charts/templates/6061.js
+++ charts/templates/6061.js
@@ -14,28 +14,25 @@
           }        
       ],
     	"tooltip": {
-    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f} ({point.percentage:,.1f}%)</b><br/>',
+    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f} ({point.percentage:,.1f}%)</b><br/>',
         "shared": false
       },
       plotOptions: {
         pie: {
          colors: [
-           '#B00000', 
+    	   '#B00000', 
            '#FF8028',
            '#FABD24',
            '#E6E600',
            "#8AB77D",
            '#68AB2B',
            '#007A2F',
-           '#A8C3CA', 
-           '#008AC3',
-           '#3C3C3C',
-           '#923F8D',
-           ],
+
+        ],
 		dataLabels: {
 		                enabled: true,
 		                format: '{point.percentage:.1f}%',
-		                distance: 20,
+		                distance: 10,
 		                style: {
 		                    color: 'black',
 		                    textOutline: "0px black", 
--- charts/templates/6481.js
+++ charts/templates/6481.js
@@ -1,10 +1,16 @@
 (function(){
     return {
-      legend:{
-      	style: {
-		fontSize: "12px"
-		}
-      },
+	  "legend": { 
+	    y: 55,
+	    "enabled": true,
+	    "layout": "horizontal",
+	   "itemWidth": 1000,
+	    "verticalAlign": "top",
+	    "align": "left",
+	    "itemStyle": {
+	    "fontWeight": "normal"
+	    }
+	  },
       "data":{
       	"switchRowsAndColumns": true
       },
@@ -14,28 +20,21 @@
           }        
       ],
     	"tooltip": {
-    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f} ({point.percentage:,.1f}%)</b><br/>',
+    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f} ({point.percentage:,.1f}%)</b><br/>',
         "shared": false
       },
       plotOptions: {
         pie: {
          colors: [
-           '#B00000', 
+    	   '#B00000', 
            '#FF8028',
            '#FABD24',
            '#E6E600',
-           "#8AB77D",
-           '#68AB2B',
-           '#007A2F',
-           '#A8C3CA', 
-           '#008AC3',
-           '#3C3C3C',
-           '#923F8D',
-           ],
+        ],
 		dataLabels: {
 		                enabled: true,
 		                format: '{point.percentage:.1f}%',
-		                distance: 20,
+		                distance: 10,
 		                style: {
 		                    color: 'black',
 		                    textOutline: "0px black", 
