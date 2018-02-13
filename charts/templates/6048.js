59d20f2aea814a6dd970e27f1dc84fd7cbf7a9b7
larafaelivrin <lara.faelivrin@gmail.com>
2018-02-13 17:25:03 +0000

    update 6048

--- charts/templates/6048.js
+++ charts/templates/6048.js
@@ -41,8 +41,8 @@
     "inverted": false
   },
   "tooltip": {
-  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>',
-  "footerFormat": 'Total: <b>{point.total:,.2f}%</b>',
+  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
+  "footerFormat": 'Total: <b>{point.total:,.1f}%</b>',
    "shared": true
   },
 }
