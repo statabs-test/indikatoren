197208c7a0da83a8baaa1e403ed637d1d8eecf25
Jonas Bieri <jonas.bieri@gmail.com>
2018-03-07 14:12:44 +0000

    Improve tooltip of 5198, build. Close #707

--- charts/templates/5198.js
+++ charts/templates/5198.js
@@ -34,14 +34,11 @@
     useHTML: true
   },*/
         "tooltip": {
+            useHTML: true,
             "formatter": function(args){
-                var this_point_index = this.series.data.indexOf(this.point);
-                var other_series_index = this.series.index == 0 ? 1 : 0; // assuming 2 series
-                var other_series = args.chart.series[other_series_index];
-                var other_point = other_series.data[this_point_index];
-                return '<span style="color:' + this.color + ';">\u25CF</span><span style="font-size: 0.85em;"> ' + this.series.name + ':</span><br/>' + 
-                    this.point.name +': <b>' + Highcharts.numberFormat((this.y*100),1) + '%</b><br/>' + 
-                    'Rang <b>' + other_point.y + '</b>';
+                console.log(this);
+                return '<span style="color:' + this.color + ';">\u25CF</span><span style="font-size: 0.85em;"> ' + this.key + ':</span><br/>' + 
+                    '<b>' + Highcharts.numberFormat(this.y, 1) + '</b> m<sup>2</sup> pro Einwohner';
             },        
             shared: true
         },
