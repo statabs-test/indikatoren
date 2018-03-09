f3c4c5a17ecc90683ac520cd5eb2af45ac1352d8
Jonas Bieri <jonas.bieri@gmail.com>
2018-03-09 11:06:24 +0000

    Optimize 6090

--- charts/templates/6090.js
+++ charts/templates/6090.js
@@ -1,31 +1,15 @@
 (function(){
     return {
-"plotOptions": {
-    "series": {
-      pointPadding: 100,
-      borderWidth: 0,
-      "pointWidth": 10,
-      "dataLabels": {
-        "style": {
-          "fontSize": "10px"
-        }
-      },
-      "stacking": "normal"
-    }
-  },
-  "xAxis": {
-    "type": "category",
-    "tickInterval": 1
-  },  
-  "yAxis": {
-  	max: 100,
-  	tickInterval: 20,
-    "labels": {
-      "format": "{value:,.0f}%",
-    },  
-  	},
-  "legend": {
-  	"itemWidth": 115,
+ "series": [
+  {"color": "rgb(103,39,115)", index: 2, legendIndex: 0}, /*Sek1*/
+  {"color": "rgb(180,117,171)", index: 1, legendIndex: 1}, /*Sek2*/
+  {"color": "rgb(231,207,226)", index: 0, legendIndex:2}, /*Tert.*/
+],
+  "chart": {
+    "inverted": true
+  }, 
+  legend: {
+    itemDistance: 15,
     "enabled": true,
     "layout": "horizontal",
     "verticalAlign": "top",
@@ -34,22 +18,19 @@
     "itemStyle": {
       "fontWeight": "normal"
     }
+  },  
+  xAxis: {
+    labels: {
+      formatter: function() {
+      	return this.value;
+      }
+    }
   },
- "series": [
-  {"color": "rgb(103,39,115)", index: 2, legendIndex: 0}, /*Sek1*/
-  {"color": "rgb(180,117,171)", index: 1, legendIndex: 1}, /*Sek2*/
-  {"color": "rgb(231,207,226)", index: 0, legendIndex:2}, /*Tert.*/
-],
-  "chart": {
-    "renderTo": 'container-I.01.1.0016', 
-    "marginBottom": 75,
-    //"marginTop": 75,
-    "type": "column",
-    "inverted": true
+  yAxis: {
+     tickInterval: 20,
   },
-  "tooltip": {
-  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>',
-   "shared": false
-  },
+  tooltip: {
+    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.0f}</b> ({point.percentage:.1f}%)<br/>',
+  }
 };
 }());
