76bcd54a3ad9b6976a0a40821525b5872066dd6f
larafaelivrin <lara.faelivrin@gmail.com>
2018-03-09 13:37:31 +0000

    2173+

--- charts/templates/2173.js
+++ charts/templates/2173.js
@@ -13,13 +13,13 @@
 	"labels": {
 		"format": "{value:,.0f}",
 		"formatter": function(){
-        return Highcharts.numberFormat((this.value*100),0)+'%'; 
+        return Highcharts.numberFormat((this.value),0)+''; 
       },
 	}
   },	
   tooltip: {
   "pointFormatter": function(){
-      return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y*100),1) + '% </b><br/><b>' 
+      return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y),0) + ' </b><br/><b>' 
     },
   },
  "series": [
@@ -30,8 +30,6 @@
  	{"color": "#FF8028","index": 1}, /*BL*/
 	{"color": "#A8C3CA", "index": 1}, /*AG*/	  
 	{"color": "#008AC3", "index": 2}, /*ZH*/
-	{"color": "#990300", "index": 3}, /*CH*/
- 
 	  ],
   "legend": {
     "enabled": true,
