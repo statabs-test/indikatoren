2273011eb670fd7a59e495b59ea879ccee3f5e0f
Jonas Bieri <jonas.bieri@gmail.com>
2018-03-28 13:31:19 +0000

    Add a decimal in dataLabels in 6027

--- charts/templates/6027.js
+++ charts/templates/6027.js
@@ -13,7 +13,7 @@
     "min": 0
   },
   "xAxis": {
-    "tickInterval": 5
+    "tickInterval": 5,
   },
   "series": [
     {
@@ -32,16 +32,21 @@
   "plotOptions": {
     "series": {
       "dataLabels": {
-        "enabled": true,
-          //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
-          formatter: function() {
-            var first = this.series.data[0];
-            var last  = this.series.data[this.series.data.length - 1];
-            if (this.point.y === first.y || this.point.y === last.y ) {
-              return Highcharts.numberFormat(this.point.y, 0, ",", " ");
-            }
-            return "";
+        overflow: 'none',
+        crop: false,
+        enabled: true,
+        style: {
+          textOverflow: 'none'
+        },
+        //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
+        formatter: function() {
+          var first = this.series.data[0];
+          var last  = this.series.data[this.series.data.length - 1];
+          if (this.point.y === first.y || this.point.y === last.y ) {
+            return Highcharts.numberFormat(this.point.y, 1, ",", " ");
           }
+          return "";
+        }
       }
     }
   }
