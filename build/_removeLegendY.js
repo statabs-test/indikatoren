9b57bbdc287319b846856968ca199cc0aa18003a
Jonas Bieri <jonas.bieri@gmail.com>
2018-04-16 12:00:04 +0000

    Enhance script

--- build/_removeLegendY.js
+++ build/_removeLegendY.js
@@ -1,12 +1,7 @@
 var fs = require('fs');
 var pathBase = "charts/templates/";
 
-var serialize = require('serialize-javascript');
-
 fs.readdirSync(pathBase).forEach(file => {
-    var id = file.substr(0,4);
-    var idText = padLeft(id, 4);
-    var path = 'charts/configs/portal/' + idText + '.json';
     //try{
         var configFileContents = fs.readFileSync(path, 'utf8');
         var config = deserialize(configFileContents);
@@ -16,7 +11,7 @@ fs.readdirSync(pathBase).forEach(file => {
             var count = (configFileContents.match(/y:/g) || []).length;
             if (count == 1){
                 var changedFile = configFileContents.replace(/y:/g, "//y:");
-                fs.writeFileSync(path, changedFile);
+                fs.writeFileSync(pathBase + file, changedFile);
             }
         }
     /*        
