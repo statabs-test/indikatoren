7c7e3f0a64f824a99d5ae4125e5fdc0c7cda03c6
Jonas Bieri <jonas.bieri@gmail.com>
2018-04-16 12:13:12 +0000

    Enhance script

--- build/_removeLegendY.js
+++ build/_removeLegendY.js
@@ -2,34 +2,24 @@ var fs = require('fs');
 var pathBase = "charts/templates/";
 
 fs.readdirSync(pathBase).forEach(file => {
-    //try{
-        var configFileContents = fs.readFileSync(path, 'utf8');
-        var config = deserialize(configFileContents);
+    try{
+        console.log('checking file ' + file + '...');
+        var configFileContents = fs.readFileSync(pathBase + file, 'utf8');
+        var config = eval(configFileContents);
         //check if legend.y is defined
-        if (config["legend"]["y"]){
+        if (config["legend"] && config["legend"]["y"]){
             //only change file if only one occurence of 'y:' is found
             var count = (configFileContents.match(/y:/g) || []).length;
-            if (count == 1){
-                var changedFile = configFileContents.replace(/y:/g, "//y:");
+            //make sure we don't replace point.y and other cases
+            var pointYCount = (configFileContents.match(/point.y:/g) || []).length;
+            if (count == 1 && pointYCount == 0){
+                console.log('only 1 occurence of "y:" found, commenting out...');
+                var changedFile = configFileContents.replace(/y:/g, "//y:").replace(/"y":/g, "//y:").replace(/'y':/g, "//y:");
                 fs.writeFileSync(pathBase + file, changedFile);
             }
         }
-    /*        
     }
     catch (error){
-        //silently ignore errors
-        //console.log(error);
+        console.log(error);
     }
-    */
 });
-
-
-//https://stackoverflow.com/a/5367656
-function padLeft(nr, n, str){
-    return Array(n-String(nr).length+1).join(str||'0')+nr;
-}
-
-//https://github.com/yahoo/serialize-javascript
-function deserialize(serializedJavascript){
-  return eval('(' + serializedJavascript + ')');
-}
