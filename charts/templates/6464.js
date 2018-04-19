b4f4f6eac5c5487a3e0156ce2fb8ea4db69aacd6
Jonas Bieri <jonas.bieri@gmail.com>
2018-04-19 12:25:14 +0000

    Optimize charts

--- charts/templates/6464.js
+++ charts/templates/6464.js
@@ -40,9 +40,6 @@
     "layout": "horizontal",
     "verticalAlign": "top",
     "align": "left",
-    //"x": 25,
-	  //"y": 55,  
-	  "itemWidth": 70,
     "itemStyle": {
       "fontWeight": "normal"
     },
