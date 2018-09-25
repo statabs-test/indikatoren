4b9280aa54d039ab6bb5bd4d42042af6545aa389
Kevin Zaugg <kevin.zaugg@bs.ch>
2018-09-25 12:44:22 +0000

    update 6455 and 6531

--- charts/templates/6455.js
+++ charts/templates/6455.js
@@ -17,9 +17,9 @@
     },
   }, 
  "series": [
-    {"color": "#474747"}, /*BL*/
- 	{"color": "#923F8D"}, /*AG*/
-	{"color": "#008AC3"}, /*ZH*/
+    {"color": "#008AC3"}, /*Total*/
+ 	{"color": "#B00000"}, /*Frauen*/
+	{"color": "#474747"}, /*Männer*/
 	  ],
   "legend": {
     "enabled": true,
