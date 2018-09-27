59966798443a5a8058518d31f3c62e2d749dbbfc
JE1982 <jonas.eckenfels@bs.ch>
2018-09-27 12:38:44 +0000

    Correct js 6982, 6983

--- charts/templates/6982.js
+++ charts/templates/6982.js
@@ -19,13 +19,14 @@
   	}
   },
  "series": [
-  {"color": "#2B0099", legendIndex: 6, index: 6},
-  {"color": "#008AC3", legendIndex: 5, index: 5},
-  {"color": "#A0BEC8", legendIndex: 4, index: 4},
-  {"color": "#007A2F", legendIndex: 3, index: 3},
-  {"color": "#68AB2B", legendIndex: 2, index: 2},
-  {"color": "#73B97C", legendIndex: 1, index: 1},
-  {"color": "#D7E8D2", legendIndex: 0, index: 0}
+  {"color": "#2B0099", legendIndex: 7, index: 7},
+  {"color": "#008AC3", legendIndex: 6, index: 6},
+  {"color": "#A0BEC8", legendIndex: 5, index: 5},
+  {"color": "#007A2F", legendIndex: 4, index: 4},
+  {"color": "#68AB2B", legendIndex: 3, index: 3},
+  {"color": "#73B97C", legendIndex: 2, index: 2},
+  {"color": "#D7E8D2", legendIndex: 1, index: 1},
+  {"color": "#FFDA80", legendIndex: 0, index: 0} 
      ],
   "legend": { 
     "enabled": true,
