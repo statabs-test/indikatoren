39456c141fc5112f1b6bff6643de59e152ceb358
Kevin Zaugg <kevin.zaugg@bs.ch>
2018-10-12 15:00:31 +0000

    change 2528

--- charts/templates/2528.js
+++ charts/templates/2528.js
@@ -1,7 +1,11 @@
 (function(){
     return {
  "xAxis": {
-    "tickInterval": 1,
+   "tickPositions": [2005,2008,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
+    //"tickInterval": 1
+   	
+   	
+
   },
   "yAxis": {
 	"labels": {
@@ -32,7 +36,7 @@
   "plotOptions": {
     "line": {
       "marker":{
-        "enabled": false,
+        "enabled": true,
         "symbol": "circle",
       } 
     },
