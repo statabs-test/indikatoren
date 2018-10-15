cdbbb928f10f34be41b0c53d021bb96587bcb9d9
Kevin Zaugg <kevin.zaugg@bs.ch>
2018-10-15 08:55:01 +0000

    change 2529, 2547, 6456 and 6457

--- charts/templates/2529.js
+++ charts/templates/2529.js
@@ -1,7 +1,8 @@
 (function(){
     return {
  "xAxis": {
-    "tickInterval": 1,
+    //"tickInterval": 1,
+    "tickPositions": [2005,2008,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021]
   },
   "yAxis": {
 	"labels": {
