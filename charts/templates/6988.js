0a6a03f201ccfa046ce5016626d76994e364362d
Jonas Bieri <jonas.bieri@gmail.com>
2018-12-12 11:49:39 +0000

    Add own template for 6988

---
+++ charts/templates/6988.js
@@ -0,0 +1,65 @@
+/* 
+global Highcharts
+*/
+
+
+(function(){
+    return {
+      data: {
+        parsed: function(columns){ 
+          //define which wohnviertel to display
+          var filterValue = this.chartOptions.customFunctions.filter;
+          
+          //define which column (zero-based index) should be filtered 
+          var filterColumnIndex = 4;
+          //go backwards through the column with the values to filter (because we remove values at the end of the array and want to retain the index)
+          for (var i = columns[0].length; i > 0; i--){
+            if (columns[filterColumnIndex][i] != filterValue){
+              //go through all columns and remove the value at the current position
+              for (var j=0; j < columns.length; j++){
+                columns[j].splice(i, 1);
+              }
+            }
+          }
+          //Create a new Series named like the current Wohnviertel, with its id as content
+          //columns[1][0] = columns[2][1];
+        },
+        seriesMapping: [
+          {x: 2, Wohnviertel: 5, y: 3}
+        ]
+      },
+      tooltip: {
+        pointFormatter: function(){
+          //var pcnt = (this.y / this.series.data.map(p => p.y).reduce((a, b) => a + b, 0)) * 100;
+          var anteilInWv = (this.y / this.series.data
+            .map(function(p){return p.y;})
+            .reduce(function(accumulator, currentValue){return accumulator + currentValue;})) * 100;
+          return '<span style="color:' + this.color + '">●</span> ' + this.Wohnviertel + ': <b>' + Highcharts.numberFormat(this.y, 0)  + '</b> '
+            + '(' + Highcharts.numberFormat(anteilInWv, 1) + '%) <br/>';
+        },
+      },
+      yAxis: {
+        labels: {
+          format: "{value::,.f}",
+        }
+      },
+      xAxis: {
+        reversed: false,
+      },
+      legend: {
+        enabled: false
+      },
+      series: [
+        {
+        color: "rgb(37, 99, 112)",
+        }
+      ],
+      chart: {
+        type: "bar", 
+        marginRight: 15,
+      }, 
+     customFunctions: {
+       filter: 1
+     }
+    };
+}());
