5e60f171abb78f134fc995a244d645c1759a93f6
Kevin Zaugg <kevin.zaugg@bs.ch>
2019-01-30 16:04:16 +0000

    Optimize 2461

--- charts/templates/2461.js
+++ charts/templates/2461.js
@@ -23,11 +23,8 @@
     },
   },
  "series": [
- 	 {"color": "#6F6F6F", "index": 0,	
- 	 "marker": {
-        "enabled": true
-    }}, /*BS*/
- 	{"color": "#FF8028","index": 1}, /*BL*/
+ 	{"color": "#6F6F6F", "index": 0}, /*BS*/
+ 	{"color": "#FF8028", "index": 1}, /*BL*/
 	{"color": "#A8C3CA", "index": 1}, /*AG*/	  
 	{"color": "#008AC3", "index": 2}, /*ZH*/
 	  ],
