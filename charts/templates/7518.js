4cb9ac9208071f25613413f9c5c82b05f0a585ce
Kevin Zaugg <kevin.zaugg@bs.ch>
2019-02-01 10:32:33 +0000

    Optimize 7518

--- charts/templates/7518.js
+++ charts/templates/7518.js
@@ -44,7 +44,7 @@
     "inverted": false
   },
   "tooltip": {
-  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
+  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y} </span> ({point.percentage:,.1f}%) </b><br/>',
   "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
    "shared": true
   },
