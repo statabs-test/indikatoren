427a91e1621452b326694f31a92ada6eaeb2eb2b
Nana Mittelmaier <nana.mittelmaier@bs.ch>
2019-02-01 08:18:53 +0000

    test chart 7510

---
+++ charts/templates/7510.js
@@ -0,0 +1,59 @@
+
+/*global Highcharts*/
+(function(){
+    return {
+        "plotOptions": {
+            "series": {
+                "stacking": null,
+				pointPadding: 0, 
+				groupPadding: 0.1, 
+				borderWidth: 0
+
+            }
+        },
+        "chart": {      
+            "type": "column"
+        },
+        "series": [
+            {"color": "#083038"},
+            {"color": "#008AC3"},
+            {"color": "#71A3B5"}
+        ],
+  "xAxis": {
+    type: 'datetime',
+    tickInterval: 1000 * 60 * 60 * 24 * 30,
+    labels: { 
+      formatter: function() {
+        if (this.isFirst || this.isLast) {
+          return Highcharts.dateFormat('%b %y', this.value);
+        } else {
+          return Highcharts.dateFormat('%b', this.value);
+        }
+      }
+      },
+  "yAxis": {
+	"labels": {
+		"format": "{value:,.0f}",
+		"formatter": function(){
+        return Highcharts.numberFormat((this.value*100),0)+'%'; 
+      },
+	}
+  },	
+  "legend": {
+    "enabled": true,
+    //"y": 45,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+        tooltip: {
+            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>'
+      }
+	}};
+}());
+
+ 
--- metadata/all/indikatoren.js
+++ metadata/all/indikatoren.js
@@ -36184,6 +36184,46 @@ var indikatoren = [
 		"id": 7432
 	},
 	{
+		"title": "Arbeitslosenquote nach Alter",
+		"subtitle": "Basel-Stadt",
+		"kuerzel": "I.03.4.07510",
+		"kuerzelKunde": "",
+		"description": "Arbeitslosenquote nach Alter",
+		"thema": "03 Erwerbsleben",
+		"unterthema": "Arbeitslose",
+		"kennzahlenset": "Monatsindikatoren",
+		"raeumlicheGliederung": [
+			"Kanton"
+		],
+		"aktualisierungsdatum": "",
+		"stufe1": "",
+		"stufe2": "",
+		"stufe3": "",
+		"stufe4": "",
+		"stufe5": "",
+		"lesehilfe": "Die Arbeitslosenquote der Personen zwischen 15 und 24 Jahren ist in der Regel höher als die Quote in der Gesamtbevölkerung und zeigt ausgeprägte Schwankungen.",
+		"erlaeuterungen": "Als Arbeitslose gelten Personen, welche bei einem Regionalen Arbeitsvermittlungszentrum (RAV) gemeldet sind, keine Stelle haben und sofort vermittelbar sind. Die Arbeitslosenquote berechnet sich mit der Anzahl Arbeitslosen geteilt durch die Erwerbspersonen.",
+		"template": "template001",
+		"darstellungsart": "Grafik",
+		"printformat": "",
+		"visibleInPortal": true,
+		"orderKey": "_000",
+		"parentId": null,
+		"children": [],
+		"renderLink": [],
+		"quellenangabe": [
+			"Staatssekretariat für Wirtschaft SECO, Arbeitslosenstatistik"
+		],
+		"datenInChartIntegriert": false,
+		"externalLinks": [
+			""
+		],
+		"data-id": 7510,
+		"chart-id": 7510,
+		"filter": "",
+		"id": 7510
+	},
+	{
 		"title": "Test Daten",
 		"subtitle": "Test",
 		"kuerzel": "",
