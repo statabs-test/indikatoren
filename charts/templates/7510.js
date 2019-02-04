01ef385ea841486f19f688612a729dc9704e863f
grilli73 <nathalie.grillon@gmail.com>
2019-02-04 09:31:54 +0000

    Fix tooltip in 7510

--- charts/templates/7510.js
+++ charts/templates/7510.js
@@ -21,40 +21,40 @@
             {"color": "#008AC3"},
             {"color": "#71A3B5"}
         ],
-  "xAxis": {
-    type: 'datetime',
-    tickInterval: 1000 * 60 * 60 * 24 * 30,
-    labels: { 
-      formatter: function() {
-        if (this.isFirst || this.isLast) {
-          return Highcharts.dateFormat('%b %y', this.value);
-        } else {
-          return Highcharts.dateFormat('%b', this.value);
-        }
-      }
-      },
-  "yAxis": {
-	"labels": {
-		"format": "{value:,.0f}",
-		"formatter": function(){
-        return Highcharts.numberFormat((this.value*100),0)+'%'; 
-      },
-	}
-  },	
-  "legend": {
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-         },
-
+        "xAxis": {
+          type: 'datetime',
+          tickInterval: 1000 * 60 * 60 * 24 * 30,
+          labels: { 
+            formatter: function() {
+              if (this.isFirst || this.isLast) {
+                return Highcharts.dateFormat('%b %y', this.value);
+              } else {
+                return Highcharts.dateFormat('%b', this.value);
+              }
+            },
+          },
+        },
+        "yAxis": {
+        	"labels": {
+        		"format": "{value:,.0f}",
+        		"formatter": function(){
+                return Highcharts.numberFormat((this.value*100),0)+'%'; 
+              },
+      	  }
+        },	
+        "legend": {
+          "enabled": true,
+          "layout": "horizontal",
+          "verticalAlign": "top",
+          "align": "left",
+          "itemStyle": {
+            "fontWeight": "normal"
+          }
+        },
         tooltip: {
-            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>'
-      }
-	}};
+          "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.1f}%</b><br/>'
+        }
+	  };
 }());
 
  
