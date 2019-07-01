1f1756ed0c9ca1aa3fce666648c35a37b693ac9c
Zaugg <ssszak@bs.ch>
2019-07-01 09:16:14 +0200

    Create Charts 7724 to 7741

---
+++ charts/templates/7727.js
@@ -0,0 +1,86 @@
+(function(){
+    return {
+  "xAxis": {
+    //"tickInterval": 1
+  },
+  "yAxis": {
+    "min": 0, 
+	"labels": {
+		"format": "{value:,.1f}"
+	}
+  },	
+  "tooltip": {
+    "shared": false, 
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+  },
+  "series": [
+  	   {"color": "#007A2F",
+      "marker": {
+        "enabled": false
+      }    
+    }, /* black */
+    {"color": "#923F8D",
+      "marker": {
+        "enabled": false
+      }    
+    }, /* violett */
+	{"color": "#DC440E",
+      "marker": {
+        "enabled": false
+      }    
+    }, /* rot */
+    {"color": "#FFDA80",
+      "marker": {
+        "enabled": false
+      }    
+    },/* gelb  */
+    {"color": "#007A2F",
+    dashStyle: 'Dash',
+    "marker": {
+      "enabled": false
+    }    
+  }, /* black */
+  {"color": "#923F8D",
+  dashStyle: 'Dash',
+    "marker": {
+      "enabled": false
+    }    
+  }, /* violett */
+  {"color": "#DC440E",
+  dashStyle: 'Dash',
+    "marker": {
+      "enabled": false
+    }    
+  }, /* rot */
+  {"color": "#FFDA80",
+  dashStyle: 'Dash',
+    "marker": {
+      "enabled": false
+    }    
+  },/* gelb  */
+  ],
+  "legend": {
+    "enabled": true,
+    //"x": 50,
+    //"y": 25,
+    "itemWidth": 180,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  "plotOptions": {
+    "line": {
+	  "connectNulls": true,
+      "marker":{
+        "enabled": false,
+        "symbol": "circle",
+      }
+    }
+  }
+}
+}());
+ 
