1e75212812ed2622ade482fa8acc625f8eab8f41
bl99 <ssslib@bs.ch>
2019-09-13 13:02:26 +0200

    test 7775

--- charts/templates/7775.js
+++ charts/templates/7775.js
@@ -1,22 +1,34 @@
-(function(){
-    return {
-   "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}",
-    }
-  },
-  "xAxis": {
-	"tickInterval": 2,
-  },
-  "series": [
-    {     
-      "color": "#8b2223"
-    }
-  ],
-  "tooltip": {
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
-    "shared": false
-  },
-};
+(function () {
+  return {
+    "yAxis": {
+      "labels": {
+        "format": "{value:,.1f}",
+      },
+      min: null
+    },
+    "xAxis": {
+      "tickInterval": 2,
+    },
+    data: {
+      seriesMapping: [
+        { x: 0, y: 8 },
+        { x: 0, y: 9 },
+      ]
+    },
+    "series": [
+      {
+        "color": "#8b2223",
+        type: "column"
+      },
+      {
+        "color": "#DC440E",
+        type: "column"
+      }
+    ],
+    "tooltip": {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
+      "shared": false
+    },
+  };
 }());
 
