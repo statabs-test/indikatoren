80f78bc778335b422dd19174b34839c6c92985fa
bl99 <ssslib@bs.ch>
2019-10-08 10:27:09 +0200

    Push again

--- charts/templates/4914.js
+++ charts/templates/4914.js
@@ -1,69 +1,69 @@
-(function(){
-    return {
-  "yAxis": [{
-    "title": {
-      "text": null
-    },
-    "labels": {
-      "format": "{value:,.0f}",
-      "style": {
-        "color": "#000000"
-      }
-    },
-    "min": 0,
-    },
-  {
-    "title": {
-      "text": null
-    },
-    "labels": {
-      "format": "{value:,.0f}",
-      "style": {
-        "color": "#000000"
-      }
-    },
-    "min": 0,
-    "opposite": true
-  }
-  ],
-  "xAxis": {
-    "tickInterval": 2,
-    "type": "category"
-  },
-  "legend": {
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },
-  "plotOptions": {
-    "line": {
-	  "connectNulls": true,
-      "marker":{
-        "enabled": false,
-        "symbol": "circle",
-      }
-    }
-  },
-  "series": [
-    {
-      "color": "#cd9c00",
-      "index": 0,
-      "type":"column",
-      "yAxis":0, 
-      //"pointWidth": "10"
+(function () {
+  return {
+    "yAxis": [{
+      "title": {
+        "text": null
+      },
+      "labels": {
+        "format": "{value:,.0f}",
+        "style": {
+          "color": "#000000"
+        }
+      },
+      "min": 0,
     },
     {
-      "color": "#73b97c",
-      "index": 1,
-      "yAxis": 1
+      "title": {
+        "text": null
+      },
+      "labels": {
+        "format": "{value:,.0f}",
+        "style": {
+          "color": "#000000"
+        }
+      },
+      "min": 0,
+      "opposite": true
     }
-  ],  
-  "tooltip": {
-    "shared": false
-  },
-	};
+    ],
+    "xAxis": {
+      "tickInterval": 2,
+      "type": "category"
+    },
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    "plotOptions": {
+      "line": {
+        "connectNulls": true,
+        "marker": {
+          "enabled": false,
+          "symbol": "circle",
+        }
+      }
+    },
+    "series": [
+      {
+        "color": "#cd9c00",
+        "index": 0,
+        "type": "column",
+        "yAxis": 0,
+        //"pointWidth": "10"
+      },
+      {
+        "color": "#73b97c",
+        "index": 1,
+        "yAxis": 1
+      }
+    ],
+    "tooltip": {
+      "shared": false
+    },
+  };
 }());
