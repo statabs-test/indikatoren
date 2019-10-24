2f7a1e35c7991f3a6a5d5bcdb299682fa0913b39
bl99 <ssslib@bs.ch>
2019-10-24 09:14:26 +0200

    update 4374

--- charts/templates/4374.js
+++ charts/templates/4374.js
@@ -1,41 +1,21 @@
-(function(){
+(function () {
   return {
-"plotOptions": {
-  "series": {
-    "dataLabels": {
-      "enabled": false,
-        //display label at first or last point: https://gist.github.com/jeremywrowe/3506869
-     
-          }
-         
-       
-    
+    "chart": {
+      "marginBottom": 65,
+      "marginTop": 75,
+      "type": "column"
+    },
+    "xAxis": {
+      "type": "category",
+      "tickInterval": 1
+    },
+    "legend": {
+      "enabled": false
+    },
+    "series": [
+      {
+        "color": "#ff8028"
+      }
+    ]
   }
-},
-"yAxis": {
-  "labels": {
-    "format": "{value:,.0f}",
-  },
-},
-"xAxis": {
-  "type": "category",
-  "tickInterval": 1
-},
-"legend": {
-  "enabled": false
-},
-"series": [
-  {
-  "color": "#ff8028"
-  }
-],
-"tooltip": {
-  "shared": false
-},
-"chart": {
-  "marginBottom": 65,
-  "marginTop": 75,
-  "type": "column"
-}
-}
 }());
