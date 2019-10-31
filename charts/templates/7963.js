85eb4f3643f9b8e61a3fb8a769fae4d7083ca5dc
Alfany Fernandez <sssmoj@bs.ch>
2019-10-31 11:00:21 +0100

    prepare ome more indicators

---
+++ charts/templates/7963.js
@@ -0,0 +1,44 @@
+(function(){
+    return {
+    "xAxis": {
+        "type": "category", 
+        "labels": {
+            formatter: undefined/*,
+            align: "left"*/
+        }
+    },
+    "yAxis": {
+        "tickInterval": null,
+        "reversedStacks": false
+    },   
+    "series": [
+        {
+        "color": "#689199"
+        },
+        {
+        "color": "#661200"
+        },
+        {
+        "color": "#8AB77D"
+        },      
+    ],        
+    "legend": {
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5,     
+        "align": "left",
+        //"x": 145,
+        //"y": 70,
+        "itemWidth": 250,
+        "itemStyle": {
+            "fontWeight": "normal"
+        }
+    },      
+    "chart":
+    {
+        height: 500,
+        "inverted": true
+    }
+	}
+}());
+ 
---
+++ charts/templates/7968.js
@@ -0,0 +1,54 @@
+(function(){
+    return {
+    "xAxis": {
+        "type": "category", 
+        "labels": {
+            formatter: undefined/*,
+            align: "left"*/
+        }
+    },
+    "yAxis": {
+        "tickInterval": null,
+        "reversedStacks": false,
+        labels:
+    {
+        format: "{value:,.0f}"
+    }
+    },   
+    "series": [
+        {
+        "color": "#689199"
+        },
+        {
+        "color": "#661200"
+        },
+        {
+        "color": "#8AB77D"
+        },      
+    ],        
+    "legend": {
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 5,     
+        "align": "left",
+        //"x": 145,
+        //"y": 70,
+        "itemWidth": 250,
+        "itemStyle": {
+            "fontWeight": "normal"
+        }
+    },      
+    "chart":
+    {
+        height: 500,
+        "inverted": true
+    },
+    plotOptions:
+    {
+        series:{
+            stacking: "normal"
+        }
+    },
+	}
+}());
+ 
