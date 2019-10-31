c15c9f228b4b804acb1f6131ef1bfd1f673f9b30
bl99 <ssslib@bs.ch>
2019-10-31 15:56:57 +0100

    update templates template001 & print, move function out of chart-object

--- charts/templates/template001_print.js
+++ charts/templates/template001_print.js
@@ -1,135 +1,129 @@
-(function(){
+// call this in chart.events.load: function () {onLoadFunction(this);}
+var onLoadFunction = function (chart) {
+
+    //remove default function for credits-link
+    chart.credits.element.onclick = function () { };
+
+    //square legends must be placed 3 pixels more to the left that lines, don't know why
+    var squareLegendX = (chart.legend.allItems != undefined && chart.legend.allItems[0].options.type == 'column' ? 3 : 0);
+
+    //for top-left legends with no x defined: move legend to x position of first yAxis
+    if (chart['legend']['options']['align'] == 'left' && chart['legend']['options']['verticalAlign'] == 'top' && chart['legend']['options']['x'] == 0) {
+        chart.update({
+            legend: {
+                x: chart.yAxis[0].left - chart.spacingBox.x - chart.legend.padding// - squareLegendX
+            }
+        });
+    }
+};
+
+
+(function () {
     return {
-    "chart": {
-        "borderColor": "#fbfbfb",
-        "backgroundColor": "#fbfbfb",
- 		"width": 320,
-    	"height": 208,  
-    	spacing: [2,2,2,2], /*top, right, bottom and left */
-        "style": {
-            "fontFamily": "Arial"
-        },
-        "zoomType": "xy",
-        "events":{
-                "load": function() {
-                    this.credits.element.onclick = function() {};
-                    
-                    //square legends must be placed 3 pixels more to the left that lines, don't know why
-                    var squareLegendX = (this['options']['chart']['type'] == 'line'  ? 0 : 3);
-                    
-                    //for top-left legends with no x defined: move legend to x position of first yAxis
-                    if (this['legend']['options']['align'] == 'left' && this['legend']['options']['verticalAlign'] == 'top' && this['legend']['options']['x'] == 0){
-                      this.update(
-                        {
-                          legend: {
-                            x: this.yAxis[0].left - this.spacingBox.x - this.legend.padding - squareLegendX
-                          }
-                        }
-                      );
-                    }              
-                    
-                }
-        }
-    },    
-    "plotOptions": {
-        "series": {
-            borderWidth: 0,
-            "dataLabels": {
-                "style": {
-                "fontSize": "10px",
-                fontFamily: "Arial",
-                "fontWeight": "normal", 
-                color: 'black',
-                }
-            }
-        }
-    },
-    "title": {
-        "style": {
-        "fontSize": "10px",
-        "fontWeight": "bold",
-        "color": "#000000"
-        },
-        "align": "left"
-    },
-    "subtitle": {
-        "style": {
-        "fontSize": "10px",
-        "fontWeight": "normal",
-        "fontFamily": "Arial",
-        "color": "#000000"
-        },
-        "text": "",
-        "align": "left"
-    },
-    "xAxis": {
-        lineColor: '#B9CFD7', 
-        lineWidth: 0.5,
-        "uniqueNames": true,
-        "title": {
-            "style": {
-                "color": "#000000"
-            }
-        },
-        "labels": {
-            "style": {
-            	fontSize: '10px',
-                color: "#000000",
-            }
-        },
-        "tickLength": 0,
-    }, 
-    "yAxis": {
-        gridLineColor: '#B9CFD7', 
-        gridLineWidth: 0.5,
-        lineColor: '#B9CFD7', 
-        "title": {
-            "style": {
-                "color": "#000000",
-                "fontSize": 10
+        chart: {
+            borderColor: "#fbfbfb",
+            backgroundColor: "#fbfbfb",
+            width: 320,
+            height: 208,
+            spacing: [2, 2, 2, 2], /*top, right, bottom and left */
+            style: {
+                fontFamily: "Arial"
             },
-            "text": ''
+            events: {
+                load: function () {
+                    onLoadFunction(this);
+                }
+            }
         },
-        "labels": {
-            y: 3, 
-            "style": {
-            	fontSize: '10px',
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    style: {
+                        fontSize: "10px",
+                        fontFamily: "Arial",
+                        fontWeight: "normal",
+                        color: "#000000"
+                    }
+                }
+            }
+        },
+        title: {
+            align: "left",
+            style: {
+                fontSize: "10px",
+                fontWeight: "bold",
                 color: "#000000"
             }
-        }
-    },    
-	"navigation": {
-		"menuItemStyle": {
-			"fontFamily": Highcharts.SVGRenderer.prototype.getStyle().fontFamily,
-			"padding": '2px 10px'
-		}
-	},
-    "credits": {
-        "enabled": true,
-        "style": {
-            "color": "#000000",
-            "fontSize": "10",
-            "cursor": "default"
         },
-        "position": {
-            "align": "left",
-            "verticalAlign": "bottom",
-            "x": 10
-        }
-    },
-    "legend": {
-    	padding: 0,
-        symbolRadius: 0,
-        itemMarginBottom: 2,
-        itemStyle:{
-            fontSize: "10px"
-        }
-    },
-    dataLabels: {
+        subtitle: {
+            text: "",
+            align: "left",
+            style: {
+                fontSize: "10px",
+                fontWeight: "normal",
+                fontFamily: "Arial",
+                color: "#000000"
+            }
+        },
+        xAxis: {
+            lineColor: '#B9CFD7',
+            lineWidth: 0.5,
+            tickLength: 0,
+            uniqueNames: true,
+            title: {
+                style: {
+                    color: "#000000"
+                }
+            },
+            labels: {
+                style: {
+                    fontSize: "10px",
+                    color: "#000000"
+                }
+            }
+        },
+        yAxis: {
+            gridLineColor: '#B9CFD7',
+            gridLineWidth: 0.5,
+            lineColor: '#B9CFD7',
+            title: {
+                text: '',
+                style: {
+                    fontSize: "10px",
+                    color: "#000000"
+                }
+            },
+            labels: {
+                y: 3,
+                style: {
+                    fontSize: "10px",
+                    color: "#000000"
+                }
+            }
+        },
+        credits: {
+            enabled: true,
+            position: {}
+        },
+        legend: {
+            padding: 0,
+            symbolRadius: 0,
+            itemMarginBottom: 2,
+            itemStyle: {
+                fontSize: "10px"
+            }
+        },
+        dataLabels: {
             enabled: false,
             style: {
                 fontSize: '10px',
             }
+        },
+        navigation: {
+            buttonOptions: {
+                enabled: false
+            }
         }
-	};
+    };
 }());
