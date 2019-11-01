ae496a7e043e354be8adae66b056fe4b9a3741e8
Alfany Fernandez <sssmoj@bs.ch>
2019-11-01 15:50:27 +0100

    Some more changes and new charts

---
+++ charts/templates/7964.js
@@ -0,0 +1,49 @@
+(function(){
+    return {
+    xAxis: {
+        type: "category", 
+        labels: {
+            formatter: undefined/*,
+            align: "left"*/
+        }
+    },
+    yAxis: {
+        tickInterval: null,
+        reversedStacks: false
+    },   
+    series: [
+        {
+        color: "#689199"
+        },
+        {
+        color: "#661200"
+        }      
+    ],        
+    legend: {
+        layout: "horizontal",
+        verticalAlign: "top",
+        itemMarginBottom: 5,     
+        align: "left",
+        //"x": 145,
+        //"y": 70,
+        itemWidth: 250,
+        itemStyle: {
+            fontWeight: "normal"
+        }
+    },      
+    chart:
+    {
+        height: 450,
+        inverted: true
+    },
+    plotOptions: {
+        series: {
+          groupPadding: 0.1
+        }
+      },
+      tooltip: {
+        footerFormat: '\u25CF Total: <b>{point.total:,.0f}</b><br/>'
+     },
+	}
+}());
+ 
--- charts/templates/7968.js
+++ charts/templates/7968.js
@@ -1,54 +1,56 @@
 (function(){
     return {
-    "xAxis": {
-        "type": "category", 
-        "labels": {
+    xAxis: {
+        type: "category", 
+        labels: {
             formatter: undefined/*,
             align: "left"*/
         }
     },
-    "yAxis": {
-        "tickInterval": null,
-        "reversedStacks": false,
+    yAxis: {
+        tickInterval: null,
+        reversedStacks: false,
         labels:
     {
-        format: "{value:,.0f}"
+        format: "{value:,.0f}%"
     }
     },   
-    "series": [
+    series: [
         {
-        "color": "#689199"
+        color: "#689199"
         },
         {
-        "color": "#661200"
+        color: "#661200"
         },
         {
-        "color": "#8AB77D"
+        color: "#8AB77D"
         },      
     ],        
-    "legend": {
-        "layout": "horizontal",
-        "verticalAlign": "top",
-        "itemMarginBottom": 5,     
-        "align": "left",
+    legend: {
+        layout: "horizontal",
+        verticalAlign: "top",
+        itemMarginBottom: 5,     
+        align: "left",
         //"x": 145,
         //"y": 70,
-        "itemWidth": 250,
-        "itemStyle": {
-            "fontWeight": "normal"
+        itemWidth: 250,
+        itemStyle: {
+            fontWeight: "normal"
         }
     },      
-    "chart":
+    chart:
     {
         height: 500,
-        "inverted": true
+        inverted: true
     },
-    plotOptions:
-    {
-        series:{
-            stacking: "normal"
+    plotOptions: {
+        series: {
+          groupPadding: 0.1
         }
-    },
+      },
+    tooltip: {
+        footerFormat: '\u25CF Total: <b>{point.total:,.0f}</b><br/>'
+     },
 	}
 }());
  
