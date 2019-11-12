88c31f0e7ba8842b4c18644f3e6571e9e038cc69
bl99 <ssslib@bs.ch>
2019-11-12 11:54:19 +0100

    Init SMKK indicators: js copied from SMR, JSON

---
+++ charts/templates/8053.js
@@ -0,0 +1,52 @@
+(function () {
+  return {
+    "plotOptions": {
+      "series": {
+        "stacking": "percent"
+      },
+      borderwidth: 0
+    },
+    "xAxis": {
+      "type": "category"
+    },
+    "yAxis": {
+      "tickInterval": 10,
+      "labels": {
+        "format": "{value}%"
+      },
+      "min": 0
+    },
+    "tooltip": {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b> ({point.percentage:.0f}%)<br/>',
+      "shared": false
+    },
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "itemMarginBottom": 5,
+      "align": "left",
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    "series": [
+      {
+        "color": "#2f656b",
+        "index": 0
+      },
+      {
+        "color": "#73b97c",
+        "index": 1
+      },
+      {
+        "color": "#d7e8d2",
+        "index": 2
+      }
+    ],
+    "chart": {
+      "type": "column"
+    }
+  }
+}());
+
