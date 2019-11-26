f27366509db84b10c3112d801daec643a1d5640e
Alfany Fernandez <sssmoj@bs.ch>
2019-11-26 15:07:18 +0100

    Force last year in x to show

--- charts/templates/5186.js
+++ charts/templates/5186.js
@@ -1,9 +1,41 @@
 (function(){
     return {
   "xAxis": {
+    tickPositioner: function() 
+    {
+      var positions = [],
+      ext = this.getExtremes(),
+      xMax = Math.round(ext.max),
+      xMin = Math.round(ext.min)+1;
+      var xMin_even=Math.round(ext.min);
+      if(xMax % 2 == 1) 
+      {
+        for (var i = xMin; i < xMax; i++) 
+        {
+        if (i % 2 == 1) 
+        {
+          positions.push(i);
+        }
+      }
+      positions.push(xMax);
+      return positions;
+    } 
+    else if (xMax % 2 == 0)
+    {
+      for (var i = xMin_even; i < xMax; i++) 
+      {
+        if (i % 2 == 0) 
+        {
+          positions.push(i);
+        }
+      }
+      positions.push(xMax);
+      return positions;
+    }
+  },
     tickInterval: 1,
     labels: { 
-      step: 2,
+      //step: 2,
   		style: { 
   		  //	textOverflow: 'none' // prevents ellipsis
   	  }, 
