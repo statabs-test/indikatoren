f25dca1d5bcede996bc7c55f476d931f81f33c64
Robert Luginbühl <robert.luginbuehl@bs.ch>
2019-12-04 16:37:02 +0100

    Anpassungen für Jose

--- charts/templates/7954.js
+++ charts/templates/7954.js
@@ -1,30 +1,20 @@
-/*  global rheinData
-	global Highcharts
-	global geojson_wohnviertel
+/* 
+global Highcharts
+global geojson_UA_Gemeinden_100
+global rheinData
 */
 (function(){
     return {
         "tooltip": {
             "formatter": function(args){
-        		/*if (! this.point["Wohnviertel Id"]) {
-        		    //Rhein
-        			return '<span style="color:' + this.color + ';">\u25CF </span><span>' + this.series.name + '</span>';
-        		}
-                else {*/
-                    //Wohnviertel
-                    var this_point_index = this.series.data.indexOf(this.point);
-                    var other_series_index = this.series.index == 0 ? 1 : 0; // assuming 2 series
-                    var other_series = args.chart.series[other_series_index];
-                    var other_point = other_series.data[this_point_index];
-                    return '<span style="color:' + this.color + ';">\u25CF</span><span style="font-size: 0.85em;"> ' + this.series.name + ':</span><br/>' + 
-                        this.point.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.point.value),0) + ' Fr.</b><br/>' + 
-                        'Rang <b>' + other_point.value + '</b>';
-                //}
+				var this_point_index = this.series.data.indexOf(this.point);
+				return '<span style="color:' + this.color + ';">\u25CF</span><span style="font-size: 0.85em;"> ' + this.series.name + ':</span><br/>' + 
+					this.point.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.point.value),0) + ' Personen</b></b>';
             }
         },    	
 		"legend": {
 			"title": {
-				"text": "in 1000 Fr."
+				"text": "in 100 Personen pro Hektar"
 			}
 		},
 		"colorAxis": {
@@ -32,29 +22,25 @@
 			"maxColor": "#4f6e75",
 			"labels": {
 				"formatter": function () {
-					return Highcharts.numberFormat(this.value / 1000, 0); 
+					return Highcharts.numberFormat(this.value / 100, 0); 
 				}
 			}
 		},
         "data": {
 		    "seriesMapping": [
 		      {
-		      	x: 0, y: 1
-		      },
-		      {
-		      	//2nd series: use x values from column 2
-		      	x: 3
-		      }		      
+		      	x: 0, y: 2
+		      }	      
 		    ]
         },
 		"series": [
 			{
-				"name": "Wohnviertel", 
+				"name": "Bevölkerungsdichte", 
 				"animation": true,
-				"mapData": geojson_Trinational_Gemeinden, // geojson_wohnviertelEPSG2056,
+				"mapData": geojson_gemeinden,
 				"borderColor": "#fbfbfb",		
-				"joinBy": ['CODGEO', 'Codegeo'],
-				"keys": ['Codegeo', 'value'],
+				"keys": ['gemeindecode', 'value'],
+				"joinBy": ['CODGEO', 'gemeindecode'],
 				"states": {
 					"hover": {
 						"enabled": false,
