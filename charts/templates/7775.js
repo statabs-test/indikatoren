ecb179a4e77a4cd2d417e525276ed10740390062
bl99 <ssslib@bs.ch>
2019-12-09 18:34:33 +0100

    Update div. charts/maps in Richtplan-Set

--- charts/templates/7775.js
+++ charts/templates/7775.js
@@ -3,97 +3,104 @@
     chart: {
       zoomType: "x"
     },
-    "plotOptions": {
-      "series": {
-        pointPadding: 0.06,
-        groupPadding: 0.1,
-      }
-    },
+    /*    "plotOptions": {
+          "series": {
+            pointPadding: 0.06,
+            groupPadding: 0.1,
+          }
+        },
+    */
     subtitle: {
       useHTML: true
     },
     legend: {
       enabled: true,
       align: "left",
-      verticalAlign: "top"
+      verticalAlign: "top",
+      labelFormatter: function () {
+        return this.name.replace(' (Index)', '');
+      }
     },
-    yAxis: [
-      {
-        tickAmount: 6,
-        labels: {
-          format: "{value:,.0f}",
-        },
-        plotLines: [{
-          value: 0,
-          color: '#B9CFD7',
-          width: 2
-        }],
-        gridLineColor: '#B9CFD7',
-        gridLineWidth: 0.5,
-        "title": {
-          enabled: false
-        },
-        labels: {
-          style: {
-            color: "#000000"
-          }
+    yAxis:
+    {
+      // opposite: true,
+      //tickAmount: 6,
+      //min: 99.5,
+      //max: 101,
+      tickInterval: .1,
+      labels: {
+        format: "{value:,.2f}",
+        style: {
+          color: "black"
         }
       },
-      {
-        opposite: true,
-        tickAmount: 6,
-        //min: 99.75,
-        //max: 101,
-        labels: {
-          format: "{value:,.2f}",
-          style: {
-            color: "black"
-          }
-        },
-        plotLines: [{
-          value: 100,
-          color: '#B9CFD7',
-          width: 2
-        }],
-        gridLineColor: '#B9CFD7',
-        gridLineWidth: 0.5,
-        "title": {
-          enabled: false
-        },
-        labels: {
-          style: {
-            color: "#000000"
-          }
+      plotLines: [{
+        value: 100,
+        color: '#B9CFD7',
+        width: 2
+      }],
+      gridLineColor: '#B9CFD7',
+      gridLineWidth: 0.5,
+      "title": {
+        enabled: false
+      },
+      labels: {
+        style: {
+          color: "#000000"
         }
       }
-    ],
+    },
+    /*
+    {
+      tickAmount: 6,
+      labels: {
+        format: "{value:,.0f}",
+      },
+      plotLines: [{
+        value: 0,
+        color: '#B9CFD7',
+        width: 2
+      }],
+      gridLineColor: '#B9CFD7',
+      gridLineWidth: 0.5,
+      "title": {
+        enabled: false
+      },
+      labels: {
+        style: {
+          color: "#000000"
+        }
+      }
+    },
+  ],*/
     xAxis: {
       tickInterval: 1,
     },
     data: {
       seriesMapping: [
-        { x: 0, y: 8 },
-        { x: 0, y: 9 },
+        //{ x: 0, y: 8 },
+        //{ x: 0, y: 9 },
         { x: 0, y: 4 },
         { x: 0, y: 5 },
       ]
     },
     series: [
-      {
-        color: "#8b2223",
-        type: "column"
-      },
-      {
-        color: "#DC440E",
-        type: "column"
-      },
+      /*      {
+              color: "#8b2223",
+              type: "column"
+            },
+            {
+              color: "#DC440E",
+              type: "column"
+            },
+      */
       {
         color: "#7b1213",
         type: "line",
         marker: {
           enabled: false
         },
-        yAxis: 1
+        // yAxis: 1
       },
       {
         color: "#aC140E",
@@ -101,7 +108,7 @@
         marker: {
           enabled: false
         },
-        yAxis: 1
+        //yAxis: 1
       }
     ],
     tooltip: {
