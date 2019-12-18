a47bfe8537f81549edd5eefa02bd9fd6cc6b9bcc
Alfany Fernandez <sssmoj@bs.ch>
2019-12-18 08:54:23 +0100

    Corrections in 8285

--- charts/templates/8285.js
+++ charts/templates/8285.js
@@ -1,59 +1,65 @@
-/* 
-global Highcharts
-global geojson_wohnviertelEPSG2056 
+/*  
+	global Highcharts
+	global geojson_wohnviertelEPSG2056 
+	global $
 */
 (function(){
-   return {
+
+    return {
 		"legend": {
     		useHTML: false,
 			"title": {
-			"text": "Anteil 5-17-jährige",
-				style: {'fontWeight':' bold'} 
+				"text": null, 
+				style: {'fontWeight':' bold'}
 			},
 			"layout": "vertical",
 			//"verticalAlign": "middle",
 			"align": "right",
-			"x": -30,
-			//"y": -20,
+			"x": -120,//-140,
+			"y": -20,
 			itemMarginBottom: 2, 
 			symbolRadius: 0,
 			itemStyle: {
 				fontWeight: 'normal'
 				}
 		},
-         colorAxis: {
+		colorAxis: {
             dataClassColor: 'category',
                    dataClasses: [{
                	from: 0.0,
-                to:33.9,
+                to:39.9,
                 color: '#D3E2E4',
-                name:  "<span style='color: rgba(0,0,0,0)'>00,0 </span> < 34,0"
+                name:  "<span style='color: rgba(0,0,0,0)'>00,0% </span> < 40,0%"
             }, {
-                from: 34.0,
-                to: 63.9,
+                from: 40.0,
+                to: 69.9,
                 color: '#A8C3CA',
-                name: "34,0 − 63,9"
+                name: "40,0% − 69,9%"
             }, {
-                from: 64.0,
+                from: 70.0,
                 to: 79.9,
                  color: '#689199',
-                 name: "64,0 − 79,9"
+                 name: "70,0% − 79,9%"
             },{
                 from: 80.0,
                 to: 89.9,
                  color: '#246370',
-                 name: "80,0 − 89,9"
+                 name: "80,0% − 89,9%"
             },{
                 from: 90,
                 color: '#083038',
-                name:  "<span style='color: rgba(0,0,0,0)'>00,0 </span> ≥ 90,0"
+                name:  "<span style='color: rgba(0,0,0,0)'>00,0% </span> ≥ 90,0%"
             }], 
 		},
         "data": {
 		    "seriesMapping": [
 		      {
 		      	x: 0, y: 3
-		      }		      
+		      },
+		      {
+		      	//2nd series: use y values from column 3
+		      	y: 2
+		      }
 		    ]
         },
 		"series": [
@@ -62,29 +68,113 @@ global geojson_wohnviertelEPSG2056
 				"animation": true,
 				"mapData": geojson_wohnviertelEPSG2056_StadtBasel,
 				"borderColor": "#fbfbfb",		
-				"joinBy": ['TXT', 'Wohnviertel Id'],
-				"keys": ['Wohnviertel Id', 'value'],
+				"joinBy": ['TXT', 'Wohnviertel_Id'],
+				"keys": ['Wohnviertel_Id', 'value'],
 				"states": {
 					"hover": {
 						"enabled": false,
 						"borderColor": '#BADA55',
 						"brightness": 0
 					}
+				}, 
+				tooltip: {
+					pointFormatter: function(){
+						//console.log(this);
+						return this.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.value),1) + '%</b><br/>';
+					}
 				}
-			}	
-		], 
-	chart: {
+			}, 
+			{
+				"visible": false
+			}
+		],
+		chart: {
 			events: {
 	            load: function (e) {
+	            	
 	            	this.credits.element.onclick = function() {};
+
 	                var chart = this;
 	                var fn = this.options.customFunctions;
 	                //define new Highcharts template "mappie"
-	                fn.addLegendRectangle(chart, 260+70, 220+45, 160-45, 120, '#fbfbfb');
+					fn.defineTemplate();
+					
+					var choroplethSeries = chart.series[1];
+					var pieSizeSeries = chart.series[2];
+					
+					//pie diameters in px
+					var maxPieDiameter = 20;
+					
+					//configuration of categorical pie sizes
+					var pieSizeCatConfig = 
+					[
+						{
+							name: "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 < \u00A0\u00A0 300",
+							from: 0,
+							to: 299, 
+							diameter: 5
+						},
+						{
+							name: ' 300 − \u00A0\u00A0 999',
+							from: 300,
+							to: 999,
+							diameter: 10
+						},
+						{
+							name:"\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0 ≥  1 000",
+							from: 1000,							
+							to: 2000,
+							diameter: 20
+						}
+					];
+					
+					//define different colors for positive and negative values
+	                var color = function(value){
+	                	return (value >= 0) ? '#7F5F1A' : '#FABD24';
+	                };					
+					
+					//define chart-specific details
+					var pieSeriesConfig = function(data, correspondingMapSeriesItem, color){
+						return {
+	                        sizeFormatter: function () {
+	                            var fn = this.chart.options.customFunctions;
+								//return fn.pieSize(Math.abs(data.value), fn.getPointsExtremes(pieSizeSeries.points).maxAbsNumber, maxPieDiameter); 
+								return fn.pieSizeCategorical(Math.abs(data.value), pieSizeCatConfig).diameter;
+	                        },
+	                        tooltip: {
+	                            pointFormatter: function () {
+	                            	return correspondingMapSeriesItem.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.v),0) + '</b><br/>';
+	                            }
+	                        }
+	                    };
+					};
+					
+					//put the pies / bubbles on the map
+					fn.drawPies(chart, pieSizeSeries, choroplethSeries, pieSeriesConfig, pieSizeCatConfig, color);
+
+	                //Add manually drawn legend
+	                fn.addLegendRectangle(chart, 243-5, 212+20, 105+5, 130-10, '#fbfbfb');
+					fn.addLegendRectangle(chart, 355, 212+20, 110, 130-22, '#fbfbfb');
+					fn.addLegendTitle(chart, chart.series[2].name.split(' ').slice(0, 2).join(' <br>'), 245, 210+20);
+	                fn.addLegendTitle(chart, chart.series[1].name.split(' ').slice(0, 2).join(' <br>'), 355, 210+20);
+	                
+	               	fn.addLegendCircle(chart, 370, 266+15, 0.5*pieSizeCatConfig[0].diameter, '#7F5F1A');
+	                fn.addLegendLabel(chart, pieSizeCatConfig[0].name, 460, 255+15, undefined, false, 'right');
+	                fn.addLegendCircle(chart, 370, 285+15, 0.5*pieSizeCatConfig[1].diameter, '#7F5F1A');
+	                fn.addLegendLabel(chart, pieSizeCatConfig[1].name, 460, 275+15, undefined, false, 'right');
+	                fn.addLegendCircle(chart, 370, 310+15, 0.5*pieSizeCatConfig[2].diameter, '#7F5F1A');
+					fn.addLegendLabel(chart, pieSizeCatConfig[2].name, 460, 300+15, undefined, false, 'right');
+					
+					//fn.addLegendSquare(chart, 565, 240, 10, '#7F5F1A');
+					//fn.addLegendLabel(chart, 'Zunahme', 580, 236);
+					//fn.addLegendSquare(chart, 565, 256, 10, '#FABD24');
+					//fn.addLegendLabel(chart, 'Abnahme', 580, 252);
+				
+					//make sure pies are hidden upon click onto pie legend
+					fn.AddPieLegendClickHandler(chart, '#cccccc');
 	            }
 			}
-		}		
+		}
 	};
 }());
-
 
