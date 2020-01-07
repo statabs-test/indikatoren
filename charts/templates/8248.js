588c88d5b4bf5726a1e92311fe6fbf32fad5948a
Alfany Fernandez <sssmoj@bs.ch>
2020-01-07 09:43:50 +0100

    8248 finished

--- charts/templates/8248.js
+++ charts/templates/8248.js
@@ -1,62 +1,109 @@
 (function(){
-    return {
-   "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}%",
-    }
-  },
-  "xAxis": {
-	"tickInterval": 1,
-  },
-
-
-
-
-  data: {
-    seriesMapping: [
+  return {
+"xAxis": {
+  //"tickInterval": 1,
+  "type": "category"
+},
+"yAxis": {
+  //tickAmount: 5,
+  tickInterval: 10,
+  min: 0, 
+  max: 40,
+  labels: {
+    format: "{value:,.0f}%",
+  }
+},	
+tooltip: {
+  shared: false, 
+  headerFormat: '<span style="font-size: 10px"> {point.key}</span><br/>',
+  pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}%</b><br/>'
+},
+series: 
+[
+  {
+    type: "scatter",
+    marker: 
       {
-        name: 'Range',
-        x: 0, y: 2, y:3
+        symbol: "circle",
+        radius: 3,
+        "enabled": true
       },
-      {
-        //2nd series: use y values from column 3
-        name: 'Average',
-        y: 1
-      }
-    ]
-    },
-
-  series: [{
-    name: 'Average',
-    //data: averages,
-    zIndex: 1,
-    marker: {
-        fillColor: 'white',
-        lineWidth: 2,
-        lineColor: Highcharts.getOptions().colors[0]
-    }
-}, {
-    name: 'Range',
-    //data: ranges,
-    type: 'arearange',
-    lineWidth: 0,
-    linkedTo: ':previous',
-    color: Highcharts.getOptions().colors[0],
-    fillOpacity: 0.3,
-    zIndex: 0,
-    marker: {
-        enabled: false
-    }
-}],
-
-
-
-
-
-  "tooltip": {
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}%</b><br/>',
-    "shared": false
+      "color": "#246370"
   },
+  {
+    type: "scatter",
+    "marker": 
+      {
+        "symbol": "triangle-down",
+        radius: 3,
+        "enabled": true
+      },
+      "color": "#246370"
+  },
+  {
+    type: "scatter",
+    "marker": 
+      {
+        "symbol": "triangle",
+        "enabled": false,
+        radius: 3,
+        "enabled": true
+      },
+      "color": "#246370"
+  },
+
+
+
+  {
+    type: "scatter",
+    "marker": 
+      {
+        "symbol": "circle",
+        "enabled": false,
+        radius: 3,
+        "enabled": true
+      },
+      "color": "#990300"
+  },
+  {
+    type: "scatter",
+    "marker": 
+      {
+        "symbol": "triangle-down",
+        "enabled": false,
+        radius: 3,
+        "enabled": true
+      },
+      "color": "#990300"
+  },
+  {
+    type: "scatter",
+    "marker": 
+      {
+        "symbol": "triangle",
+        "enabled": false,
+        radius: 3,
+        "enabled": true
+      },
+      "color": "#990300"
+  }
+
+],
+"legend": {
+  "enabled": true,
+  //"x": 30,
+  //"y": 35,
+  //"itemWidth": 150,
+  "layout": "horizontal",
+  "verticalAlign": "top",
+  "itemMarginBottom": 5,
+  "align": "left",
+  "itemStyle": {
+    "fontWeight": "normal"
+  }
+},
+chart: {
+  inverted: true
+}
 };
 }());
-
