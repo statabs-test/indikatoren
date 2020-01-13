86a22cd9ed24a831da63a74732a16f4a304646a2
Hofer <ssshof@bs.ch>
2020-01-13 18:12:36 +0100

    Grafiken MB

--- charts/templates/8075.js
+++ charts/templates/8075.js
@@ -56,26 +56,26 @@
       {
         //"color": "#D7E8D2",
         "color": "#cd9c00",
-        "index": 0,
+        "index": 1,
         "type": "column",
         tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .0f}</b><br/>' }
       },
       {
         //"color": "#E7CEE2",
         "color": "#b375ab",
-        "index": 1,
+        "index": 0,
         "type": "column",
         tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .0f}</b><br/>' }
       },
       {
         "color": "#7f5f1a", //rgb(0,122,47)
-        "index": 2,
+        "index": 3,
         "yAxis": 1,
         tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .1f}%</b><br/>' }
       },
       {
         "color": "#923f8d", //rgb(146,63,141)
-        "index": 3,
+        "index": 2,
         "yAxis": 1,
         tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .1f}%</b><br/>' }
       }
