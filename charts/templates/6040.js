1fa17b4a566fe2e54c5a5c561a22aeba27ecbdc1
bl99 <ssslib@bs.ch>
2020-01-14 11:02:47 +0100

    6040

--- charts/templates/6040.js
+++ charts/templates/6040.js
@@ -1,28 +1,30 @@
-(function(){
+(function () {
     return {
-"plotOptions": {
-    "series": {
-      "pointWidth": 100,
-      },
-},
-    "series": [
-        {
-            "color": "#71A3B5"
+        "plotOptions": {
+            "series": {
+                "pointWidth": 100,
+            },
         },
-        {
-            "color": "#FFBB58"
+        "series": [
+            {
+                "color": "#71A3B5",
+                index: 2
+            },
+            {
+                "color": "#FFBB58",
+                index: 1
+            },
+            {
+                "color": "#8A8A8A",
+                index: 0
+            }
+        ],
+        "xAxis": {
+            "type": "category"
         },
-        {
-            "color": "#8A8A8A"
-        }        
-  ],
-  "xAxis": {
-      "type": "category"
-  },
-  "legend": {
-      "reversed": false
-  }
-	}
+        "legend": {
+            "reversed": false
+        }
+    }
 }());
 
- 
