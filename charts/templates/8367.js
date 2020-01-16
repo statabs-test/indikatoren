98d4201b55f3cf4dd820d2125f315d6d88b002f5
Zaugg <ssszak@bs.ch>
2020-01-16 16:12:25 +0100

    Optimize Charts

--- charts/templates/8367.js
+++ charts/templates/8367.js
@@ -1,71 +1,77 @@
 (function(){
-  return {
+    return {
 "plotOptions": {
-  "series": {
-  // pointPadding: 0.1,
-    //"pointWidth": 15,
-    "dataLabels": {
-      "style": {
-        "fontSize": "10px"
-      }
+    "series": {
+     // pointPadding: 100,
+     // "pointWidth": 15,
+      "dataLabels": {
+        "style": {
+          "fontSize": "10px"
+        }
+      },
+      "stacking": "normal"
+    }
+  },
+  "xAxis": {
+    "type": "category",
+    "tickInterval": 1
+  },  
+  "yAxis": {
+    tickInterval: 40000,
+    tickAmount: 6,
+    "labels": {
+      "format": "{value:,.0f}",
     },
-    "stacking": "normal"
-  }
-},
-"xAxis": {
-  "type": "category",
-  "tickInterval": 1
-},  
-"yAxis": {
-  "labels": {
-    "format": "{value:,.0f}"
+   /*plotLines: [{
+                    value: 0,
+                    color: 'white',
+                    width: 2,
+                    zIndex: 4
+                }]*/
   },
- /*plotLines: [{
-                  value: 0,
-                  color: 'white',
-                  width: 2,
-                  zIndex: 4
-              }]*/
-},
-"legend": {
-  "enabled": true,
-  "layout": "horizontal",
-  "verticalAlign": "top",
-  "align": "left",
-  "itemStyle": {
-    "fontWeight": "normal"
-  }
-},
-"series": [
-{"color": "#246370", "index": 1, legendIndex: 1}, /*rot */
-{"color": "#A8C3CA", "index": 2, legendIndex: 3}, /*blau */
-{"color": "#007A2F", "index": 3, legendIndex: 2}, /*hellrot */
-{"color": "#8AB77D", "index": 4, legendIndex: 4}, /*hellblau */
-    {
-    "color": "#000000",
-    //"index": 1,
-    "type": "line",
-    lineWidth: 0,
-
-   "marker": {
+  "legend": {
     "enabled": true,
-    symbol: "diamond",
-    radius: 6
-    }, 
-    legendIndex: 4,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
   },
-],
-"chart": {
-  "renderTo": 'container-I.01.1.0016', 
-  "marginBottom": 75,
-  //"marginTop": 75,
-  "type": "column",
-  "inverted": false
-},
-"tooltip": {
-"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
-//"footerFormat": 'Total: <b>{point.total:,.0f}</b>',
- "shared": true,
-},
+  "tooltip": {
+    "pointFormatter": function(){
+      if (this.series.name == "Pendlersaldo") {
+      return '<b>' +  Highcharts.numberFormat(this.y,0) + '</b><br/><b>'
+    }
+    else return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(Math.abs(this.y,0),0) + '</b><br/><b>'
+  },
+   "shared": true,
+  },
+  "series": [
+  {"color": "#246370", "index": 1, legendIndex: 1}, /*rot */
+  {"color": "#A8C3CA", "index": 2, legendIndex: 3}, /*blau */
+  {"color": "#007A2F", "index": 3, legendIndex: 2}, /*hellrot */
+  {"color": "#8AB77D", "index": 4, legendIndex: 4}, /*hellblau */
+      {
+      "color": "#000000",
+      //"index": 1,
+      "type": "scatter",
+	   "marker": {
+      "enabled": true,
+      symbol:"diamond",
+      radius: 6,
+      }, 
+      legendIndex: 4,
+    },
+  ],
+  "chart": {
+    //"renderTo": 'container-I.01.1.0016', 
+    "marginBottom": 75,
+    //"marginTop": 75,
+    "type": "column",
+  },
+  "data":{
+    "switchRowsAndColumns": true
+  },
 };
 }());
