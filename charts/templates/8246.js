eb4ac60ee46a366251a590792a644f745cdfdfb9
Bestgen <sssbem@bs.ch>
2020-01-16 17:19:20 +0100

    Update

--- charts/templates/8246.js
+++ charts/templates/8246.js
@@ -1,93 +1,15 @@
-(function(){
+(function () {
     return {
         "series": [
-            {
-                "color": "#007A2F",
-                "index": 0,
-                "legendIndex": 5 
-            },
-            {
-                "color": "#73BA7C",
-                "index": 1,
-                "legendIndex": 4 
-            },
-            {
-                "color": "#FFBB58",
-                "index": 2,
-                "legendIndex": 3 
-            },
-            {
-                "color": "#FF8028",
-                "index": 3,
-                "legendIndex": 2
-            },
-            {
-                "color": "#C8C8C8",
-                "index": 4,
-                "legendIndex": 1
-            }  ,
-            {
-                "color": "#6F6F6F",
-                "index": 5,
-                "legendIndex": 0
-            }          
-    ],
-    xAxis: {
-        "type": "category",
-        "labels": {
-           align: "left",
-            x: -120,
-            useHTML: true,
-            style: {
-                whiteSpace: 'nowrap'
-            },
-            "formatter": function() {
-                return this.value.replace("Einelternfamilie", "&nbsp; &nbsp; &nbsp; Einelternfamilie")
-                .replace("Zweielternfamilie", "&nbsp; &nbsp; &nbsp; Zweielternfamilie")
-                .replace("1 Kind", "&nbsp; &nbsp; &nbsp; 1 Kind")
-                .replace("2 Kinder", "&nbsp; &nbsp; &nbsp; 2 Kinder")
-                .replace("3 Kinder u.m.", "&nbsp; &nbsp; &nbsp; 3 Kinder u.m.")
-                .replace("0-4 Jahre", "&nbsp; &nbsp; &nbsp; 0-4 Jahre")
-                .replace("5-8 Jahre", "&nbsp; &nbsp; &nbsp; 5-8 Jahre")
-                .replace("9-12 Jahre", "&nbsp; &nbsp; &nbsp; 9-12 Jahre")
-                .replace("13-17 Jahre", "&nbsp; &nbsp; &nbsp; 13-17 Jahre")
-                .replace("Obligat. Schule", "&nbsp; &nbsp; &nbsp; Obligat. Schule")
-                .replace("Sekundarstufe II", "&nbsp; &nbsp; &nbsp; Sekundarstufe II")
-                .replace("Tertiärstufe", "&nbsp; &nbsp; &nbsp; Tertiärstufe")
-                .replace("ch/dt", "&nbsp; &nbsp; &nbsp; ch/dt")
-                //.replace("ch/dt und andere", "&nbsp; &nbsp; &nbsp; ch/dt und andere")
-                .replace("nur andere", "&nbsp; &nbsp; &nbsp; nur andere")
-                .replace("<3 000 Fr.", "&nbsp; &nbsp; &nbsp; <3 000 Fr.")
-                .replace("3 000-7 499 Fr.", "&nbsp; &nbsp; &nbsp; 3 000-7 499 Fr.")
-                .replace(">7 499 Fr.", "&nbsp; &nbsp; &nbsp; >7 499 Fr.")
-                .replace("Grossbasel Ost", "&nbsp; &nbsp; &nbsp; Grossbasel Ost")
-                .replace("Grossbasel West", "&nbsp; &nbsp; &nbsp; Grossbasel West")
-                .replace("Kleinbasel", "&nbsp; &nbsp; &nbsp; Kleinbasel")
-                .replace("Riehen, Bettingen", "&nbsp; &nbsp; &nbsp; Riehen, Bettingen")
-                .replace("2009", "&nbsp; &nbsp; &nbsp; 2009")
-                .replace("2013", "&nbsp; &nbsp; &nbsp; 2013")
-                .replace("2017", "&nbsp; &nbsp; &nbsp; 2017")     
-                ;
-            },
-            
-            
-        } 
-  },
-   
-    yAxis:{
-        tickInterval: 20,
-        max: 100,
-        labels:{
-            "type": "category",
-            rotation: 0,
+            { color: "#007A2F", index: 0, legendIndex: 5 },
+            { color: "#73BA7C", index: 1, legendIndex: 4 },
+            { color: "#FFBB58", index: 2, legendIndex: 3 },
+            { color: "#FF8028", index: 3, legendIndex: 2 },
+            { color: "#C8C8C8", index: 4, legendIndex: 1 },
+            { color: "#6F6F6F", index: 5, legendIndex: 0 }
+        ],
+        "xAxis": {
+            type: "category",
         }
-    },
-    "chart": {     
-        marginLeft: 130,
-        "inverted": true,
-        "height": 600,
-     }
-	}
+    }
 }());
-
- 
--- charts/templates/8260.js
+++ charts/templates/8260.js
@@ -1,20 +1,20 @@
-(function(){
+(function () {
     return {
         "series": [
             {
                 "color": "#007A2F",
                 "index": 0,
-                "legendIndex": 5 
+                "legendIndex": 5
             },
             {
                 "color": "#73BA7C",
                 "index": 1,
-                "legendIndex": 4 
+                "legendIndex": 4
             },
             {
                 "color": "#FFBB58",
                 "index": 2,
-                "legendIndex": 3 
+                "legendIndex": 3
             },
             {
                 "color": "#FF8028",
@@ -25,70 +25,56 @@
                 "color": "#C8C8C8",
                 "index": 4,
                 "legendIndex": 1
-            }  ,
+            },
             {
                 "color": "#6F6F6F",
                 "index": 5,
                 "legendIndex": 0
-            }          
-    ],
-    xAxis: {
-        "type": "category",
-        "labels": {
-           align: "left",
-            x: -120,
-            useHTML: true,
-            style: {
-                whiteSpace: 'nowrap'
-            },
-            "formatter": function() {
-                return this.value.replace("Männlich", "&nbsp; &nbsp; &nbsp; Männlich")
-                                .replace("Weiblich", "&nbsp; &nbsp; &nbsp; Weiblich")
-                                .replace("<3 000 Fr.", "&nbsp; &nbsp; &nbsp; <3 000 Fr.")
-                                .replace("3 000-7 499 Fr.", "&nbsp; &nbsp; &nbsp; 3 000-7 499 Fr.")
-                                .replace(">7 499 Fr.", "&nbsp; &nbsp; &nbsp; >7 499 Fr.")
-                                .replace("Grossbasel Ost", "&nbsp; &nbsp; &nbsp; Grossbasel Ost")
-                                .replace("Grossbasel West", "&nbsp; &nbsp; &nbsp; Grossbasel West")
-                                .replace("Kleinbasel", "&nbsp; &nbsp; &nbsp; Kleinbasel")
-                                .replace("Riehen, Bettingen", "&nbsp; &nbsp; &nbsp; Riehen, Bettingen")
-                                .replace("Single-HH", "&nbsp; &nbsp; &nbsp; Single-HH")
-                                .replace("Übrige HH o. K.", "&nbsp; &nbsp; &nbsp; Übrige HH o. K.")
-                                .replace("HH m. mind. 1 K.", "&nbsp; &nbsp; &nbsp; HH m. mind. 1 K.")
-                                .replace("18-29 Jahre", "&nbsp; &nbsp; &nbsp; 18-29 Jahre")
-                                .replace("30-39 Jahre", "&nbsp; &nbsp; &nbsp; 30-39 Jahre")
-                                .replace("40-49 Jahre", "&nbsp; &nbsp; &nbsp; 40-49 Jahre")
-                                .replace("50-59 Jahre", "&nbsp; &nbsp; &nbsp; 50-59 Jahre")
-                                .replace("60-69 Jahre", "&nbsp; &nbsp; &nbsp; 60-69 Jahre")
-                                .replace(">69 Jahre", "&nbsp; &nbsp; &nbsp; >69 Jahre")
-                                .replace("Schweiz", "&nbsp; &nbsp; &nbsp; Schweiz")
-                                .replace("Ausland", "&nbsp; &nbsp; &nbsp; Ausland")
-                                .replace("2003", "&nbsp; &nbsp; &nbsp; 2003")
-                                .replace("2005", "&nbsp; &nbsp; &nbsp; 2005")
-                                .replace("2007", "&nbsp; &nbsp; &nbsp; 2007")
-                                .replace("2011", "&nbsp; &nbsp; &nbsp; 2011")
-                                .replace("2015", "&nbsp; &nbsp; &nbsp; 2015")
-                                .replace("2019", "&nbsp; &nbsp; &nbsp; 2019")
-                ;
-            },
-            
-            
-        } 
-  },
-   
-    yAxis:{
-        tickInterval: 20,
-        max: 100,
-        labels:{
+            }
+        ],
+        xAxis: {
             "type": "category",
-            rotation: 0,
+            "labels": {
+                align: "left",
+                x: -115,
+                useHTML: true,
+                style: {
+                    whiteSpace: 'nowrap'
+                },
+                "formatter": function () {
+                    var tmp = "&nbsp;&nbsp;&nbsp;" + this.value;
+                    return tmp.replace("&nbsp;&nbsp;&nbsp;*", "");
+                }
+            }
+        },
+        "legend": {
+            "enabled": true,
+            //"x": 45,
+            //"y": 35,
+            margin: 0,
+            padding: 5,
+            "itemWidth": 114,
+            itemDistance: 1,
+            "layout": "horizontal",
+            "verticalAlign": "top",
+            //"itemMarginBottom": 5,
+            "align": "left"
+        },
+        yAxis: {
+            tickInterval: 20,
+            max: 100,
+            labels: {
+                "type": "category",
+                rotation: 0,
+            }
+        },
+        "chart": {
+            marginLeft: 125,
+            // marginTop: 120,
+            marginRight: 25,
+            "inverted": true,
+            "height": 700,
         }
-    },
-    "chart": {     
-        marginLeft: 130,
-        "inverted": true,
-        "height": 600,
-     }
-	}
+    }
 }());
 
- 
