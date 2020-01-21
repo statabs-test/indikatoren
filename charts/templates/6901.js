b7252cf4b7a8a3910b21d8cfe15a46d4e1a09e0b
bl99 <ssslib@bs.ch>
2020-01-21 15:33:44 +0100

    Correct 6901

--- charts/templates/6901.js
+++ charts/templates/6901.js
@@ -1,27 +1,30 @@
-(function(){
+(function () {
   return {
+    "chart": {
+      "alignTicks": false
+    },
     "yAxis": {
-    	"min": 0,
+      "min": 0,
       //"max": 200000,
-    	title: {
-          text: null,
-          "color": "#000000",
-          "fontSize": null
+      title: {
+        text: null,
+        "color": "#000000",
+        "fontSize": null
       },
       "labels": {
         "format": "{value:,.0f}",
-         style:{
-         		color: "black",
-         },
+        style: {
+          color: "black",
+        },
       },
 
     },
     "xAxis": {
       "tickInterval": 1,
       "labels": {
-          	step: 1,
-              "rotation": -45,
-        }  
+        step: 1,
+        "rotation": -45,
+      }
 
     },
     "legend": {
@@ -30,9 +33,8 @@
       "verticalAlign": "top",
       "align": "left",
       itemWidth: 140,
-  	  "itemMarginBottom": 5,
+      "itemMarginBottom": 5,
       "itemStyle": {
-        "fontWeight": "normal", 
         textOverflow: 'nowrap',
         whiteSpace: "nowrap"
       }
@@ -45,53 +47,49 @@
         pointPadding: 0,
         borderWidth: 0,
         legendIndex: 1,
-      	"tooltip": {
-    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-  		   },
+        "tooltip": {
+          "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+        },
       },
       {
         "color": "#C8C8C8", // 
-        "index": 1,
+        "index": 2,
         "type": "line",
-         "marker": {
+        "marker": {
           "enabled": false
         },
         legendIndex: 2,
-  	  "tooltip": {
-    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-  	   },
+        "tooltip": {
+          "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+        },
       },
-       {
-        "color":  "#246370", // 
-        "index": 0,
+      {
+        "color": "#246370", // 
+        "index": 1,
         "type": "column",
         pointPadding: 0,
         borderWidth: 0,
         //"pointWidth": "8",
         legendIndex: 3,
-  	  "tooltip": {
-    			"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-        },
-  
+        "tooltip": {
+          "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+        }
       },
       {
-        "color":  "#474747", // 
-        "index": 1,
+        "color": "#474747", // 
+        "index": 3,
         "type": "line",
         "legendIndex": 3,
         "tooltip": {
-    		"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-  	   },
+          "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+        },
         "marker": {
           "enabled": false
-        },
+        }
       }
-    ],  
-     "tooltip": {
-    	    //"shared": true
-  		   },
-    "chart": {
-      "alignTicks": false
+    ],
+    "tooltip": {
+      //"shared": true
     }
   };
 }());
