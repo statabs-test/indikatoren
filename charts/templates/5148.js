4e7fba9e581984e18a4535e5b5a105a0bfb31c9e
bl99 <ssslib@bs.ch>
2020-01-29 17:49:02 +0100

    Update 5148

--- charts/templates/5148.js
+++ charts/templates/5148.js
@@ -1,32 +1,44 @@
-(function(){
-    return {
-   "yAxis": {
-   	min: undefined,
-     "labels": {
-      "format": "{value:,.0f}",
-    }
-  },
-  "xAxis": {
-	"tickInterval": 1,
-  },
-  "series": [
-    {
-      "color": "#8b2223"
-    }
-  ],
-  "tooltip": {
-	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-    "shared": false
-  },
-  "plotOptions": {
-    "series": {
-          },
 
-  },
-  "chart": {
-    "type": "column"
+/*global Highcharts*/
+(function () {
+  return {
+    "chart": {
+      "type": "column",
+      "inverted": false,
+      "spacingBottom": 40
+    },
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0,
+      }
+    },
+    "xAxis": {
+      tickInterval: 1,
+    },
+    "yAxis": {
+      //"max": 1000,
+      min: -500,
+      tickInterval: 250,
+      plotLines: [{
+        color: '#B9CFD7',
+        value: 0,
+        width: 1
+      }]
+    },
+    "series": [
+      { "color": "#FABD24" }, /* gelb*/
+      { "color": "#923F8D" }, /* violett*/
+      { "color": "#000000", type: 'line' } /* grau */
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left"
+    },
+    tooltip: {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+    }
   }
-  
-}
 }());
-
