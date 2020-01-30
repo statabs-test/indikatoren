57ed3cb06e09307cb78b64e1edd27d2ec311e9e7
Zaugg <ssszak@bs.ch>
2020-01-30 15:49:07 +0100

    Update Charts

--- charts/templates/8440.js
+++ charts/templates/8440.js
@@ -1,52 +1,56 @@
-(function(){
-    return {
-   plotOptions: {
-        series: {
-            pointPadding: 0,
-            borderWidth: 0,
-			//itemWidth: 10, 
+(function () {
+  return {
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0,
+        //itemWidth: 10, 
 
+      }
+    },
+    "xAxis": {
+      "type": "category",
+      "labels": {
+        "rotation": 0
+      }
+    },
+    "yAxis": {
+      "labels": {
+        "format": "{value:,.0f}",
+        "formatter": function() {
+          return this.point.key.replace("Dienstleistungen","Dienst-<br/>leistungen");
         }
-   },
-  "xAxis": {
-    "type": "category",
-    "labels": {
-      "rotation": 0 
-    } 
-  },
-  "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}"
-    }    
-  },
-  "series": [
-    { "color": "#474747" },
-    { "color": "#FF8028" },
-    { "color": "#FABD24" },
-    { "color": "#B375AB" },
-    { "color": "#990300" }
-  ],
-  "legend": {
-    itemWidth: 120,
-    itemDistance: 5,
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    //"x": 25,
-	//"y": 55,    
-    "itemStyle": {
-      "fontWeight": "normal"
+      }
+    },
+    "series": [
+      { "color": "#474747" },
+      { "color": "#FF8028" },
+      { "color": "#FABD24" },
+      { "color": "#B375AB" },
+      { "color": "#990300" }
+    ],
+    "legend": {
+      //itemWidth: 126,
+      itemDistance: 5,
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      //"x": 25,
+      //"y": 55,    
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    tooltip: {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
+      "shared": false
+    },
+    "chart": {
+      "type": "column",
+      "inverted": true,
+      //marginLeft: 95
+      //"spacingTop": 100
     }
-  },
-   tooltip: {
-    "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
-    "shared": false
-  },
-  "chart": {      
-    "type": "column",
-    "inverted": true,
-    //"spacingTop": 100
   }
-}
 }());
