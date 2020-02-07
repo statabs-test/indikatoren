0860a8a09bf703c27a8e1db332e689a7ea147238
bl99 <ssslib@bs.ch>
2020-02-07 16:33:54 +0100

    update charts

--- charts/templates/8527.js
+++ charts/templates/8527.js
@@ -1,66 +1,54 @@
-(function(){
+(function () {
   return {
-"plotOptions": {
-  "series": {
-    pointPadding: 100,
-    borderWidth: 0,
-    "pointWidth": 30,
-    "dataLabels": {
-      "style": {
-        "fontSize": "10px"
+    "plotOptions": {
+      "series": {
+        pointPadding: 100,
+        borderWidth: 0,
+        "pointWidth": 30,
+        "stacking": "normal"
       }
     },
-    "stacking": "normal"
+    "xAxis": {
+      //"type": "category",
+      "labels": {
+        useHTML: true,
+        "rotation": 0,
+        "formatter": function () {
+          return this.value.replace("Vorläufig Aufgenommene", "Vorläufig Aufgen.").
+          replace("Asylsuchende","Asyl-suchende").
+          replace(': ','<br>');
+        }
+      }
+    },
+    "yAxis": {
+      tickInterval: 250,
+      "labels": {
+        "format": "{value:,.0f}",
+      },
+      "max": undefined
+    },
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      //"y": 40,
+      "itemWidth": 120,
+      "itemMarginBottom": 5,
+      "itemStyle": {
+        width: 300
+      }
+    },
+    "series": [
+      { "color": "#923f8d", "index": 3, "legendIndex": 0 }, /*grün dunkel2*/
+      { "color": "#b375ab", "index": 2, "legendIndex": 2 }, /*grün mittel*/
+      { "color": "#A8C3CA", "index": 1, "legendIndex": 1 },
+      { "color": "#246370", "index": 0, "legendIndex": 3 },
+    ],
+    "chart": {
+      "marginBottom": 55,
+      //"marginTop": 75,
+      "type": "column",
+    }
   }
-},
-"xAxis": {
-  "type": "category",
-  "labels": {
-    useHTML:true,
-    "rotation": -45
-    ,
-    "formatter": function () {
-      return this.value.replace("Vorläufig", "Vorläuf.")
-      .replace("Kleidung Erwachsene", "Erwachsenenkleider")
-      .replace("Freizeit Kinder", "Freizeit&nbsp;Kinder");
-  } }
-},
-"yAxis": {
-  tickInterval:250,
-  "labels": {
-    "format": "{value:,.0f}",
-  },
-"max": undefined
-},
-"legend": {
-  "enabled": true,
-  "layout": "horizontal",
-  "verticalAlign": "top",
-  "align": "left",
-  //"y": 40,
-  "itemWidth": 120,
-  "itemMarginBottom": 5,
-  "itemStyle": {
-    "fontWeight": "normal",
-    width: 300
-  }
-},
-"series": [
-{"color": "#923f8d", "index": 3, "legendIndex":0}, /*grün dunkel2*/
-{"color": "#b375ab", "index": 2, "legendIndex":2}, /*grün mittel*/
-{"color": "#A8C3CA", "index": 1, "legendIndex":1}, 
-{"color": "#246370", "index": 0, "legendIndex":3}, 
-],
-"chart": {
-  "marginBottom": 75,
-  //"marginTop": 75,
-  "type": "column",
-  "inverted": false
-},
-"tooltip": {
-"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-"footerFormat": 'Total: <b>{point.total:,.0f}</b>',
- "shared": false
-},
-}
 }());
--- charts/templates/8585.js
+++ charts/templates/8585.js
@@ -1,78 +1,45 @@
-(function(){
-    return {
-  "plotOptions": {
-  	   series: {
-         pointPadding: 0
-          //pointWidth: 5, 
-        },
-    "column": {
-     
-    }
-  },
- "xAxis": {
-    "type": "category",
-        "labels": {
-      "rotation": -90,
-      "formatter": function() {
-        return this.value.replace("BS", "<b>BS<br/>");
-}
-    } 
-  },
-  "yAxis": {
+(function () {
+  return {
+    "chart": {
+      "type": "column",
+      "spacingBottom": 4,
+      width: 665,
+      //height: 270
+    },
+    "plotOptions": {
+      series: {
+        pointPadding: 0
+        //pointWidth: 5, 
+      },
+    },
+    "xAxis": {
+      "type": "category",
+      "labels": {
+        "rotation": -90,
+        "formatter": function () {
+          return this.value.replace("BS", "<b>BS<br/>");
+        }
+      }
+    },
+    "yAxis": {
       tickInterval: 2,
-     // max: 5,
-    "labels": {
-      "format": "{value:,.0f}"
-    }    
-  },
-  
-  series: [{
-    
-    "color":'#A8C3CA'
-  },
-  {
-    
-    "color":'#246370'
-  },  
-  {
-    
-    "color":'#083038'
-  }    
+    },
+    series: [
+      {
+        "color": '#A8C3CA'
+      },
+      {
+        "color": '#246370'
+      },
+      {
+        "color": '#083038'
+      }
     ],
-
-  /*"series": [
-           function() {
-           var col;
-				if(this.series.data[0]=="BS"){
-		   col="#0066FF";
-				}else {
-    		col="#0066EE";
-			}
-            }, 
-  {"color": "col", "visible": true}, 
-  ],*/
-  "legend": {
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    //"x": 35,
-	//"y": 35,    
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },
-   tooltip: {
-    headerFormat: '<span style="font-size: 10px">{series.name}</span><br/>',
-    pointFormat: '<span style="color:{point.color}">\u25CF</span> {point.name}: <b>{point.y:,.2f}</b><br/>',
-    "shared": false
-  },
-  "chart": {      
-    "type": "column",
-    "inverted": false,
-    "spacingBottom": 40,
-    width: 665,
-    height: 270
+    "legend": {
+      "layout": "vertical",
+      "verticalAlign": "middle",
+      "align": "right",
+      y: -45
+    },
   }
-}
 }());
