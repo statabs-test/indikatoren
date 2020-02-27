c829ce6b8266c9f17544dbf4067ef9dbbbf47104
Zaugg <ssszak@bs.ch>
2020-02-27 13:28:42 +0100

    Show last year on x-axis

--- charts/templates/6137.js
+++ charts/templates/6137.js
@@ -1,11 +1,38 @@
 (function(){
     return {
-  "xAxis": {
-    tickInterval: 1,
-    labels: {
-      rotation: -45
+      "xAxis": {
+        labels:{
+          rotation: -45,
+        },
+        tickPositioner: function() {
+          var positions = [],
+          ext = this.getExtremes(),
+          xMax = Math.round(ext.max),
+          xMin = Math.round(ext.min)+1;
+          var xMin_even=Math.round(ext.min);
+          
+      
+          if(xMax % 2 == 1) {
+      
+          for (var i = xMin; i < xMax; i++) {
+            if (i % 2 == 1) {
+              positions.push(i);
+            }
+          }
+          positions.push(xMax);
+          return positions;
+        } else if (xMax % 2 == 0){
+          for (var i = xMin_even; i < xMax; i++) {
+            if (i % 2 == 0) {
+              positions.push(i);
+            }
+          }
+          positions.push(xMax);
+          return positions;
+        }    
+      }
     },
-  },
+    
   "yAxis": {
 	"labels": {
 		"format": "{value:,.0f}",
