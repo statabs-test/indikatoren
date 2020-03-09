a22436cf8b6825be87fe87b2966e0c75cb217d70
bl99 <ssslib@bs.ch>
2020-03-09 18:45:43 +0100

    update div

--- charts/templates/4852.js
+++ charts/templates/4852.js
@@ -1,27 +1,26 @@
-(function(){
+(function () {
     return {
-    series: [
-      { "color": "#246370"}, // dunkelgrün
-      { "color": "#A8C3CA"}, // grün
-      { "color": "#FFBB58"}, // orange
-      { "color": "#DC440E"}, // orange
-      { "color": "#D3E2E4"}, // orange
-      { "color": "#C8C8C8"}, // grau
-      { "color": "#6F6F6F"}, // grau
-    ],
-    xAxis: {
-        type: "category"
-    },
-    yAxis: {
-        reversedStacks: false,
-    },
-    legend: {
-        reversed: true,
-        labelFormatter: function(){
-            return this.name.replace("/", " /<br/>");
+        series: [
+            { "color": "#246370" }, // dunkelgrün
+            { "color": "#A8C3CA" }, // grün
+            { "color": "#FFBB58" }, // orange
+            { "color": "#DC440E" }, // orange
+            { "color": "#D3E2E4" }, // orange
+            { "color": "#C8C8C8" }, // grau
+            { "color": "#6F6F6F" }, // grau
+        ],
+        xAxis: {
+            type: "category"
+        },
+        yAxis: {
+            reversedStacks: false,
+        },
+        legend: {
+            reversed: true,
+            labelFormatter: function () {
+                return this.name.replace("mich", " mich<br/>");
+            }
         }
-    }
-	};
+    };
 }());
 
- 
