e7225a291f2cb1697131e87f9110ed552f93bcab
bl99 <ssslib@bs.ch>
2020-03-11 15:56:48 +0100

    update 2 BIP-Indicators

--- charts/templates/6051.js
+++ charts/templates/6051.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   return {
     "data": {
       "seriesMapping": [
@@ -10,46 +10,57 @@
         }
       ]
     },
-    
-/*    "yAxis": [
-      {
-        "labels": {
-          "format": "{value:,.0f}",
-          "style": {
-            "color": "#000000"
+    yAxis: {
+      plotLines: [{
+        color: '#B9CFD7',
+        value: 100,
+        width: 1
+      }]
+    },
+    /*    "yAxis": [
+          {
+            "labels": {
+              "format": "{value:,.0f}",
+              "style": {
+                "color": "#000000"
+              }
+            },
+            "min": 0,
+            "max": undefined,
+            "title": {
+              enabled: false
+            }
+          },
+          {
+            "opposite": true,
+            "title": {
+              enabled: false
+            },
+            "min": 0,
+            "max": 240000,
+            "gridLineWidth": 0,
+            "labels": {
+              "style": {
+                "color": "#000000"
+              }
+            }
           }
-        },
-        "min": 0,
-        "max": undefined,
-        "title": {
-          enabled: false
-        }
-      },
-      {
-        "opposite": true,
-        "title": {
-          enabled: false
-        },
-        "min": 0,
-        "max": 240000,
-        "gridLineWidth": 0,
-        "labels": {
-          "style": {
-            "color": "#000000"
-          }
-        }
-      }
-    ],
-*/
+        ],
+    */
     "xAxis": {
-  	  "tickInterval": 2
+      "tickInterval": 2,
+      plotLines: [{
+        color: '#B9CFD7',
+        value: 2000,
+        width: 1
+      }]
     },
     "series": [
       {
         "marker": {
           enabled: false,
           "symbol": "circle"
-        },     
+        },
         "color": "#008AC3",
         "yAxis": 0,
         "tooltip": {
@@ -60,36 +71,36 @@
         "marker": {
           enabled: false,
           "symbol": "circle"
-        },     
-        "color": "#68AB2B",
-        "yAxis": 0,
-        "tooltip": {
-          "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
-        }
-      }
-/*      {
-        "marker": {
-          enabled: false,
-          "symbol": "circle"
-        },     
-        "color": "#008AC3",
-        "yAxis": 0,
-        "tooltip": {
-          "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
         },
-      },
-      {
-        "marker": {
-          enabled: false,
-          "symbol": "circle"
-        },     
         "color": "#68AB2B",
-        "yAxis": 1,
+        "yAxis": 0,
         "tooltip": {
           "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
         }
       }
-*/
+      /*      {
+              "marker": {
+                enabled: false,
+                "symbol": "circle"
+              },     
+              "color": "#008AC3",
+              "yAxis": 0,
+              "tooltip": {
+                "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+              },
+            },
+            {
+              "marker": {
+                enabled: false,
+                "symbol": "circle"
+              },     
+              "color": "#68AB2B",
+              "yAxis": 1,
+              "tooltip": {
+                "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+              }
+            }
+      */
     ],
     "legend": {
       "enabled": true,
