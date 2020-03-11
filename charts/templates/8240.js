a0501cc2bab7d1ebd8ff924c0a8400931e9ce5b0
Alfany Fernandez <sssmoj@bs.ch>
2020-03-11 15:13:16 +0100

    8240 updated

--- charts/templates/8240.js
+++ charts/templates/8240.js
@@ -143,7 +143,7 @@
 					
 					fn.addLegendTitle(chart, "In der Schweiz <br/> geborene <br/> Personen (%)", 245, 210);
 					
-					fn.addLegendTitle(chart, "Veränderung <br/> gegenüber <br/> 2009 (pp)", 355, 210);
+					fn.addLegendTitle(chart, "Veränderung <br/> gegenüber <br/> 2010 (pp)", 355, 210);
 					//fn.addLegendSquare(chart, 363, 275, 10, '#007A2F');
 					//fn.addLegendLabel(chart, 'Zunahme', 382, 269);
 					//fn.addLegendSquare(chart, 363, 295, 10, '#990300');
