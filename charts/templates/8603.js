523eccdb70f2f435cda5ed2b6660dd97c365acfc
Alfany Fernandez <sssmoj@bs.ch>
2020-03-12 08:23:03 +0100

    8603 updated

--- charts/templates/8603.js
+++ charts/templates/8603.js
@@ -146,7 +146,7 @@
 					
 					fn.addLegendTitle(chart, "Anteil <br/> deutschsprachige <br/> Personen (%)", 245 + shiftHoriz, 220 + shiftVert);
 					
-					fn.addLegendTitle(chart, "Veränderung <br/> gegenüber <br/> 2009 (pp)", 385 + shiftHoriz, 220 + shiftVert);
+					fn.addLegendTitle(chart, "Veränderung <br/> gegenüber <br/> 2010 (pp)", 385 + shiftHoriz, 220 + shiftVert);
 					fn.addLegendSquare(chart, 388 + shiftHoriz, 305 + shiftVert, 10, '#007A2F');
 					fn.addLegendLabel(chart, 'Zunahme', 405 + shiftHoriz, 298 + shiftVert);
 					fn.addLegendSquare(chart, 388 + shiftHoriz, 325 + shiftVert, 10, '#990300');
