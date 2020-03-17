68f6fbf9c432d83dc295bd88a521d3205df22fc1
bl99 <ssslib@bs.ch>
2020-03-17 09:43:15 +0100

    final corrections

--- charts/templates/6464.js
+++ charts/templates/6464.js
@@ -1,58 +1,59 @@
-(function(){
-    return {
-	plotOptions: {
-        series: {
-            pointPadding: 0,
-            borderWidth: 0
-        }
+(function () {
+  return {
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0
+      }
     },
-  "xAxis": {
-    "type": "category",
-    "labels": {
-		  align: "left",
-          x: -185,
-          "formatter": function() {
-            return this.value;//.replace("        ", "<br/>")
+    "xAxis": {
+      "type": "category",
+      "labels": {
+        align: "left",
+        x: -185,
+        "formatter": function () {
+          return this.value;//.replace("        ", "<br/>")
         },
-        style: { 
-			textOverflow: 'none' // prevents ellipsis
-		} 
+        style: {
+          textOverflow: 'none' // prevents ellipsis
+        }
+      },
     },
-  },
-  "yAxis": {
-  	//"min": 0,
-  	//"max": 0.5,
-  	tickInterval: 3000,
-    "labels": {
-      "format": "{value:,.0f}" ,
-        style: { 
-			textOverflow: 'none' // prevents ellipsis
-		} 
-    }    
-  },
-  "series": [
-  {"color": "#ECE1D0"},
-  {"color": "#9E7C59"}, 
-  {"color": "#3A2012"}, 
-  ],
-  "legend": {
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    "itemStyle": {
-      "fontWeight": "normal"
+    "yAxis": {
+      //"min": 0,
+      //"max": 0.5,
+      tickInterval: 3000,
+      "labels": {
+        "format": "{value:,.0f}",
+        style: {
+          textOverflow: 'none', // prevents ellipsis
+          whiteSpace: 'nowrap'
+        }
+      }
     },
-  },
-  tooltip: {
-    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-    shared: false
-  },  
-  "chart": {  
-  	marginLeft: 200,
-  	"height": 650,        
-    "type": "column",
-    "inverted": true
-  }
-};
+    "series": [
+      { "color": "#ECE1D0" },
+      { "color": "#9E7C59" },
+      { "color": "#3A2012" },
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      "itemStyle": {
+        "fontWeight": "normal"
+      },
+    },
+    tooltip: {
+      pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+      shared: false
+    },
+    "chart": {
+      marginLeft: 200,
+      "height": 650,
+      "type": "column",
+      "inverted": true
+    }
+  };
 }());
