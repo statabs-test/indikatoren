be58c049aa3825fe6ce3ed6bbff9a80c70ad47d5
Alfany Fernandez <sssmoj@bs.ch>
2020-03-25 14:31:50 +0100

    8926 corrected

--- charts/templates/8926.js
+++ charts/templates/8926.js
@@ -1,26 +1,41 @@
-(function () {
+(function(){
     return {
-        series: [
-            { "color": "#246370" }, // dunkelgrün
-            { "color": "#A8C3CA" }, // grün
-            { "color": "#FFBB58" }, // orange
-            { "color": "#DC440E" }, // orange
-            { "color": "#D3E2E4" }, // orange
-            { "color": "#C8C8C8" }, // grau
-            { "color": "#6F6F6F" }, // grau
-        ],
-        xAxis: {
-            type: "category"
-        },
-        yAxis: {
-            reversedStacks: false,
-        },
-        legend: {
-            reversed: true,
-            labelFormatter: function () {
-                return this.name.replace("mich", " mich<br/>");
-            }
+    series: [
+      { "color": "#246370"}, // dunkelgrün
+      { "color": "#A8C3CA"}, // grün
+      { "color": "#FFBB58"}, // orange
+      { "color": "#DC440E"}, // orange
+      { "color": "#D3E2E4"}, // orange
+      { "color": "#C8C8C8"}, // grau
+      { "color": "#6F6F6F"}, // grau
+    ],
+    xAxis: {
+        type: "category",
+        reversed: false
+    },
+    yAxis: {
+        reversedStacks: false,
+        tickInterval: 25,
+        labels: {
+            y: 10
         }
-    };
+    },
+    legend: {
+        reversed: false,
+        labelFormatter: function(){
+            return this.name.replace("/", " /<br/>");
+        },
+        //x: 0,
+        layout: "horizontal",
+        align: "left",
+        verticalAlign: "top",
+        itemWidth: 115,
+        itemDistance: 2,
+    },
+    chart: {
+        inverted: true
+    }
+	};
 }());
 
+ 
