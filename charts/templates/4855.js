06a3c8217514f70899caec7411f8257a342749f7
Bestgen <sssbem@bs.ch>
2020-04-27 13:09:12 +0200

    Update

--- charts/templates/4855.js
+++ charts/templates/4855.js
@@ -1,12 +1,12 @@
 (function(){
     return {
     series: [
-      { "color": "#007A2F"}, // dunkelgrün
-      { "color": "#68AB2B"}, // grün
-      { "color": "#FABD24"}, // orange
-      { "color": "#B00000"}, // orange
-      { "color": "#C8C8C8"}, // grau
-      { "color": "#6F6F6F"}, // grau
+      { "color": "#007A2F",index: 0, legendIndex: 5 }, // dunkelgrün
+      { "color": "#68AB2B",index: 1, legendIndex: 4 }, // grün
+      { "color": "#FABD24",index: 2, legendIndex: 3 }, // orange
+      { "color": "#B00000",index: 3, legendIndex: 2 }, // orange
+      { "color": "#C8C8C8",index: 4, legendIndex: 1 }, // grau
+      { "color": "#6F6F6F",index: 5, legendIndex: 0 }, // grau
     ],
     xAxis: {
         type: "category"
@@ -16,6 +16,10 @@
     },
     legend: {
         reversed: true,
+        "layout": "horizontal",
+        "verticalAlign": "top",
+        "itemMarginBottom": 4,
+        "align": "left",
         labelFormatter: function(){
             return this.name.replace("/", " /<br/>");
         }
