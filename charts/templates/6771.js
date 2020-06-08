3f5bc1469588269d4f796bfb9a285b953ce649ab
Alfany Fernandez <sssmoj@bs.ch>
2020-06-08 13:50:35 +0200

    6771 and 6774 corrected

--- charts/templates/6771.js
+++ charts/templates/6771.js
@@ -3,51 +3,94 @@
 	global geojson_wohnviertelEPSG2056
 	global $
 */
+
+var legendPosition = {
+	blockChoropleth: {
+		x: 518, // Customizable
+		y: -15,  // Customizable
+		title: {
+			y: [320, 302, 285],
+			x: 525, // Customizable
+		}
+	},
+	blockSymbol: {
+		x: [750,745], // Customizable
+		y: [377, 402, 427, 452], // Customizable
+		y3C: [382, 412, 442],
+		y4S: [365, 390, 420, 445],
+		numbers: {
+			x: 0,
+			y: [365, 390, 420, 445], // Customizable
+			y3C: [370, 400, 430],
+			y4S: [378, 403, 433, 458]
+		},
+		title: {
+			x: 0
+		}
+	}
+};
+
+legendPosition.blockSymbol.numbers.x = legendPosition.blockSymbol.x[0] + 15;
+
+legendPosition.blockSymbol.title.x = legendPosition.blockSymbol.x[0] - 10;
+var i;
+for (i = 0; i < 3; i++) {
+	legendPosition.blockChoropleth.title.y[i] -= legendPosition.blockChoropleth.y;
+};
+
 (function(){
 
 return {
-    		"legend": {
-    		useHTML: true,
-			"title": {
-				"text": null, 
-				style: {'fontWeight':' normal'}
-			},
-			"layout": "vertical",
-			//"verticalAlign": "middle",
-			"align": "right",
-			"x": -85,
-			"y":  -25,
-			itemMarginBottom: 2, 
-			symbolRadius: 0,
-			itemStyle: {
-				fontWeight: 'normal'
+			"legend": 
+			{
+				useHTML: true,
+				"title": 
+				{
+					"text": null, 
+					style: 
+					{
+						fontWeight: 'normal',
+						fontSize: "15px"
+					}
+				},
+				"layout": "vertical",
+				//"verticalAlign": "middle",
+				"align": "left",
+				"x": legendPosition.blockChoropleth.x,
+				"y": legendPosition.blockChoropleth.y,
+				itemMarginBottom: 2, 
+				symbolRadius: 0,
+				itemStyle: 
+				{
+					fontWeight: 'normal',
+					fontSize: "15px"
 				}
-		},
+			},
 		colorAxis: {
 		            dataClassColor: 'category',
 		                   dataClasses: [{
 		                to: -1.0,
 		                color: '#662673', //rgb(242,242,242)',
-		                name:  "<span style='color: rgba(0,0,0,0)'>-0,99 </span> <<span style='color: rgba(0,0,0,0)'>1</span>-1,00"//"<span style='color: rgba(0,0,0,0)'>0</span> <<span style='color: rgba(0,0,0,0)'>0</span>-1,00"
+		                name:  "<span style='color: rgba(0,0,0,0)'>0 -0,99</span> <<span style='color: rgba(0,0,0,0)'>1</span>-1,00"//"<span style='color: rgba(0,0,0,0)'>0</span> <<span style='color: rgba(0,0,0,0)'>0</span>-1,00"
 		            }, {
 		                from: -0.99999,
 		                to:  -0.5,
 		                color: '#B375AB',//rgb(230,230,230)',
-		                name:  "<span style='color: rgba(0,0,0,0)' ></span>-0,99 −<span style='color: rgba(0,0,0,0)'>1</span>-0,50" //"-0,99 − -0,50"
+		                name:  "<span style='color: rgba(0,0,0,0)'>0 </span>-0,99 −<span style='color: rgba(0,0,0,0)'>1</span>-0,50" //"-0,99 − -0,50"
 		            }, {
 		                from: -0.499999,
 		                to:-0.000001,
 		                 color: '#E7CEE2',  //rgb(200,200,200)',
-		                 name: "<span style='color: rgba(0,0,0,0)' ></span>-0,49 −<span style='color: rgba(0,0,0,0)'>1</span>-0,01" //"-0,49 − -0,01" 
+		                 name: "<span style='color: rgba(0,0,0,0)' >0 </span>-0,49 −<span style='color: rgba(0,0,0,0)'>1</span>-0,01" //"-0,49 − -0,01" 
 		            },{
 		                from: 0,
 		                to: 0.79999,
 		                 color: '#D3E2E4', //rgb(153,153,153
-		                 name: "<span style='color: rgba(0,0,0,0)' >-</span>0,00 −<span style='color: rgba(0,0,0,0)'>1-</span>0,79"//" 0,00 − -0,79" //
+		                 name: "<span style='color: rgba(0,0,0,0)' >-0 </span>0,00 −<span style='color: rgba(0,0,0,0)'>1-</span>0,79"//" 0,00 − -0,79" //
 		            },{
 		                from: 0.8,
 		                color: '#71A3B5', //rgb(60,60,60)
-		                name: "<span style='color: rgba(0,0,0,0)'>-0,99 </span> ≥<span style='color: rgba(0,0,0,0)'>1-</span>0,80" //"<span style='color: rgba(0,0,0,0)'>7,0</span> ≥<span style='color: rgba(0,0,0,0)'>1</span> 0,80"
+		                name: "<span style='color: rgba(0,0,0,0)'>0 -0,99</span> ≥<span style='color: rgba(0,0,0,0)'>1-</span>0,80" //"<span style='color: rgba(0,0,0,0)'>7,0</span> ≥<span style='color: rgba(0,0,0,0)'>1</span> 0,80"
 		            }], 
         },
        
@@ -208,7 +251,8 @@ return {
                 	
                 	//var legendTop = 280; //220
                 	//var legendLeft = 520; //365
-	                fn.addLegendText(chart, legendLeft+0,  legendTop+0, 'Wanderungs-/Umzugssaldo'); //Reihenfolge bei 6022.js falsch xy und dann Titel...
+					//fn.addLegendText(chart, legendLeft+0,  legendTop+0, 'Wanderungs-/Umzugssaldo'); //Reihenfolge bei 6022.js falsch xy und dann Titel...
+					fn.addLegendTitle(chart, choroplethSeries.name.replace("er ", "er<br/>"), legendPosition.blockChoropleth.title.x, legendPosition.blockChoropleth.title.y[0]);
 	                	                // fn.addLegendTitle(chart, 'Wanderung/Umzug', 500, 280); //Reihenfolge bei 6022.js falsch xy und dann Titel...
 	                fn.addLegendColumnChart(chart, legendLeft+0,  legendTop+35,  /*legendColumnValues*0.466*/  [1.44, 0.72], color, 'columnLegendHideOnZoom'); //Hemdsärmlige Lösung MB
 	                
@@ -216,16 +260,18 @@ return {
 	                //fn.addLegendText(chart,        legendLeft+71, legendTop+45,  ',', undefined, 'columnLegendHideOnZoom');
 	                fn.addLegendText(chart,        legendLeft+25, legendTop+35,  Highcharts.numberFormat(1.5, 1,","," "), color(1.5, 1), 'columnLegendHideOnZoom');
 
-					fn.addLegendSquare(chart,      legendLeft-0,  legendTop+45,  10, color(1, 0));
-					fn.addLegendText(chart,        legendLeft+15, legendTop+53,  'Wanderung positiv');
-					fn.addLegendSquare(chart,      legendLeft-0,  legendTop+59,  10, color(-1, 0));
-					fn.addLegendText(chart,        legendLeft+15, legendTop+68,  'Wanderung negativ');
+					fn.addLegendSquare(chart,      legendPosition.blockSymbol.x[1], legendPosition.blockSymbol.y4S[0],  15, color(1, 0));
+					fn.addLegendText(chart,        legendPosition.blockSymbol.numbers.x, legendPosition.blockSymbol.numbers.y4S[0],  'Wanderung positiv');
+					fn.addLegendSquare(chart,      legendPosition.blockSymbol.x[1], legendPosition.blockSymbol.y4S[1],  15, color(-1, 0));
+					fn.addLegendText(chart,        legendPosition.blockSymbol.numbers.x, legendPosition.blockSymbol.numbers.y4S[1],  'Wanderung negativ');
 					
-					fn.addLegendSquare(chart,      legendLeft+0,  legendTop+73,  10, color(1, 1));
-					fn.addLegendText(chart,        legendLeft+15, legendTop+82,  'Umzug positiv');
-					fn.addLegendSquare(chart,      legendLeft+0,  legendTop+87, 10, color(-1, 1));
-					fn.addLegendText(chart,        legendLeft+15, legendTop+96 , 'Umzug negativ');
-					fn.addLegendText(chart,  legendLeft+133, legendTop+0, 'Bevölkerungssaldo'  );	
+					fn.addLegendSquare(chart,      legendPosition.blockSymbol.x[1], legendPosition.blockSymbol.y4S[2],  15, color(1, 1));
+					fn.addLegendText(chart,        legendPosition.blockSymbol.numbers.x, legendPosition.blockSymbol.numbers.y4S[2],  'Umzug positiv');
+					fn.addLegendSquare(chart,      legendPosition.blockSymbol.x[1], legendPosition.blockSymbol.y4S[3],  15, color(-1, 1));
+					fn.addLegendText(chart,        legendPosition.blockSymbol.numbers.x, legendPosition.blockSymbol.numbers.y4S[3], 'Umzug negativ');
+					fn.addLegendTitle(chart,  'Bevölkerungssaldo', legendPosition.blockSymbol.title.x, legendPosition.blockChoropleth.title.y[0]);	
+					//fn.addLegendTitle(chart,  pieSizeSeries.name.replace(" ", "<br/>"), legendPosition.blockSymbol.title.x, legendPosition.blockChoropleth.title.y[1]);
+
 					//fn.addLegendTextbold(chart,    legendLeft-98, legendTop+105 , 'Bewegung pro 100 Einwohner');
 					//fn.addLegendTextbold(chart,    legendLeft-98, legendTop+108 , '(Saldo total)');
 					
