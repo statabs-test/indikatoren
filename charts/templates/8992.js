1cf9df6ffe63d4a2d7ee0203c5d239471f671a57
Zaugg <ssszak@bs.ch>
2020-06-10 16:36:01 +0200

    Custom Buttons for Range Selector

--- charts/templates/8992.js
+++ charts/templates/8992.js
@@ -3,11 +3,11 @@
   return {
 
     "yAxis": {
-       showLastLabel: true,
-       endOnTick: true,
-       labels: {
-         format: "{value:,.0f}",
-       },
+      showLastLabel: true,
+      endOnTick: true,
+      labels: {
+        format: "{value:,.0f}",
+      },
       opposite: false,
     },
 
@@ -30,20 +30,17 @@
         "marker": {
           "enabled": false
         }
-      }, /* violett */
+      }, 
       {
         "color": "#FABD24",
         "marker": {
           "enabled": false
         }
-      }, /* rot */
+      }, 
 
     ],
     "legend": {
       "enabled": true,
-      //"x": 50,
-      //"y": 25,
-      //"itemWidth": 150,
       "layout": "horizontal",
       "verticalAlign": "top",
       "itemMarginBottom": 5,
@@ -53,62 +50,51 @@
       }
     },
 
-    /*data: {
-      //convert year in first column to UTC date to be used by Highstock
-      parsed: function(columns){
-          columns[0].forEach(function(v, i, a){
-              columns[0][i] = Date.UTC(columns[0][i]);
-          });
-      }
-    }, */
 
     rangeSelector: {
-      //enabled:  true,
       buttonTheme: {
         width: null
       },
       buttons:
-        [
-          {
+        [{
             type: '',
             count: 2,
             text: 'vor 2020'
           },
           {
-
+            type: '',
+            count: 2,
+            text: 'ab 2020'
+          },
+          {
             type: 'all',
             text: 'Alle Jahre'
           }
-
-
-
         ],
       buttonSpacing: 40,
       enabled: true,
       inputEnabled: false,
-      selected: 1,
-      //inputDateFormat: '%Y',
-      // inputEditDateFormat: '%Y'
+      selected: 2,
+
 
     },
     "xAxis": {
-     // minRange: 3600 * 1000,
       events: {
         afterSetExtremes: function (e) {
           if (e.trigger == "rangeSelectorButton" &&
             e.rangeSelectorButton.text == "vor 2020") {
-            // it is your button that caused this,
-            // so setExtrememes to your custom
-            // have to do in timeout to let
-            // highcharts finish processing events...
             setTimeout(function () {
               Highcharts.charts[0].xAxis[0].setExtremes(Date.UTC(2006, 0), Date.UTC(2019, 12))
             }, 1);
           }
+          else if (e.trigger == "rangeSelectorButton" &&
+            e.rangeSelectorButton.text == "ab 2020") {
+            setTimeout(function () {
+              Highcharts.charts[0].xAxis[0].setExtremes(Date.UTC(2020, 0), undefined)
+            }, 1);
+          }
         },
-
       }
-
     },
   }
 }());
