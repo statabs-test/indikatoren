219e99757181358a08e4bc8d8f5e2868f27794f7
bl99 <ssslib@bs.ch>
2020-08-26 19:00:00 +0200

    7578 & 7582

--- charts/templates/7578.js
+++ charts/templates/7578.js
@@ -1,61 +1,58 @@
 /*global $*/
-(function(){
-    
-  return {
-    
-    plotOptions: {
-      pie: {
-        size: 250,
-      },
-      series: {
-          allowPointSelect: false,
-          point: {
-              events: {
-                  legendItemClick: function (event) {
-                      event.preventDefault();
-
-                  }
-              }
-          }
-      }
-  },
-      
-    legend:{
-      layout: "horizontal",
-      verticalAlign: "top",
-      align: "left",
-      itemDistance: 2,
-      style: {
-  fontSize: "12px"
-  }
-    },
-    "series": [{
-          showInLegend: false,
-          type: 'pie',
-          innerSize: '100%',
-          dataLabels: {
-            enabled: true,
-            format: '{point.name}',
-            distance: 15,
-            connectorWidth: 0,
-            style: {
-                    color: 'black',
-                    textOutline: "0px black", 
-                    fontWeight: "normal", 
-                    fontSize: "12px"
-                }
-      }
-         
-    }, {
-          showInLegend: false,
-          type: 'pie',
-          innerSize: '40%',
-        borderWidth: 2,
-           colors: [
-            '#FFDA80', 
-            '#CD9C00',
-            '#45381D',
-           ]/*,
+(function () {
+	return {
+		plotOptions: {
+			pie: {
+				size: 250,
+			},
+			series: {
+				allowPointSelect: false,
+				point: {
+					events: {
+						legendItemClick: function (event) {
+							event.preventDefault();
+						}
+					}
+				}
+			}
+		},
+		legend: {
+			layout: "horizontal",
+			verticalAlign: "top",
+			align: "left",
+			itemDistance: 2,
+			style: {
+				fontSize: "12px"
+			}
+		},
+		"series": [{
+			visible: false,
+			showInLegend: false,
+/*			type: 'pie',
+			innerSize: '100%'			
+*/
+		}, {
+			showInLegend: false,
+			type: 'pie',
+			innerSize: '40%',
+			borderWidth: 0,
+			colors: [
+				'#FFDA80',
+				'#CD9C00',
+				'#45381D',
+			],
+			dataLabels: {
+				enabled: true,
+				format: '{point.name}',
+				distance: 15,
+				connectorWidth: 0,
+				style: {
+					color: 'black',
+					textOutline: "0px black",
+					fontWeight: "normal",
+					fontSize: "12px"
+				}
+			}/*,
           dataLabels: {
             enabled: true,
                   format: '{point.percentage:.0f}%',
@@ -67,11 +64,11 @@
                       fontSize: "12px"
                   }
           }*/
-      }],
-      
-    "tooltip": {
-      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} </b> ({point.percentage:.1f}%)<br/><br/>',
-      "shared": false
-    }   
-  };
+		}],
+
+		"tooltip": {
+			"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} </b> ({point.percentage:.1f}%)<br/><br/>',
+			"shared": false
+		}
+	};
 }());
--- charts/templates/7582.js
+++ charts/templates/7582.js
@@ -1,90 +1,86 @@
 /*global $*/
-(function(){
-    
-  return {
-    
-    plotOptions: {
-      pie: {
-        size: 250,
-      },  
-      series: {
-          allowPointSelect: false,
-          point: {
-              events: {
-                  legendItemClick: function (event) {
-                      event.preventDefault();
+(function () {
+	return {
+		plotOptions: {
+			pie: {
+				size: 250,
+			},
+			series: {
+				allowPointSelect: false,
+				point: {
+					events: {
+						legendItemClick: function (event) {
+							event.preventDefault();
+						}
+					}
+				}
+			}
+		},
+		legend: {
+			layout: "horizontal",
+			verticalAlign: "top",
+			align: "left",
+			itemDistance: 2,
+			style: {
+				fontSize: "12px"
+			}
+		},
+		/*data: {
+			seriesMapping: [
+				{
+					x: 0, y: 2
+				},
+				{
+					x: 0, y: 1
+				}
+			]
+		},*/
+		"series": [{
+			visible: false,
+			showInLegend: false,
+/*			type: 'pie',
+			innerSize: '100%'			
+*/
+		}, {
+			showInLegend: false,
+			type: 'pie',
+			innerSize: '40%',
+			borderWidth: 0,
+			colors: [
+				'#FFDA80',
+				'#CD9C00',
+				'#45381D',
+			],
+			dataLabels: {
+				enabled: true,
+				format: '{point.name}',
+				distance: 15,
+				connectorWidth: 0,
+				style: {
+					color: 'black',
+					textOutline: "0px black",
+					fontWeight: "normal",
+					fontSize: "12px"
+				}
+			}/*,
 
-                  }
-              }
-          }
-      }
-  },
-      
-    legend:{
-      layout: "horizontal",
-      verticalAlign: "top",
-      align: "left",
-      itemDistance: 2,
-      style: {
-  fontSize: "12px"
-  }
-    },
-    data: {
-      seriesMapping: [
-        {
-          x: 0, y: 2
-        },
-        {
-          x: 0, y: 1
-        } 
-      ]
-  },
-    "series": [{
-          showInLegend: false,
-          type: 'pie',
-          innerSize: '100%',
-        dataLabels: {
-          enabled: true,
-                format: '{point.name}',
-                distance: 15,
-                connectorWidth: 0,
-                style: {
-                    color: 'black',
-                    textOutline: "0px black", 
-                    fontWeight: "normal", 
-                    fontSize: "12px"
-                }
-      },
-       
-    }, 
-    {
-          showInLegend: false,
-          type: 'pie',
-          innerSize: '40%',
-        borderWidth: 2,
-           colors: [
-            '#FFDA80', 
-            '#CD9C00',
-            '#45381D',
-           ],
+			/*,
+		   dataLabels: {
+			 enabled: true,
+				   format: '{point.percentage:.0f}%',
+				   distance: -35,
+				   style: {
+					   color: 'contrast',
+					   textOutline: "0px black", 
+					   fontWeight: "normal", 
+					   fontSize: "12px"
+				   }
+		   }*/
+		}],
 
-           /*,
-          dataLabels: {
-            enabled: true,
-                  format: '{point.percentage:.0f}%',
-                  distance: -35,
-                  style: {
-                      color: 'contrast',
-                      textOutline: "0px black", 
-                      fontWeight: "normal", 
-                      fontSize: "12px"
-                  }
-          }*/
-      }],
-      
-    "tooltip": {
-      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}% </b>',
-      "shared": true
-    }  
-  };
+		"tooltip": {
+			"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} </b> ({point.percentage:.1f}%)<br/><br/>',
+			"shared": false
+		}
+	};
 }());
