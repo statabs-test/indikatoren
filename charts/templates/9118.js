98e64d739932659985ec67942cc8df51d63fbd15
Hofer <ssshof@bs.ch>
2020-08-27 16:46:20 +0200

    9118 und 9120

--- charts/templates/9118.js
+++ charts/templates/9118.js
@@ -1,64 +1,26 @@
 (function(){
     return {
-    "yAxis":{
-        tickInterval:25,
+    series: [
+      { "color": "#71A3B5"}, // blau
+      { "color": "#FABD24"}, // gelb
+      { "color": "#C8C8C8"}, // hellgrau
+      { "color": "#6F6F6F"}, // grau
+    ],
+    xAxis: {
+        type: "category"
     },
- 	"xAxis": {
-        "type": "category",
-        "labels": {
-		  align: "left",
-          x: -125,
-            "formatter": function() {
-                //add sum of observations of visible series to the axis label
-                var allVisibleSeries = this.chart.series.filter(function(val, i, arr){
-                    return val.visible;
-                });
-                var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-                var sum = allVisibleSeries.reduce(function(accumulator, series, index, arr){
-                    return accumulator + series.yData[indexOfCurrentValue];
-                }, 0);
-                //use N if all series are visible, otherwise use n
-                var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n='; 
-                //check for value that contains only spaces
-            	return (this.value.replace(/\s/g,"") == "") ? this.value : this.value + ' (' + nString + sum + ')';
-            }
-        } 
-      },  
-      plotOptions: {
-      	column: {
-      		groupPadding: 0,
-    
-      	}
-      },
-      "series": [
-      {"color": "#083038", "index": 6, "legendIndex":0}, /*blau dunkel*/
-      {"color": "#246370", "index": 5, "legendIndex":1}, /*blau mittel*/
-      {"color": "#FF8028", "index": 4, "legendIndex":2}, /*gelb*/
-      {"color": "#DC440E", "index": 3, "legendIndex":3}, /*orange*/
-      {"color": "#B00000", "index": 2, "legendIndex":4}, /*rot*/
-      { "color": "#C8C8C8", "index": 1, "legendIndex": 5 }, /*grau hell*/
-      { "color": "#6F6F6F", "index": 0, "legendIndex": 6 }, /*grau*/
-      ],
-
-    "chart": {      
-    	 marginLeft: 135,
-         "inverted": true,
-        "height": 550,       
-
-      },
-    "legend": {
+    yAxis: {
+        reversedStacks: false,
+    },
+    legend: {
+        reversed: false,
         "layout": "horizontal",
         "verticalAlign": "top",
-        "itemMarginBottom": 5,     
+        "itemMarginBottom": 4,
         "align": "left",
-        "useHTML": false,
-        "itemStyle": {
-            "fontWeight": "normal"
-        },
-        "symbolRadius": 0,
-        "labelFormatter": function () {
-            return this.name.replace('/', '/<br/>');
+        labelFormatter: function(){
+            return this.name.replace("/", " /<br/>");
         }
     }
-    };
+	};
 }());
--- charts/templates/9120.js
+++ charts/templates/9120.js
@@ -7,21 +7,8 @@
         "type": "category",
         "labels": {
 		  align: "left",
-          x: -125,
-            "formatter": function() {
-                //add sum of observations of visible series to the axis label
-                var allVisibleSeries = this.chart.series.filter(function(val, i, arr){
-                    return val.visible;
-                });
-                var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-                var sum = allVisibleSeries.reduce(function(accumulator, series, index, arr){
-                    return accumulator + series.yData[indexOfCurrentValue];
-                }, 0);
-                //use N if all series are visible, otherwise use n
-                var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n='; 
-                //check for value that contains only spaces
-            	return (this.value.replace(/\s/g,"") == "") ? this.value : this.value + ' (' + nString + sum + ')';
-            }
+          x: -145,
+            "formatter": undefined
         } 
       },  
       plotOptions: {
@@ -36,12 +23,12 @@
       {"color": "#FF8028", "index": 4, "legendIndex":2}, /*gelb*/
       {"color": "#DC440E", "index": 3, "legendIndex":3}, /*orange*/
       {"color": "#B00000", "index": 2, "legendIndex":4}, /*rot*/
-      { "color": "#C8C8C8", "index": 1, "legendIndex": 5 }, /*grau hell*/
-      { "color": "#6F6F6F", "index": 0, "legendIndex": 6 }, /*grau*/
+      {"color": "#C8C8C8", "index": 1, "legendIndex": 5 }, /*grau hell*/
+      {"color": "#6F6F6F", "index": 0, "legendIndex": 6 }, /*grau*/
       ],
 
     "chart": {      
-    	 marginLeft: 135,
+    	 marginLeft: 145,
          "inverted": true,
         "height": 550,       
 
