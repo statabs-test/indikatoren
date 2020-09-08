a055c235522dafd41cf4f0446d3a7f693dcd5b5e
Hofer <ssshof@bs.ch>
2020-09-08 15:08:41 +0200

    Update 9115, 9117, 9120, 9118

--- charts/templates/9117.js
+++ charts/templates/9117.js
@@ -31,14 +31,14 @@
       	}
       },
       "series": [
-      {"color": "#083038", "index": 6, "legendIndex":0}, /*blau dunkel*/
-      {"color": "#246370", "index": 5, "legendIndex":1}, /*blau mittel*/
-      {"color": "#FF8028", "index": 4, "legendIndex":2}, /*gelb*/
-      {"color": "#DC440E", "index": 3, "legendIndex":3}, /*orange*/
-      {"color": "#B00000", "index": 2, "legendIndex":4}, /*rot*/
-      {"color": "#C8C8C8", "index": 1, "legendIndex": 5 }, /*grau hell*/
-      {"color": "#6F6F6F", "index": 0, "legendIndex": 6 }, /*grau*/
-      ],
+        {"color": "#246370", "index": 6, "legendIndex": 0}, /*blau dunkel*/
+        {"color": "#A8C3CA", "index": 5, "legendIndex": 1}, /*blau mittel*/
+        {"color": "#FFBB58", "index": 4, "legendIndex": 2}, /*gelb*/
+        {"color": "#DC440E", "index": 3, "legendIndex": 3}, /*orange*/
+        {"color": "#D3E2E4", "index": 2, "legendIndex": 4}, /*rot*/
+        {"color": "#C8C8C8", "index": 1, "legendIndex": 5}, /*grau hell*/
+        {"color": "#6F6F6F", "index": 0, "legendIndex": 6}, /*grau*/
+        ],
 
     "chart": {      
     	 marginLeft: 135,
