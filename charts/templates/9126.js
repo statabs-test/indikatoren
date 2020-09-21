2b689fa7418606212f5b55c95fba2c9bdb4b945a
Alfany Fernandez <sssmoj@bs.ch>
2020-09-21 08:29:10 +0200

    precision tooltip 9126

--- charts/templates/9126.js
+++ charts/templates/9126.js
@@ -51,7 +51,7 @@
 				tooltip: {
 					pointFormatter: function(){
 						//console.log(this);
-						return this.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.value),1) + '  </b><br/>';
+						return this.properties.LIBGEO +': <b>' + Highcharts.numberFormat((this.value),2) + '  </b><br/>';
 					}
 				}
 			}, 
