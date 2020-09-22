ab0cf3264ea2ce1ca733d17bf1c9c4db9fd107d7
Alfany Fernandez <sssmoj@bs.ch>
2020-09-22 11:31:44 +0200

    still trying with 7575...

--- charts/templates/7575.js
+++ charts/templates/7575.js
@@ -1,32 +1,40 @@
-/*  global rheinData
+/*  
+/*  
 	global Highcharts
-	global geojson_wohnviertel
+	global geojson_wohnviertelEPSG2056 
+	global $
 */
 (function(){
+
     return {
-		colorAxis: {
-			minColor: "#D3E2E4",
-			maxColor: "#083038",
-			labels: {
-				formatte: function () {
+    	"legend": {
+    		y:5,
+			"title": {
+				"text": ""
+			}
+			},
+		"colorAxis": {
+			"min": 0,
+			"minColor": "#eff6e9",
+			"maxColor": "#0A3B19",
+			"labels": {
+				"formatter": function () {
 					return Highcharts.numberFormat((this.value),3); 
-				},
-				style: {
-					whiteSpace: "nowrap"
 				}
 			}
-		},		
-        data: {
-		    seriesMapping: [
+		},
+        "data": {
+		    "seriesMapping": [
 		      {
-		      	x: 2, y: 5
-		      }	      
+		      	x: 1, y: 5
+		      },
+		      {
+		      //2nd series: use y values from column 3
+		      	x: 1, y: 4
+		      }
 		    ]
-		},
-		legend: {
-			align: 'right'
-		},
-		series: [
+        },
+		"series": [
 			{
 				name: "Gemeinde", 
 				animation: true,
@@ -34,19 +42,134 @@
 				borderColor: "#fbfbfb",		
 				joinBy: ['GD_NR', 'g1g13.GMDNR'],
 				keys: ['GD_NAME', 'value'],
-				states: {
-					hover: {
-						enabled: false,
-						borderColor: '#BADA55',
-						brightness: 0
+				"states": {
+					"hover": {
+						"enabled": true,
+						"borderColor": '#aaa',
+						"brightness": 0,
+					}
+				}, 
+				tooltip: {
+					pointFormatter: function(){
+						return this.properties.GD_NAME +': <b>' + Highcharts.numberFormat((this.value),0) + '  </b><br/>';
 					}
 				}
 			}, 
 			{
-				visible: false,
+				visible: false, 
 				showInLegend: false,
     			colorAxis: false
-			}	
-		]
+			}
+		],
+		chart: {
+			events: {
+	            load: function (e) {
+	            	
+	            	this.credits.element.onclick = function() {};
+
+	                var chart = this;
+	                var fn = this.options.customFunctions;
+	                //define new Highcharts template "mappie"
+					fn.defineTemplate();
+					
+					var choroplethSeries = chart.series[1];
+					var pieSizeSeries = chart.series[2];
+
+					//pie diameters in px
+					var maxPieDiameter = 25;
+
+					var extremeValues = fn.getPointsExtremes(pieSizeSeries.points);
+					//define different colors for positive and negative values
+	                var color = function(value){
+	                	return (value >= 0) ? '#7F5F1A' : '#FABD24';
+	                };					
+					
+					//define chart-specific details
+
+					var pieSeriesConfig = function(data, correspondingMapSeriesItem, color){
+						return {
+	                        sizeFormatter: function () {
+	                            var fn = this.chart.options.customFunctions;
+	                            var yAxis = chart.yAxis[0], zoom = (yAxis.dataMax - yAxis.dataMin) / (yAxis.max - yAxis.min);
+								return zoom * fn.pieSize(Math.abs(data.value), fn.getPointsExtremes(pieSizeSeries.points).maxAbsNumber, maxPieDiameter); 
+								//return fn.pieSizeCategorical(Math.abs(data.value), pieSizeCatConfig).diameter;
+	                        },
+	                        /*tooltip: {
+	                            pointFormatter: function () {
+	                            	return correspondingMapSeriesItem.properties.PLZ +': <b>' + Highcharts.numberFormat((this.v),0) + '  </b><br/>';
+	                            }
+	                        }*/
+	                    };
+					};
+					 var pieSizeCatConfig;
+
+					//put the pies / bubbles on the map
+					fn.drawPies(chart, pieSizeSeries, choroplethSeries, pieSeriesConfig, pieSizeCatConfig, color);
+	                
+					//pie values in legend
+	                var minValueInLegend = 60; 
+	                var maxValueInLegend = 60000; 
+	                
+                	//Add manually drawn legend		
+                	fn.addLegendRectangle(chart, 250, 220, 230, 77, '#fbfbfb', 'pieLegend');
+                	fn.addLegendRectangle(chart, 250, 300, 230, 75, '#fbfbfb');
+	                //fn.addLegendTitle(chart, pieSizeSeries.name, 265, 220, 'pieLegend pieLegendHideOnZoom');
+
+	                fn.addLegendCircle(chart, 280, 255, 0.5*fn.pieSize(minValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), '#7F5F1A', 'pieLegendStayeOnZoom');
+	                //fn.addLegendLabel(chart, Highcharts.numberFormat((minValueInLegend),0,","," "), 300, 245, 'pieLegendRecalculateOnZoom', false, minValueInLegend);
+	                fn.addLegendCircle(chart, 280, 280, 0.5*fn.pieSize(maxValueInLegend, extremeValues.maxAbsNumber, maxPieDiameter), '#7F5F1A', 'pieLegendStayeOnZoom');
+	                //fn.addLegendLabel(chart, Highcharts.numberFormat((maxValueInLegend),0,"."," "), 300, 270, 'pieLegendRecalculateOnZoom', false, maxValueInLegend);
+	                
+	                var zoomableLabels = [];
+	                zoomableLabels.push({
+	                	chart: chart, 
+	                	text: Highcharts.numberFormat((minValueInLegend),0,","," "), 
+	                	x: 12, 
+	                	y: 1200, 
+	                	cssClass: 'pieLegendRecalculateOnZoom', 
+	                	useHtml: false, 
+	                	initialValue: minValueInLegend,
+	                	align: 'right',
+	                	legendLabelZoomFormatter: function(value){
+	                		return Highcharts.numberFormat((value), 0, ",", " ");
+	                	},					
+	                }); 
+	                zoomableLabels[0].label = fn.addLegendLabel(zoomableLabels[0].chart, zoomableLabels[0].text, zoomableLabels[0].x, zoomableLabels[0].y, zoomableLabels[0].cssClass, zoomableLabels[0].useHtml, zoomableLabels[0].align);
+	                //copy first label but overwrite some properties
+	                zoomableLabels.push($.extend({}, zoomableLabels[0], {
+	                	text: Highcharts.numberFormat((maxValueInLegend),0,"."," "),
+	                	y: 270,
+	                	initialValue: maxValueInLegend,
+	                }));
+	                zoomableLabels[1].label = fn.addLegendLabel(zoomableLabels[1].chart, zoomableLabels[1].text, zoomableLabels[1].x, zoomableLabels[1].y, zoomableLabels[1].cssClass, zoomableLabels[1].useHtml, zoomableLabels[1].align);
+
+
+					//fn.addLegendSquare(chart, 270, 250, 10, '#7F5F1A');
+					//fn.addLegendLabel(chart, 'Zunahme', 300, 245);
+					//fn.addLegendSquare(chart, 270, 275, 10, '#FABD24');
+					//fn.addLegendLabel(chart, 'Abnahme', 300, 270);
+					fn.addLegendLabelbold(chart, pieSizeSeries.name, 265, 220, 'pieLegendStayeOnZoom');
+					fn.addLegendTitle(chart, choroplethSeries.name, 265, 310);
+
+					
+					//make sure pies are hidden upon click onto pie legend
+					fn.AddPieLegendClickHandler(chart);
+					
+					chart.update(
+					{
+						xAxis: {
+				    		events: {
+								//recalculate and hide svg elements on zoom
+								afterSetExtremes: function(e){
+									var fn = this.chart.userOptions.customFunctions;
+									fn.recalculateOnZoom(e, zoomableLabels);
+								}
+				    		}
+						}
+					});
+					
+	            }
+			}
+		}
 	};
 }());
