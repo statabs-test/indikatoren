ee0f25b7d783614131603cf50f45b2161768bb33
ksz91 <kevin.zaugg@bs.ch>
2020-10-05 16:53:03 +0200

    thousands separator

--- charts/templates/9109.js
+++ charts/templates/9109.js
@@ -9,7 +9,7 @@
             }
         },
         "tooltip": {
-            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.0f}</b><br/>'
+            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>'
         },
         "series": [
             {
