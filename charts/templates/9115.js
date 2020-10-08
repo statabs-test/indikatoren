25af3734138be18e726fc85c6325509127fbf6c0
bl99 <bjoern.lietzke@bs.ch>
2020-10-08 16:35:24 +0200

    add gliederung, build

--- charts/templates/9115.js
+++ charts/templates/9115.js
@@ -1,65 +1,65 @@
-(function(){
+(function () {
     return {
-    "yAxis":{
-        tickInterval:25,
-    },
- 	"xAxis": {
-        "type": "category",
-        "labels": {
-		  align: "left",
-          x: -125,
-            "formatter": function() {
-                //add sum of observations of visible series to the axis label
-                var allVisibleSeries = this.chart.series.filter(function(val, i, arr){
-                    return val.visible;
-                });
-                var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-                var sum = allVisibleSeries.reduce(function(accumulator, series, index, arr){
-                    return accumulator + series.yData[indexOfCurrentValue];
-                }, 0);
-                //use N if all series are visible, otherwise use n
-                var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n='; 
-                //check for value that contains only spaces
-            	return (this.value.replace(/\s/g,"") == "") ? this.value : this.value + ' (' + nString + sum + ')';
-            }
-        } 
-      },  
-      plotOptions: {
-      	column: {
-      		groupPadding: 0,
-    
-      	}
-      },
-      "series": [
-      {"color": "#246370", "index": 6, "legendIndex": 0}, /*blau dunkel*/
-      {"color": "#A8C3CA", "index": 5, "legendIndex": 1}, /*blau mittel*/
-      {"color": "#FFBB58", "index": 4, "legendIndex": 2}, /*gelb*/
-      {"color": "#DC440E", "index": 3, "legendIndex": 3}, /*orange*/
-      {"color": "#D3E2E4", "index": 2, "legendIndex": 4}, /*rot*/
-      {"color": "#C8C8C8", "index": 1, "legendIndex": 5}, /*grau hell*/
-      {"color": "#6F6F6F", "index": 0, "legendIndex": 6}, /*grau*/
-      ],
-
-    "chart": {      
-    	 marginLeft: 135,
-         "inverted": true,
-        "height": 550,       
-
-      },
-    "legend": {
-        "layout": "horizontal",
-        "verticalAlign": "top",
-        "itemMarginBottom": 5,     
-        "align": "left",
-        "useHTML": false,
-        "itemWidth": 165,
-        "itemStyle": {
-            "fontWeight": "normal"
+        "yAxis": {
+            tickInterval: 25,
         },
-        "symbolRadius": 0,
-        "labelFormatter": function () {
-            return this.name.replace('/', '/<br/>');
+        "xAxis": {
+            "type": "category",
+            "labels": {
+                align: "left",
+                x: -125,
+                "formatter": function () {
+                    //add sum of observations of visible series to the axis label
+                    var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+                        return val.visible;
+                    });
+                    var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+                    var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+                        return accumulator + series.yData[indexOfCurrentValue];
+                    }, 0);
+                    //use N if all series are visible, otherwise use n
+                    var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+                    //check for value that contains only spaces
+                    return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + ' (' + nString + sum + ')';
+                }
+            }
+        },
+        plotOptions: {
+            column: {
+                groupPadding: 0,
+
+            }
+        },
+        "series": [
+            { "color": "#246370", "index": 6, "legendIndex": 0 }, /*blau dunkel*/
+            { "color": "#A8C3CA", "index": 5, "legendIndex": 1 }, /*blau mittel*/
+            { "color": "#FFBB58", "index": 4, "legendIndex": 2 }, /*gelb*/
+            { "color": "#DC440E", "index": 3, "legendIndex": 3 }, /*orange*/
+            { "color": "#D3E2E4", "index": 2, "legendIndex": 4 }, /*rot*/
+            { "color": "#C8C8C8", "index": 1, "legendIndex": 5 }, /*grau hell*/
+            { "color": "#6F6F6F", "index": 0, "legendIndex": 6 }, /*grau*/
+        ],
+
+        "chart": {
+            marginLeft: 135,
+            "inverted": true,
+            "height": 550,
+
+        },
+        "legend": {
+            "layout": "horizontal",
+            "verticalAlign": "top",
+            "itemMarginBottom": 5,
+            "align": "left",
+            "useHTML": false,
+            "itemWidth": 165,
+            "itemStyle": {
+                "fontWeight": "normal"
+            },
+            "symbolRadius": 0,
+            "labelFormatter": function () {
+                return this.name.replace('/', '/<br/>');
+            }
         }
-    }
     };
 }());
--- charts/templates/9118.js
+++ charts/templates/9118.js
@@ -1,26 +1,42 @@
-(function(){
+(function () {
     return {
-    series: [
-      { "color": "#71A3B5"}, // blau
-      { "color": "#FABD24"}, // gelb
-      { "color": "#C8C8C8"}, // hellgrau
-      { "color": "#6F6F6F"}, // grau
-    ],
-    xAxis: {
-        type: "category"
-    },
-    yAxis: {
-        reversedStacks: false,
-    },
-    legend: {
-        reversed: false,
-        "layout": "horizontal",
-        "verticalAlign": "top",
-        "itemMarginBottom": 4,
-        "align": "left",
-        labelFormatter: function(){
-            return this.name.replace("/", " /<br/>");
+        series: [
+            { "color": "#71A3B5" }, // blau
+            { "color": "#FABD24" }, // gelb
+            { "color": "#C8C8C8" }, // hellgrau
+            { "color": "#6F6F6F" }, // grau
+        ],
+        xAxis: {
+            type: "category",
+            labels: {
+                "formatter": function () {
+                    //add sum of observations of visible series to the axis label
+                    var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+                        return val.visible;
+                    });
+                    var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+                    var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+                        return accumulator + series.yData[indexOfCurrentValue];
+                    }, 0);
+                    //use N if all series are visible, otherwise use n
+                    var nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+                    //check for value that contains only spaces
+                    return (this.value.replace(/\s/g, "") == "") ? this.value : this.value + '<br>(' + nString + sum + ')';
+                }
+            }
+        },
+        yAxis: {
+            reversedStacks: false,
+        },
+        legend: {
+            reversed: false,
+            "layout": "horizontal",
+            "verticalAlign": "top",
+            "itemMarginBottom": 4,
+            "align": "left",
+            labelFormatter: function () {
+                return this.name.replace("/", " /<br/>");
+            }
         }
-    }
-	};
+    };
 }());
