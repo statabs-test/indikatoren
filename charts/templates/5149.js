e03ec55d52402a7853827875807d79e49397042b
bl99 <bjoern.lietzke@bs.ch>
2020-10-16 12:49:13 +0200

    5149

--- charts/templates/5149.js
+++ charts/templates/5149.js
@@ -1,9 +1,5 @@
 (function () {
   return {
-    chart: {
-    //  marginTop: 50,
-      //  marginRight: 10
-    },
     "xAxis": {
       "tickInterval": 1,
       labels:{
@@ -14,8 +10,6 @@
       "tickInterval": 2,
     },
     "series": [
-      { visible: false, "color": "#008AC3", legendIndex: 0, }, /* grün */
-      { visible: false, "color": "#68AB2B", legendIndex: 1, }, /* blau */
       { "color": "#3C3C3C", legendIndex: 2, }, /* scharz */
     ],
     "legend": {
