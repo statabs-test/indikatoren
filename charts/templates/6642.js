47d93f8ec5b740c2f65be4a054b781e8c8a0dccc
Bestgen <sssbem@bs.ch>
2020-11-13 17:00:33 +0100

    Update

--- charts/templates/6642.js
+++ charts/templates/6642.js
@@ -1,3 +1,43 @@
+(function () {
+  return {
+    xAxis: {
+      tickPositioner: function () {
+        var interval = 2,
+          ext = this.getExtremes(),
+          i = ext.dataMax,
+          pos = [i];
+        while (i >= ext.dataMin) pos.unshift(i = i - interval);
+        return pos;
+      }, 
+      labels:{
+        rotation: 0
+      }
+    },
+    "yAxis": {
+      tickAmount: 5,
+     // max: 40,
+      "labels": {
+        "format": "{value:,.0f}"
+      }
+    },
+    "series": [
+      {"color": "#cd9c00"}
+    ],
+    "legend": {
+      "enabled": false,
+      },
+    "plotOptions": {
+      "line": {
+        "connectNulls": true,
+        "marker": {
+          "enabled": false,
+          "symbol": "circle",
+        }
+      }
+    }
+  }
+}());
+/*
 (function(){
     return {
    "yAxis": {
@@ -25,3 +65,4 @@
 };
 }());
 
+*/
