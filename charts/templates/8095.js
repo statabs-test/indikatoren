69464306dd7216cb64bbda0352149b9abb133354
monteirojaf <25841464+monteirojaf@users.noreply.github.com>
2021-01-14 15:06:48 +0100

    8095 corrected

--- charts/templates/8095.js
+++ charts/templates/8095.js
@@ -3,15 +3,15 @@ global Highcharts
 global geojson_UA_Gemeinden_100
 global rheinData
 */
-(function(){
-    return {
-        "tooltip": {
-            "formatter": function(args){
+(function () {
+	return {
+		"tooltip": {
+			"formatter": function (args) {
 				var this_point_index = this.series.data.indexOf(this.point);
 				return '<span style="color:' + this.color + ';">\u25CF</span> Gemeinde: <b>' + this.point.properties.LIBGEO + '</b>';
 			},
 			useHTML: true
-        },   	
+		},
 		"legend": {
 			"title": {
 				/*"text": "in Einwohner pro km<sup>2</sup>"*/
@@ -21,42 +21,42 @@ global rheinData
 			itemWidth: 500
 		},
 		colorAxis: {
-            dataClassColor: 'category',
-                   dataClasses: [{
-                from: 1,
-                to:1,
-                color: '#FFBB58',
-                name:  "Kernstadt"
-            }, {
-                from: 2,
-                to: 2,
-                color: '#FF8028',
-                name: "Übrige Agglomeration CH"
-            }, {
-                from: 3,
-                to: 3,
-                 color: '#DC440E',
-                 name: "Agglomeration D"
-            },{
-                from: 4,
-                to: 4,
-                 color: '#B00000',
-                 name: "Agglomeration F"
-            }],
+			dataClassColor: 'category',
+			dataClasses: [{
+				from: 1,
+				to: 1,
+				color: '#FFBB58',
+				name: "Kernstadt"
+			}, {
+				from: 2,
+				to: 2,
+				color: '#FF8028',
+				name: "Übrige Agglomeration CH"
+			}, {
+				from: 3,
+				to: 3,
+				color: '#DC440E',
+				name: "Agglomeration D"
+			}, {
+				from: 4,
+				to: 4,
+				color: '#B00000',
+				name: "Agglomeration F"
+			}],
 		},
-        "data": {
-		    "seriesMapping": [
-		      {
-		      	x: 0, y: 2
-		      }	      
-		    ]
-        },
+		"data": {
+			"seriesMapping": [
+				{
+					x: 0, y: 2
+				}
+			]
+		},
 		"series": [
 			{
-				"name": "Perimeter", 
+				"name": "Perimeter",
 				"animation": true,
-				"mapData": geojson_gemeinden_print,
-				"borderColor": "#fbfbfb",		
+				"mapData": geojson_gemeinden,
+				"borderColor": "#fbfbfb",
 				"keys": ['gemeindecode', 'value'],
 				"joinBy": ['CODGEO', 'gemeindecode'],
 				"states": {
@@ -66,11 +66,11 @@ global rheinData
 						"brightness": 0
 					}
 				}
-			}, 
+			},
 			{
 				visible: false,
 				showInLegend: false,
-    			colorAxis: false
+				colorAxis: false
 			}
 		]
 	};
