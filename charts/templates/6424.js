204eeed5a9d5b96cc67de9308c909b42de8f248b
Bestgen <sssbem@bs.ch>
2021-03-24 12:37:23 +0100

    Update

--- charts/templates/6424.js
+++ charts/templates/6424.js
@@ -3,6 +3,7 @@
     "plotOptions": {
       "series": {
         pointPadding: 100,
+        minPointLength: 1,
         borderWidth: 0,
         "pointWidth": 20,
         "dataLabels": {
@@ -39,17 +40,17 @@
       }
     },
     "series": [
-      { "color": "#B00000", index: 10, legendIndex: 0 }, /*Schweiz*/
-      { "color": "#672773", index: 9, legendIndex: 1 }, /*Deutschland*/
-      { "color": "#007a2f", index: 8, legendIndex: 2 }, /*Italien*/
-      { "color": "#fabd24", index: 7, legendIndex: 3 }, /*Türkei*/
-      { "color": "#b475ab", index: 6, legendIndex: 4 }, /*EU-17 & EFTA*/
-      { "color": "#71a3b5", index: 5, legendIndex: 5 }, /*EU-Ost */
-      { "color": "#b9cfd7", index: 4, legendIndex: 6 }, /*Balkan*/
-      { "color": "#ff8028", index: 3, legendIndex: 7 }, /*Mittel- & Südamerika */
-      { "color": "#73ba7c", index: 2, legendIndex: 8 }, /*Arabische Länder */
-      { "color": "#9f7c5a", index: 1, legendIndex: 9 }, /*Afrikanische Länder*/
-      { "color": "#999999", index: 0, legendIndex: 10 }, /*Übrige Länder*/
+      { "color": "#B00000", index: 10, legendIndex: 0, visible: true}, /*Schweiz*/
+      { "color": "#672773", index: 9, legendIndex: 1, visible: true }, /*Deutschland*/
+      { "color": "#007a2f", index: 8, legendIndex: 2, visible: true }, /*Italien*/
+      { "color": "#fabd24", index: 7, legendIndex: 3, visible: false }, /*Türkei*/
+      { "color": "#b475ab", index: 6, legendIndex: 4, visible: true }, /*EU-17 & EFTA*/
+      { "color": "#71a3b5", index: 5, legendIndex: 5, visible: false }, /*EU-Ost */
+      { "color": "#b9cfd7", index: 4, legendIndex: 6, visible: true }, /*Balkan*/
+      { "color": "#ff8028", index: 3, legendIndex: 7, visible: true }, /*Mittel- & Südamerika */
+      { "color": "#73ba7c", index: 2, legendIndex: 8, visible: false }, /*Arabische Länder */
+      { "color": "#9f7c5a", index: 1, legendIndex: 9, visible: false }, /*Afrikanische Länder*/
+      { "color": "#999999", index: 0, legendIndex: 10, visible: false }, /*Übrige Länder*/
     ],
     "chart": {
       "renderTo": 'container-I.01.1.0016',
