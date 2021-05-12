09d700e21b3f1a97192cc9ec6282eda0bea3c0ec
Bestgen <sssbem@bs.ch>
2021-05-12 15:25:37 +0200

    Update

--- charts/templates/8242.js
+++ charts/templates/8242.js
@@ -15,23 +15,34 @@
     "type": "category"    
   },  
   "yAxis": {
+    tickInterval: 1000,
     "labels": {
       "format": "{value:,.0f}"
     },
     "reversedStacks": false
   },
+  tooltip: {
+    pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b> ({point.percentage:,.1f}%)<br/>',
+    footerFormat: '<span style="color: black">\u25CF</span> Total: <b>{point.total:,.0f}</b>',
+    shared: false
+},
+  /*
   "tooltip": {
     "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
     "shared": false
-  },  
+  },*/  
   "legend": {
-    "layout": "vertical",
-    "verticalAlign": "middle",
+    "enabled": true,
+    "layout": "horizontal",
+    "verticalAlign": "top",
     "itemMarginBottom": 5,
-    "align": "right",
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
+    "align": "left",
+    itemWidth: 200,
+width: 400,
+itemStyle: {
+textOverflow: "none",
+whiteSpace: "nowrap"
+}
   },
   "series": [
     {"color": "#662673", index: 9, legendIndex: 0}, /**/
