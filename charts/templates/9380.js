a83402481f437798f725fd9cb60af83f52f0cee9
ksz91 <kevin.zaugg@bs.ch>
2021-10-14 17:43:17 +0200

    some changes

--- charts/templates/9380.js
+++ charts/templates/9380.js
@@ -1,9 +1,10 @@
 (function(){
     return {
  "series": [
-  {"color": "rgb(103,39,115)", index: 2, legendIndex: 0}, /*Sek1*/
-  {"color": "rgb(180,117,171)", index: 1, legendIndex: 1}, /*Sek2*/
-  {"color": "rgb(231,207,226)", index: 0, legendIndex:2}, /*Tert.*/
+  {"color": "rgb(180,117,171)",legendIndex:1},
+  {"color": "rgb(103,39,115)", legendIndex:0}, 
+ 
+  //{"color": "rgb(231,207,226)", index: 0, legendIndex:2}, 
 ],
   "chart": {
       type: "column",
