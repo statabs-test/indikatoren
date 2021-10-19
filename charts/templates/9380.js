136b49dad4d677fd59ce153e7a69de72a9ed8a18
ksz91 <kevin.zaugg@bs.ch>
2021-10-19 16:26:59 +0200

    Update

--- charts/templates/9380.js
+++ charts/templates/9380.js
@@ -1,75 +1,135 @@
-(function(){
-    return {
- "series": [
- 
-  {"color": "rgb(180,117,171)",index: 1, legendIndex:1},
-  {"color": "rgb(103,39,115)", index: 2, legendIndex:0}, 
-  {"color": "rgb(250,189,36)", index: 0, legendIndex:2, visible: false},
- 
-  //{"color": "rgb(231,207,226)", index: 0, legendIndex:2}, 
-],
-  "chart": {
-      type: "column",
-    "inverted": true,
-    marginRight: 25,
-    marginLeft: 210,
-  }, 
-  subtitle: {
-    useHTML: true,
-  },
-  legend: {
-    itemDistance: 5,
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-	  //"y": 45,
-    "itemStyle": {
-      "fontWeight": "normal"
-    }
-  },  
-  xAxis: {
-    labels: {
-      //useHTML: false,
-      formatter: function () {
-        //add sum of observations of visible series to the axis label
-        var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
-            return val.visible;
-        });
-        var indexOfCurrentValue = this.axis.names.indexOf(this.value);
-        var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
-            return accumulator + series.yData[indexOfCurrentValue];
-        }, 0);
-        //use N if all series are visible, otherwise use n
-        var nString =  'n=';
-          if (this.value.match(/Total/)) nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+/*
+global Highcharts
+*/
 
-        //delete everything before ":", including ":"
-        this.value = this.value.replace(/[^:]*:/, "");
-
-        //check for value that contains only spaces
-        if (sum != 0) return this.value;
-        //else, if sum = 0, then it is assumed to be an intermediate title. return it bold
-        return "<b>" + this.value + "</b>";
-    }
-    }
-  },
-  yAxis: {
-    //tickAmount: 7
-  },
-    "tooltip": {
-        "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-        "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
-         "shared": true
+(function () {
+  return {
+    //seriesMapping necessary for charts with error bars. 
+    "data": {
+      "seriesMapping": [
+        {
+          "x": 0
+        },
+        {
+          "x": 0
+        },
+        {
+          "x": 0
+        },
+        {
+          "x": 0
+        },
+        {
+          "x": 0
+        },
+       /* {
+          "x": 0
+        },
+        {
+          "x": 0
+        },
+        {
+          "x": 0
+        }*/
+      ]
     },
-  plotOptions:{
-     column: {
-      borderWidth: 0,
-      groupPadding: 0.1,
-         stacking: 'normal',
-  }
-}
-};
+    plotOptions: {
+      series: {
+        stacking:"normal",
+        //pointPadding: 0,
+        borderWidth: 0
+      },
+    },
+    tooltip: {
+      shared: true
+    },
+    "series": [
+      {
+        "index": 0,
+        "color": "rgb(180,117,171)",
+        "tooltip": {
+          "pointFormatter": function () {
+            return '<span style="color:' + this.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y), 0) + '</b>';
+          }
+        }
+      },
+      /*{
+        "index": 1,
+        "type": "errorbar",
+        "tooltip": {
+          "pointFormatter": function () {
+            return ' (95%-Vertrauensintervall: <b>' + Highcharts.numberFormat((this.low), 0) + '</b> - <b>' + Highcharts.numberFormat((this.high), 0) + '</b>)';
+          },
+        }
+      },*/
+      {
+        "index": 2,
+        "color": "rgb(103,39,115)", 
+        "tooltip": {
+          "pointFormatter": function () {
+            return '<br/>' + '<span style="color:' + this.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y), 0) + '</b>';
+          }
+        }
+
+      },
+      /*{
+        "index": 3,
+        "type": "errorbar",
+        "tooltip": {
+          "pointFormatter": function () {
+            return ' (95%-Vertrauensintervall: <b>' + Highcharts.numberFormat((this.low), 0) + '</b> - <b>' + Highcharts.numberFormat((this.high), 0) + '</b>)';
+          }
+        }
+      },*/
+      {
+        "index": 4,
+        visible: false,
+        "color": "rgb(250,189,36)",
+        "tooltip": {
+          "pointFormatter": function () {
+            return '<br/>' + '<span style="color:' + this.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y), 0) + '</b>';
+          }
+        }
+      },
+      /*{
+        "index": 5,
+        visible: false,
+        "type": "errorbar",
+        "tooltip": {
+          "pointFormatter": function () {
+            return ' (95%-Vertrauensintervall: <b>' + Highcharts.numberFormat((this.low), 0) + '</b> - <b>' + Highcharts.numberFormat((this.high), 0) + '</b>)';
+          }
+        }
+      },*/
+    ],
+    "xAxis": {
+      "type": "category"
+    },
+    "yAxis": {
+      "labels": {
+        "formatter": function () {
+          return Highcharts.numberFormat((this.value), 0);
+        },
+      }
+    },
+    "legend": {
+      "enabled": true,
+      itemDistance: 5,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    "chart": {
+      "marginBottom": 65,
+      //"marginTop": 75,
+      "type": "column",
+      "inverted": false
+    },
+    subtitle: {
+      useHTML: true
+    }
+  };
 }());
-
-
