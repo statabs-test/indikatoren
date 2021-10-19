211665991ee9e851e8e0c6f3896569e5d4cbbe55
ksz91 <kevin.zaugg@bs.ch>
2021-10-19 10:31:04 +0200

    Update

--- charts/templates/9380.js
+++ charts/templates/9380.js
@@ -1,15 +1,18 @@
 (function(){
     return {
  "series": [
-  {"color": "rgb(180,117,171)",legendIndex:1},
-  {"color": "rgb(103,39,115)", legendIndex:0}, 
+ 
+  {"color": "rgb(180,117,171)",index: 1, legendIndex:1},
+  {"color": "rgb(103,39,115)", index: 2, legendIndex:0}, 
+  {"color": "rgb(250,189,36)", index: 0, legendIndex:2, visible: false},
  
   //{"color": "rgb(231,207,226)", index: 0, legendIndex:2}, 
 ],
   "chart": {
       type: "column",
     "inverted": true,
-    marginRight: 25
+    marginRight: 25,
+    marginLeft: 210,
   }, 
   subtitle: {
     useHTML: true,
@@ -27,14 +30,32 @@
   },  
   xAxis: {
     labels: {
-      useHTML: false,
-      formatter: function() {
-      	return this.value;//.replace("Eingebürgerte ", "Eingebürgerte<br/>");
-      }
+      //useHTML: false,
+      formatter: function () {
+        //add sum of observations of visible series to the axis label
+        var allVisibleSeries = this.chart.series.filter(function (val, i, arr) {
+            return val.visible;
+        });
+        var indexOfCurrentValue = this.axis.names.indexOf(this.value);
+        var sum = allVisibleSeries.reduce(function (accumulator, series, index, arr) {
+            return accumulator + series.yData[indexOfCurrentValue];
+        }, 0);
+        //use N if all series are visible, otherwise use n
+        var nString =  'n=';
+          if (this.value.match(/Total/)) nString = (this.chart.series.length == allVisibleSeries.length) ? 'N=' : 'n=';
+
+        //delete everything before ":", including ":"
+        this.value = this.value.replace(/[^:]*:/, "");
+
+        //check for value that contains only spaces
+        if (sum != 0) return this.value;
+        //else, if sum = 0, then it is assumed to be an intermediate title. return it bold
+        return "<b>" + this.value + "</b>";
+    }
     }
   },
   yAxis: {
-    tickAmount: 7
+    //tickAmount: 7
   },
     "tooltip": {
         "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
@@ -43,7 +64,9 @@
     },
   plotOptions:{
      column: {
-        // stacking: 'normal',
+      borderWidth: 0,
+      groupPadding: 0.1,
+         stacking: 'normal',
   }
 }
 };
