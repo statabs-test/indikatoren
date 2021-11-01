7b1c44d577bf295fcb95d7c97a9c90e452e8b87b
ksz91 <kevin.zaugg@bs.ch>
2021-11-01 11:43:19 +0100

    Change colors 9393

--- charts/templates/9393.js
+++ charts/templates/9393.js
@@ -1,118 +1,42 @@
-(function(){
+(function () {
   return {
-    title: {
-      useHTML: true
+    "chart": {
+      "type": "column"
     },
     "plotOptions": {
       "series": {
-        "borderWidth": 0,
-        /*"pointWidth": 30,*/
-        "dataLabels": {
-          "style": {
-            "fontSize": "10px"
-          }
-        },
         "stacking": "percent"
       }
     },
-    "xAxis": {
-      "type": "category",
-  	//"tickInterval": 1	
-    },  
-    "yAxis": [{
-    	title: {
-          text: null
-      },
+    "yAxis": {
       "labels": {
         "format": "{value:,.0f}%",
-         "style": {
-         "color": "black"
-          },
-      },
-      "min": 0
+      }
     },
-   /* {
-      title: {
-          text: null
-      },
-      "labels": {
-        "format": "{value:,.0f}%",
-         "style": {
-         "color": "black"
-          },
-      },
-      "min": 0,
-      "opposite": true
-    }*/
-    ],
     "series": [
       {
-  	  "index": 1,
-  	  "color": "#661200",
-        "legendIndex": 1,
-        "type": "column",
-        /*"tooltip": {
-      		"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)</b><br/>',
-          shared: true
-         }*/
+        "color": "#a8c3ca",
       },
       {
-        "index": 2,
-        "color": "#DC440E",
-          "legendIndex": 2,
-          "type": "column",
-          /*"tooltip": {
-            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)</b><br/>',
-            shared: true
-           }*/
-      },
-
-      {
-        "index": 3,
-        "color": "#083038",
-          "legendIndex": 3,
-          "type": "column",
-          /*"tooltip": {
-            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)</b><br/>',
-            shared: true
-           }*/
+        "color": "#ff8028",
       },
       {
-        "index": 4,
-        "color": "#689199",
-          "legendIndex": 4,
-          "type": "column",
-          /*"tooltip": {
-            "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)<br/>',
-            shared: true
-           }*/
+        "color": "#dc440e",
+      },
+      {
+        "color": "#246370",
       }
     ],
     tooltip: {
       pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:,.1f}%)<br/>',
       footerFormat: 'Total: <b>{point.total:,.0f}</b>',
       shared: true,
-      //useHTML: false
     },
-
     "legend": {
-      //useHTML: false,
-      "enabled": true,
-      //"y": 35,
       "layout": "horizontal",
       "verticalAlign": "top",
       "itemMarginBottom": 5,
       "align": "left",
-      "itemStyle": {
-       "fontWeight": "normal"
-      }
     },
-    "chart": {    
-      //"marginBottom": 75,
-      //"marginTop": 75,
-      "type": "column"
-    }
   };
 }());
-
-
