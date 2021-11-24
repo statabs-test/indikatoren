b1d02fa525e9156959c2c88cf7096daf07bedca9
bl99 <bjoern.lietzke@bs.ch>
2021-11-24 18:34:55 +0100

    9375

--- charts/templates/9375.js
+++ charts/templates/9375.js
@@ -1,45 +1,38 @@
 (function () {
-    return {
-      "xAxis": {
-        tickInterval: 1
-      },
-      "yAxis": {
-          tickAmount: 7,
-        "labels": {
-          "format": "{value:,.0f}"
-        }
-      },
-      "tooltip": {
-        "shared": false,
-        "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
-      }, 
-      "series": [
-        { "color": "rgb(0,138,195)" },
-        { "color": "rgb(147,63,141)" },
-      ],
-      "legend": {
-        "enabled": true,
-        "x": 30,
-        //"y": 35,
-        //"itemWidth": 145,
-        "layout": "horizontal",
-        "verticalAlign": "top",
-        "itemMarginBottom": 5,
-        "align": "left",
-        "itemStyle": {
-          "fontWeight": "normal"
-        }
-      },
-      "plotOptions": {
-        "line": {
-          "connectNulls": true,
-          "marker": {
-            "enabled": false,
-            "symbol": "circle",
-            "radius": 3
-          }
+  return {
+    "xAxis": {
+      tickInterval: 1
+    },
+    "yAxis": {
+      min: null,
+      "labels": {
+        "format": "{value:,.0f}"
+      }
+    },
+    "tooltip": {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+    },
+    "series": [
+      { "color": "rgb(0,138,195)" },
+      { "color": "rgb(147,63,141)" },
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      "itemMarginBottom": 5,
+      "x": 30,
+      //"y": 35,
+      //"itemWidth": 145,
+    },
+    "plotOptions": {
+      "line": {
+        "connectNulls": true,
+        "marker": {
+          "symbol": "circle",
         }
       }
-    };
-  }());
-  
+    }
+  };
+}());
