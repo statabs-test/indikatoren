3cd59f4307f3c8443b05c0549ffa39cb4e8a1af6
bl99 <bjoern.lietzke@bs.ch>
2021-11-26 18:13:49 +0100

    9391

--- charts/templates/9391.js
+++ charts/templates/9391.js
@@ -1,5 +1,8 @@
-(function(){
+(function () {
   return {
+    "chart": {
+      "type": "column"
+    },
     title: {
       useHTML: true
     },
@@ -7,92 +10,96 @@
       "series": {
         "borderWidth": 0,
         /*"pointWidth": 30,*/
-        "dataLabels": {
-          "style": {
-            "fontSize": "10px"
-          }
-        },
         "stacking": "true"
       }
     },
     "xAxis": {
       "type": "category",
-  	//"tickInterval": 1	
-    },  
+      //"tickInterval": 1	
+    },
     "yAxis": [{
-    	title: {
-          text: null
+      title: {
+        text: null
       },
       "labels": {
         "format": "{value:,.0f}",
-         "style": {
-         "color": "black"
-          },
+        "style": {
+          "color": "black"
+        },
       },
-      "min": 0,  },
+      "min": 0,
+    },
     {
       title: {
-          text: null
+        text: null
       },
       "labels": {
         "format": "{value:,.0f}%",
-         "style": {
-         "color": "black"
-          },
+        "style": {
+          "color": "black"
+        },
       },
       "min": 0,
       "opposite": true
     }
     ],
+    "legend": {
+      useHTML: true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "itemMarginBottom": 5,
+      "align": "left",
+      labelFormatter: function(){
+        return this.name.replace("-elektrisch", "-elektr.");
+      },
+    },
     "series": [
       {
-  	  "index": 1,
-  	  "color": "#2F656B",
+        "index": 1,
+        "color": "#2F656B",
         "legendIndex": 1,
         "type": "column",
         "tooltip": {
-      		"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
+          "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
           shared: true
-         }
+        }
       }, // Blau
       {
         "index": 2,
         "color": "#8B2223",
-          "legendIndex": 2,
-          "type": "column"/*,
+        "legendIndex": 2,
+        "type": "column"/*,
           "tooltip": {
             "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
            }*/
-        },
-        {
-          "index": 3,
-          "color": "#83522E",
-            "legendIndex": 3,
-            "type": "column"/*,
+      },
+      {
+        "index": 3,
+        "color": "#83522E",
+        "legendIndex": 3,
+        "type": "column"/*,
             "tooltip": {
               "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
              }*/
-          },
-          {
-            "index": 4,
-            "color": "#9A86A6",
-              "legendIndex": 4,
-              "type": "column"/*,
+      },
+      {
+        "index": 4,
+        "color": "#9A86A6",
+        "legendIndex": 4,
+        "type": "column"/*,
               "tooltip": {
                 "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>'
                }*/
-            },
-            {
-              "index": 5,
-              "color": "#BAA85A",
-                "legendIndex": 5,
-                "type": "column"/*,
+      },
+      {
+        "index": 5,
+        "color": "#BAA85A",
+        "legendIndex": 5,
+        "type": "column"/*,
                 "tooltip": {
                   "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>'
                  }*/
-              },
-  
-
+      },
       {
         index: 6,
         "color": "#000000",
@@ -101,35 +108,17 @@
         "yAxis": 1,
         "marker": {
           "enabled": false
-      },
-      "tooltip": {
-      		"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}%</b><br/>',
+        },
+        "tooltip": {
+          "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}%</b><br/>',
           shared: false
-       }
+        }
       }
     ],
     tooltip: {
       pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>', "footerFormat": 'Total neue Inverkehrsetzungen von Elektroautos: <b>{point.total:,.0f}</b>',
       shared: true,
       useHTML: true
-    },
-
-    "legend": {
-      useHTML: true,
-      "enabled": true,
-      //"y": 35,
-      "layout": "horizontal",
-      "verticalAlign": "top",
-      "itemMarginBottom": 5,
-      "align": "left",
-      "itemStyle": {
-       "fontWeight": "normal"
-      }
-    },
-    "chart": {    
-      //"marginBottom": 75,
-      //"marginTop": 75,
-      "type": "column"
     }
   };
 }());
