b3356a3405cdef27e1004c427a4aca960d19f804
bl99 <bjoern.lietzke@bs.ch>
2021-11-29 15:16:13 +0100

    9368, 9381

--- charts/templates/9368.js
+++ charts/templates/9368.js
@@ -1,62 +1,61 @@
-(function(){
-    return {
-"plotOptions": {
-    "series": {
-       borderWidth: 0,
-       pointPadding: 0,
-       groupPadding: 0.1,
-      //"pointWidth": 30,
-      "stacking": "normal"
-    }
-  },
-  "xAxis": {
-    "type": "category",
-    "tickInterval": 1,
-    "labels": {
-      rotation: 0
-    }
-  },  
-  "yAxis": {
+(function () {
+  return {
+    "chart": {
+      "type": "column"
+    },
+    "plotOptions": {
+      "series": {
+        borderWidth: 0,
+        pointPadding: 0,
+        groupPadding: 0.1,
+        //"pointWidth": 30,
+        "stacking": "normal"
+      }
+    },
+    "xAxis": {
+      "type": "category",
+      "tickInterval": 1,
       "labels": {
-      "format": "{value:,.0f}",
+        rotation: 0
+      }
     },
-	"max": undefined
-  },
-  "legend": {
-    "layout": "vertical",
-    "verticalAlign": "middle",
-    "itemMarginBottom": 5,
-    "align": "right",
-    width: 150,
-    "itemStyle": {
-      textOverflow: null,
-      "fontWeight": "normal"
+    "yAxis": {
+      "labels": {
+        "format": "{value:,.0f}",
+      },
+      "max": undefined
     },
-    labelFormatter: function(){
-      return this.name.replace("Unterhaltungseinrichtungen", "Unterhaltungs-einrichtungen")
-    }
-  },
-  "series": [
- 
-  {"color": "#007A2F"}, 
-  {"color": "#68AB2B"},
-  {"color": "#D7E8D2"}, 
-  {"color": "#2B0099"}, 
-  {"color": "#008AC3"}, 
-  {"color": "#689199"}, 
-  {"color": "#D3E2E4"}, 
-  ],
-  "chart": {
-    //"renderTo": 'container-I.01.1.0016', 
-    //"marginBottom": 75,
-    //"marginTop": 75,
-    "type": "column",
-    "inverted": false
-  },
-  "tooltip": {
-  "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:.0f}</b><br/>',
-  "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
-   "shared": true
-  },
-}
+    "legend": {
+      layout: "vertical",
+      verticalAlign: "middle",
+      align: "right",
+      width: 150,
+      itemMarginBottom: 5,
+      itemStyle: {
+        textOverflow: null,
+      },
+      labelFormatter: function () {
+        return this.name.replace("Unterhaltungseinrichtungen", "Unterhaltungs-einrichtungen")
+      }
+    },
+    "tooltip": {
+      shared: true,
+      useHTML: true,
+      followPointer: true,
+      headerFormat: "<table>",
+      pointFormat: '<tr><td><span style="color:{series.color}">\u25CF</span> {series.name}:</td>'
+        + '<td style="text-align:right">&nbsp;<b>{point.y}</b></td>'
+        + '<td style="text-align:right">&nbsp;({point.percentage:,.1f}%)</td></tr>',
+      footerFormat: '<tr><td>Total: </td><td><b>{point.total:,.0f}</b></td><td></td></tr></table>',
+    },
+    "series": [
+      { "color": "#007A2F" },
+      { "color": "#68AB2B" },
+      { "color": "#D7E8D2" },
+      { "color": "#2B0099" },
+      { "color": "#008AC3" },
+      { "color": "#689199" },
+      { "color": "#D3E2E4" },
+    ]
+  }
 }());
