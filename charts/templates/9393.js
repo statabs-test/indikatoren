d866ed45d1b721a9322a5a5bda1f36c59fd33051
bl99 <bjoern.lietzke@bs.ch>
2021-12-14 08:54:41 +0100

    9393

--- charts/templates/9393.js
+++ charts/templates/9393.js
@@ -1,32 +1,32 @@
 (function () {
   return {
-    "chart": {
-      "type": "column"
+    chart: {
+      type: "column",
+      events: {
+        load: function () {
+          //add and move legend title (get it from series names)
+          this.legend.title
+            .attr({
+              text:
+                this.series[0].name.split(" (").slice(-1).toString().replace(')','')
+                + ':<br>'
+                + this.series[2].name.split(" (").slice(-1).toString().replace(')','')
+                + ':'
+            })
+            .translate(-110, 42);
+        }
+      },
     },
-    "plotOptions": {
-      "series": {
-        "stacking": "percent"
+    plotOptions: {
+      series: {
+        stacking: "percent"
       }
     },
-    "yAxis": {
-      "labels": {
-        "format": "{value:,.0f}%",
+    yAxis: {
+      labels: {
+        format: "{value:,.0f}%",
       }
     },
-    "series": [
-      {
-        "color": "#a8c3ca",
-      },
-      {
-        "color": "#ff8028",
-      },
-      {
-        "color": "#dc440e",
-      },
-      {
-        "color": "#246370",
-      }
-    ],
     tooltip: {
       shared: true,
       useHTML: true,
@@ -37,11 +37,48 @@
         + '<td style="text-align:right">&nbsp;({point.percentage:,.1f}%)</td></tr>',
       footerFormat: '<tr><td>Total: </td><td><b>{point.total:,.0f} GWh</b></td><td></td></tr></table>',
     },
-    "legend": {
-      "layout": "horizontal",
-      "verticalAlign": "top",
-      "itemMarginBottom": 5,
-      "align": "left",
+    legend: {
+      layout: "horizontal",
+      verticalAlign: "top",
+      align: "right",
+      y: -40,
+      x: -55,
+      width: 250,
+      itemWidth: 100,
+      itemMarginBottom: 5,
+      itemStyle: {
+        textOverflow: 'none',
+        whiteSpace: 'nowrap',
+      },
+      labelFormatter: function () {
+        //remove text after " ("
+        return this.name.split(" (").slice(0, 1).toString();
+      },
+      title: {
+        text: '-<br>-', //wird via chart.events.load bearbeitet
+        style: {
+          fontWeight: 'normal',
+          lineHeight: 19
+        }
+      },
     },
+    series: [
+      {
+        color: "#a8c3ca",
+        legendIndex: 2
+      },
+      {
+        color: "#ff8028",
+        legendIndex: 1
+      },
+      {
+        color: "#dc440e",
+        legendIndex: 3
+      },
+      {
+        color: "#246370",
+        legendIndex: 4
+      }
+    ],
   };
 }());
