f03e9d889f053f27d2f193a383b5939306adbe46
ksz91 <kevin.zaugg@bs.ch>
2021-12-14 09:44:34 +0100

    Chart 9450

--- charts/templates/9450.js
+++ charts/templates/9450.js
@@ -1,34 +1,32 @@
 (function () {
   return {
-    "chart": {
-      "type": "column"
+    chart: {
+      type: "column",
+      events: {
+        load: function () {
+          //add and move legend title (get it from series names)
+          this.legend.title
+            .attr({
+              text:
+                this.series[0].name.split(" (").slice(-1).toString().replace(')','')
+                + ':<br>'
+                + this.series[2].name.split(" (").slice(-1).toString().replace(')','')
+                + ':'
+            })
+            .translate(-90, 28);
+        }
+      },
     },
-    "plotOptions": {
-      "series": {
-        pointPadding: 0.2,
-        "stacking": "percent"
+    plotOptions: {
+      series: {
+        stacking: "percent"
       }
     },
-    "yAxis": {
-      tickAmount: 5,
-      "labels": {
-        "format": "{value:,.0f}%",
+    yAxis: {
+      labels: {
+        format: "{value:,.0f}%",
       }
     },
-    "series": [
-      {
-        "color": "#a8c3ca",
-      },
-      {
-        "color": "#ff8028",
-      },
-      {
-        "color": "#dc440e",
-      },
-      {
-        "color": "#246370",
-      }
-    ],
     tooltip: {
       shared: true,
       useHTML: true,
@@ -39,10 +37,49 @@
         + '<td style="text-align:right">&nbsp;({point.percentage:,.1f}%)</td></tr>',
       footerFormat: '<tr><td>Total: </td><td><b>{point.total:,.0f} GWh</b></td><td></td></tr></table>',
     },
-    "legend": {
-      "layout": "horizontal",
-      "verticalAlign": "top",
-      "align": "left",
+    legend: {
+      layout: "horizontal",
+      verticalAlign: "top",
+      align: "right",
+      y: -30,
+      x: 65,
+      width: 250,
+      itemWidth: 90,
+      itemDistance: 0,
+      itemStyle: {
+        textOverflow: 'none',
+        whiteSpace: 'nowrap',
+      },
+      labelFormatter: function () {
+        //remove text after " ("
+        return this.name.split(" (").slice(0, 1).toString()
+        .replace("Andere Energieträger", "Andere");
+      },
+      title: {
+        text: '-<br>-', //wird via chart.events.load bearbeitet
+        style: {
+          fontWeight: 'normal',
+          lineHeight: 14
+        }
+      },
     },
+    series: [
+      {
+        color: "#a8c3ca",
+        legendIndex: 2
+      },
+      {
+        color: "#ff8028",
+        legendIndex: 1
+      },
+      {
+        color: "#dc440e",
+        legendIndex: 3
+      },
+      {
+        color: "#246370",
+        legendIndex: 4
+      }
+    ],
   };
 }());
