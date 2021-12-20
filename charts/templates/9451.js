649eccc6273c8cb0b9cdc10f19ba382b4336d030
bl99 <bjoern.lietzke@bs.ch>
2021-12-20 16:54:50 +0100

    9451

--- charts/templates/9451.js
+++ charts/templates/9451.js
@@ -1,10 +1,27 @@
 (function () {
     return {
         "chart": {
-            "type": "column"
+            "type": "column",
+            events: {
+                load: function () {
+                    //add and move legend title (get it from series names)
+                    this.legend.title
+                        .attr({
+                            text:
+                                this.series[1].name.split(", ").slice(0, 1).toString()
+                                + ':<br>'
+                                + this.series[3].name.split(", ").slice(0, 1).toString().replace('Organisationen', 'Organis.')
+                                + " ("
+                                +this.series[3].name.split(" (").slice(-1).toString()
+                                + ':'
+                        })
+                        .translate(-170, 28);
+                }
+            }
         },
         plotOptions: {
             series: {
+                minPointLength: 2,
                 marker: {
                     enabled: false
                 }
@@ -21,7 +38,8 @@
                     useHTML: true,
                     "format": "{value:,.0f}",
                     "style": {
-                        "color": "#000000"
+                        "color": "#000000",
+                        fontSize: "10px"
                     }
                 },
                 "min": 0,
@@ -37,7 +55,8 @@
                 "labels": {
                     "format": "{value:,.0f}",
                     "style": {
-                        "color": "#000000"
+                        "color": "#000000",
+                        fontSize: "10px"
                     }
                 },
                 "min": 0,
@@ -52,27 +71,63 @@
             "enabled": true,
             "layout": "horizontal",
             "verticalAlign": "top",
-            "align": "left",
+            align: "right",
+            y: -30,
+            x: 40,
+            width: 180,
+            itemWidth: 75,
+            itemMarginBottom: 5,
             itemStyle: {
-                textOverflow: "none"
-            }
+                textOverflow: 'none',
+                whiteSpace: 'nowrap',
+            },
+            labelFormatter: function () {
+                //remove text after " ("
+                var leg = this.name.split(", ").slice(1).toString().split(" (").slice(0,1).toString();
+                if (leg.length < 1) leg = this.name;
+                return leg;
+            },
+            title: {
+                text: '-<br>-', //wird via chart.events.load bearbeitet
+                style: {
+                    fontWeight: 'normal',
+                    lineHeight: 16
+                }
+            },
         },
         tooltip: {
             "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: ,.0f}</b><br/>'
         },
         "series": [
             {
-                "color": "#DC440E",
+                "color": "#A8C3CA",
                 "index": 0,
                 zIndex: 2,
+                legendIndex: 5,
                 //"type": "line",
                 // tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: ,.0f}</b><br/>' }
             },
             {
-                "color": "#FABD24",
+                "color": "#FF8028",
                 "index": 1,
                 zIndex: 2,
-                // tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .0f}</b><br/>' }
+                "stacking": "normal",
+                tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .0f}</b><br/>Total: <b>{point.total:,.0f}</b>' }
+            },
+            {
+                "color": "#689199",
+                "index": 2,
+                zIndex: 3,
+                "stacking": "normal",
+                tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: ,.0f}</b><br/>Total: <b>{point.total:,.0f}</b>' }
+            },
+            {
+                "color": "#DC440E",
+                "index": 2,
+                zIndex: 3,
+                "type": "line",
+                "yAxis": 1,
+                //  tooltip: { "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: ,.0f}</b><br/>' }
             },
             {
                 "color": "#246370",
