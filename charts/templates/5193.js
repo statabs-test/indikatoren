f02fec231cb6a99f31bc857bd8e89dc3bde9d008
bl99 <bjoern.lietzke@bs.ch>
2021-12-22 17:24:02 +0100

    5193

--- charts/templates/5193.js
+++ charts/templates/5193.js
@@ -26,7 +26,7 @@
       "itemMarginBottom": 5,
     },
     "tooltip": {
-      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
       "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
     },
     "series": [
