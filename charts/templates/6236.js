2e4dbc5848c738f1d342edf94b7191de7525d7b9
ksz91 <kevin.zaugg@bs.ch>
2022-01-18 16:51:47 +0100

    Shared tooltip

--- charts/templates/6236.js
+++ charts/templates/6236.js
@@ -44,8 +44,11 @@
   {"color": "#b00000", "index": 0, "legendIndex":0} /*rot dunkel */
   ],
   "tooltip": {
-    "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b><br/>',
-  },
+    "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
+    "footerFormat": 'Total: <b>{point.total:,.0f}</b>',
+     "shared": true
+    },
+    
   "chart": {
   	type: "column",
     "inverted": false,
