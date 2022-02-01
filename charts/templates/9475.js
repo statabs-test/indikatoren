9877116ac70532a5632164cfb3bc520f79bc0c2c
NiklausBaltis <niklaus.baltisberger@bs.ch>
2022-02-01 11:20:39 +0100

    New data labels

--- charts/templates/9475.js
+++ charts/templates/9475.js
@@ -1,155 +1,155 @@
 (function () {
     return {
-  
-      "yAxis": {
+      yAxis: {
         tickAmount: 7,
         showLastLabel: true,
         endOnTick: true,
-  
+
         labels: {
           format: "{value:,.0f}",
         },
         opposite: false,
       },
-      "xAxis": {
+      xAxis: {
         endOnTick: true,
         startOnTick: true,
         showFirstLabel: true,
         showLastLabel: true,
         tickInterval: 365 * 24 * 3600 * 1000,
         minTickInterval: 365 * 24 * 3600 * 1000,
-        ordinal: false
+        ordinal: false,
       },
-      "tooltip": {
+      tooltip: {
         enabled: true,
         shared: true,
         split: false,
-        borderColor: '#000000',
-        xDateFormat: '%Y',
-        "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+        borderColor: "#000000",
+        xDateFormat: "%Y",
+        pointFormat:
+          '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>',
       },
-      
-      "series": [
+
+      series: [
         {
-          "color": "#9E7C59",
+          color: "#9E7C59",
           showInNavigator: true,
-          "marker": {
-            "symbol": "circle",
-            "enabled": false
-          }
-        }, 
-        {
-          "color": "#689199",
-          visible: false,
-          showInNavigator: true,
-          "marker": {
-            "symbol": "circle",
-            "enabled": false
-          }
-        }, 
-        {
-          "color": "#DC440E",
-          visible: false,
-          showInNavigator: true,
-          "marker": {
-            "symbol": "circle",
-            "enabled": false
-          }
+          marker: {
+            symbol: "circle",
+            enabled: false,
+          },
         },
         {
-            "color": "#923F8D",
-            showInNavigator: true,
-            "marker": {
-              "symbol": "circle",
-              "enabled": false
-            }
-          }, 
-          {
-            "color": "#CD9C00",
-            visible: false,
-            showInNavigator: true,
-            "marker": {
-              "symbol": "circle",
-              "enabled": false
-            }
-          }, 
-          {
-            "color": "#B6CFD7",
-            visible: false,
-            showInNavigator: true,
-            "marker": {
-              "symbol": "circle",
-              "enabled": false
-            }
+          color: "#689199",
+          visible: true,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
           },
-          {
-            "color": "#68AB2B",
-            showInNavigator: true,
-            "marker": {
-              "symbol": "circle",
-              "enabled": false
-            }
+        },
+        {
+          color: "#DC440E",
+          visible: true,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
           },
-  
+        },
+        {
+          color: "#923F8D",
+          visible: false,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
+          },
+        },
+        {
+          color: "#CD9C00",
+          visible: false,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
+          },
+        },
+        {
+          color: "#B6CFD7",
+          visible: false,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
+          },
+        },
+        {
+          color: "#68AB2B",
+          visible: false,
+          showInNavigator: true,
+          marker: {
+            symbol: "circle",
+            enabled: false,
+          },
+        },
       ],
-      "legend": {
-        "enabled": true,
+      legend: {
+        enabled: true,
         //"x": 50,
         //"y": 25,
         //"itemWidth": 150,
-        "layout": "horizontal",
-        "verticalAlign": "top",
-        "itemMarginBottom": 5,
-        "align": "left",
-        "itemStyle": {
-          "fontWeight": "normal"
-        }
+        layout: "horizontal",
+        verticalAlign: "top",
+        itemMarginBottom: 5,
+        align: "left",
+        itemStyle: {
+          fontWeight: "normal",
+        },
       },
-  
+
       data: {
         //convert year in first column to UTC date to be used by Highstock
-        parsed: function(columns){
-            columns[0].forEach(function(v, i, a){
-                columns[0][i] = Date.UTC(columns[0][i]);
-            });
-        }
-      }, 
-      
+        parsed: function (columns) {
+          columns[0].forEach(function (v, i, a) {
+            columns[0][i] = Date.UTC(columns[0][i]);
+          });
+        },
+      },
+
       rangeSelector: {
-        buttons: 
-            [
-                {
-                  count: 10,
-                  type: 'year',
-                  text: '10 J'
-                }, 
-                {
-                  count: 20,
-                  type: 'year',
-                  text: '20 J'
-                }, 
-                {
-                  count: 50,
-                  type: 'year',
-                  text: '50 J'
-                }, 
-                {
-                  count: 100,
-                  type: 'year',
-                  text: '100 J'
-                },
-                {
-                  type: 'all',
-                  text: 'Alle J'
-                }
-            ],
-        buttonSpacing: 15,                
+        buttons: [
+          {
+            count: 10,
+            type: "year",
+            text: "10 J",
+          },
+          {
+            count: 20,
+            type: "year",
+            text: "20 J",
+          },
+          {
+            count: 50,
+            type: "year",
+            text: "50 J",
+          },
+          {
+            count: 100,
+            type: "year",
+            text: "100 J",
+          },
+          {
+            type: "all",
+            text: "Alle J",
+          },
+        ],
+        buttonSpacing: 15,
         inputEnabled: false,
-        selected: 4, 
-        inputDateFormat: '%Y',
-        inputEditDateFormat: '%Y'
-    }  
-    }
+        selected: 4,
+        inputDateFormat: "%Y",
+        inputEditDateFormat: "%Y",
+      },
+    };
   }());
   
   
