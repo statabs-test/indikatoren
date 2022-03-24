3eb07e16ae296f052071874559aea6d5461243c7
ksz91 <kevin.zaugg@bs.ch>
2022-03-24 15:19:47 +0100

    some changes

--- charts/templates/9274.js
+++ charts/templates/9274.js
@@ -1,91 +1,15 @@
 (function(){
     return {
   "xAxis": {
-    tickInterval: 1,
-    labels:{
-        step: 8
+      tickPositioner: function () {
+        var interval = 8,
+          ext = this.getExtremes(),
+          i = ext.dataMax,
+          pos = [i];
+        while (i >= ext.dataMin) pos.unshift(i = i - interval);
+        return pos;
+      }
     },
-     /* plotLines: [{
-        value: 1955,
-        width: 1,
-        color: '#CFD1D1'
-    },
-    {
-      value: 1956,
-      width: 1,
-      color: '#CFD1D1'
-  },
-  {
-    value: 1975,
-    width: 1,
-    color: '#CFD1D1'
-},
-{
-value: 1980,
-width: 1,
-color: '#CFD1D1'
-},{
-value: 1992,
-width: 1,
-color: '#CFD1D1'
-},{
-  value: 2011,
-  width: 1,
-  color: '#CFD1D1'
-  }
-]*/
-   /* plotBands: [{
-      color: '#CFD1D1',
-      from: 1955,
-      to: 1956
-    },
-    {
-      color: '#CFD1D1',
-      from: 1976,
-      to: 1980
-    },
-    {
-      color: '#CFD1D1',
-      from: 1992,
-      to: 2011
-    },]*/
-
-    /* plotBands: [{
-      color: 'rgba(200,200,200,0.1)',
-      from: 1924,
-      to: 1955
-    },
-    {
-      color: 'rgba(200,200,200,0.2)',
-      from: 1955,
-      to: 1956
-    },
-    {
-      color: 'rgba(200,200,200,0.1)',
-      from: 1956,
-      to: 1975
-    },
-    {
-      color: 'rgba(200,200,200,0.4)',
-      from: 1975,
-      to: 1980
-    },
-    {
-      color: 'rgba(200,200,200,0.3)',
-      from: 1980,
-      to: 1992
-    },
-    {
-      color: 'rgba(200,200,200,0.81)',
-      from: 1992,
-      to: 2011
-    },
-    {
-      color: 'rgba(200,200,200,0.2)',
-      from: 2011,
-      to: 2020
-    },]*/
-  },
   "yAxis": {
     "min": 0, 
     tickInterval: 800,
@@ -145,6 +69,9 @@ color: '#CFD1D1'
         "symbol": "circle",
       } 
     }
+  },
+  chart: {
+    marginRight: 20
   }
 }
 }());
