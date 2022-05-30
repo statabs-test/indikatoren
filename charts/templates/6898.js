b35e5014a6fb32ab36e9ae21308d7bb3a2055ccb
ksz91 <kevin.zaugg@bs.ch>
2022-05-30 14:04:16 +0200

    tooltip

--- charts/templates/6898.js
+++ charts/templates/6898.js
@@ -1,22 +1,20 @@
 (function(){
     return {
    "chart": {
-    "type": "area",
+    "type": "column",
    },
   plotOptions: {
-        /*series: {
-        	"stacking": "normal",
-			pointPadding: 0,
-            borderWidth: 0,
-            pointWidth: 10, 
+    column: {
+      "stacking": "normal",
+        pointPadding: 0,
+        groupPadding: 0.2,
+        borderWidth: 0,
+        pointWidth: 2, 
 
-        }*/
-         area: {
-            stacking: 'normal',
-            //lineColor: '#666666',
-            lineWidth: 0,
-
-        }
+    }, 
+    line: {
+      tooltip:{shared: false}
+    }
   },
   "yAxis": {
     "labels": {
@@ -32,21 +30,22 @@
     "layout": "horizontal",
     "verticalAlign": "top",
     "align": "left",
+    //itemWidth: 210,
     itemDistance: 0,
-    itemMarginBottom: 2,
+    //itemMarginBottom: 2,
     "itemStyle": {
     "fontWeight": "normal"
     }
   },
   "series": [
     {"color":"#73ba7c", "index": 3,legendIndex: 0,  "marker": {"enabled": false}, stacking: true}, //
-    {"color":"#FABD24", "index": 2,legendIndex: 1,  "marker": {"enabled": false}, stacking: true }, //
+    {"color":"#FABD24", "index": 2,legendIndex: 1,  "marker": {"enabled": false}, stacking: true}, //
     {"color":"#923F8D", "index": 1,legendIndex: 2, "marker": {"enabled": false}, stacking: true}, //
-    {"color":"#999999", "index": 0,legendIndex: 3, "marker": {"enabled": false}, stacking: false}, //
+    {"color":"#999999", "index": 0,legendIndex: 3, "marker": {"enabled": false}, type: "line", stacking: false, tooltip:{shared: false}}, //
     
   ],  
   "tooltip": {
-    //"shared": true
+    "shared": true
   },
 
 };
