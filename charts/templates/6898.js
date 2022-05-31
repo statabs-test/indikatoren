052039cde94e4f2b5247c29373ab763d05527628
bl99 <bjoern.lietzke@bs.ch>
2022-05-31 14:42:07 +0200

    6898.js

--- charts/templates/6898.js
+++ charts/templates/6898.js
@@ -9,13 +9,6 @@
           symbol: 'circle'
         }
       },
-      /*      column: {
-              stacking: "normal",
-              pointPadding: 0,
-              groupPadding: 0.2,
-              borderWidth: 0,
-              pointWidth: 2,
-            },*/
     },
     "yAxis": {
       "labels": {
@@ -38,42 +31,38 @@
       //itemMarginBottom: 2,
     },
     "series": [
-      { "color": "#73ba7c", "index": 3, legendIndex: 0, stacking: true }, //
-      { "color": "#FABD24", "index": 2, legendIndex: 1, stacking: true }, //
-      { "color": "#923F8D", "index": 1, legendIndex: 2, stacking: true }, //
-      { "color": "#999999", "index": 0, legendIndex: 3, stacking: false, type: "line", zIndex: 100 }, //
-
+      { "color": "#73ba7c", "index": 3, legendIndex: 0, stacking: true }, 
+      { "color": "#FABD24", "index": 2, legendIndex: 1, stacking: true }, 
+      { "color": "#923F8D", "index": 1, legendIndex: 2, stacking: true }, 
+      { "color": "#999999", "index": 0, legendIndex: 3, stacking: false, type: "line", zIndex: 100 }
     ],
     "tooltip": {
       formatter() {
-        if (this.series.userOptions.stacking == true) {
-          const chart = this.series.chart;
-          const series = chart.series;
+        if (this.series.userOptions.stacking == true) { //nur für series mit stacking: true
+          const series = this.series.chart.series;
           let tooltip = ""
           let s = 0
           series.forEach(series => {
-            if (series.userOptions.stacking == true) {
-              series.setState('hover');
+            if (series.userOptions.stacking == true) { //nur für series mit stacking: true
+              series.setState('hover'); //"aktiviere" alle series
               series.points.forEach(point => {
                 if (point.x === this.x) {
-                  tooltip += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}:</span> <b> ${point.y} </b><br>`;
+                  tooltip += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}:</span> <b> ${Highcharts.numberFormat(point.y, 0, ",", " ")} </b><br>`;
                   s += point.y;
                 }
               })
             }
           },
           )
-          return '<span style="font-size: 10px">Alter: ' + this.x + '</span><br>' + tooltip +
-            '<span style="opacity: 0">\u25CF</span> Total Veranlagungen: <b>' + s + '</b>';
-
+          return '<span style="font-size: 10px">Alter: ' + this.x +
+            '</span><br>' + tooltip + '<span style="opacity: 0">\u25CF</span> Total Veranlagungen: <b>' + Highcharts.numberFormat(s, 0, ",", " ") + '</b>';
         } else {
-          return '<span style="font-size: 10px">Alter: ' + this.x + '</span><br><span style="color:'
-            + this.color + '">●</span> ' + this.series.name + ': <b>'
+          return '<span style="font-size: 10px">Alter: ' + this.x +
+            '</span><br><span style="color:' + this.color + '">●</span> ' + this.series.name + ': <b>'
             + Highcharts.numberFormat(this.y, 0, ",", " ") + '</b><br/>';
         }
       }
-    },
-
+    }
   };
 }());
 
