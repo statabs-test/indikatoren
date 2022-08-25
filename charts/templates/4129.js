7ba4cba5bb1a8d99fb8215b7d6137981771717d8
monteirojaf <25841464+monteirojaf@users.noreply.github.com>
2022-08-25 11:38:19 +0200

    still strugling with the tooltip

--- charts/templates/4129.js
+++ charts/templates/4129.js
@@ -3,13 +3,13 @@
     title: {
       margin: 30
     },
-    "yAxis": {
-      "labels": {
-        "format": "{value:,.0f}",
+    yAxis: {
+      labels: {
+        format: "{value:,.0f}",
       }
     },
-    "xAxis": {
-      "type": "category"
+    xAxis: {
+      type: "category"
     },
     tooltip: {
       formatter: function (e) {
@@ -18,6 +18,7 @@
           chart = series.chart,
           correspondingSeries = series.linkedSeries[0] || series.linkedParent,
           linePoint,
+          symbolPoint,
           arearangePoint,
           correspondingPoint;
         //console.log(correspondingSeries);
@@ -35,10 +36,7 @@
             correspondingPoint = correspondingSeries.points[point.index];
             correspondingPoint.setState('hover');
             chart.extraHoveredPoint = correspondingPoint;
-          }
-
-
-          
+          }      
           // identify type of points for formatting purposes
           if (point.low !== undefined) {
             arearangePoint = point;
@@ -48,40 +46,13 @@
             linePoint = point;
           }
 
-          return "Line: " + linePoint.y + "<br>" + "Arearange: " + arearangePoint.low + " - " + arearangePoint.high;
-          //return this.series.index;
+          return this.series.name + ": " + this.point.y +  "<br>" + 
+          "Vertrauungsinterval: " + arearangePoint.low + " - " + arearangePoint.high;
         }
 
       }
     },
-    /*"tooltip": {
-      formatter() {
-        if (this.series.userOptions.id == "1zi") { //nur für series mit stacking: true
-          const series = this.series.chart.series;
-          let tooltip = ""
-          let s = 0
-          series.forEach(series => {
-            if (series.userOptions.id == "1zi") { //nur für series mit stacking: true
-              series.setState('hover'); //"aktiviere" alle series
-              series.points.forEach(point => {
-                if (point.x === this.x) {
-                  tooltip += `<span style="color:${point.color}">\u25CF</span> ${point.series.name}:</span> <b> ${Highcharts.numberFormat(point.y, 0, ",", " ")} </b><br>` + "Q1" + point.y.low;
-                  s += point.y;
-                }
-              })
-            }
-          },
-          )
-          return '<span style="font-size: 10px">Alter: ' + this.x +
-            '</span><br>' + tooltip + point.y + '<span style="opacity: 0">\u25CF</span> <br> Q1: <b>' + point.y.low + '</b>';
-        } else {
-          return '<span style="font-size: 10px">Alter: ' + this.x +
-            '</span><br><span style="color:' + this.color + '">●</span> ' + this.series.name + ': <b>'
-            + Highcharts.numberFormat(this.y, 0, ",", " ") + '</b><br/>';
-        }
-      }
-    },*/
-    "series": [
+    series: [
       {
         linkedTo: "1zi",
         type: "scatter",
