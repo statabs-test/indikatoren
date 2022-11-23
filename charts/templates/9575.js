5acbe831bdf6f199d9f0fbba04a418f26b27b158
bl99 <bjoern.lietzke@bs.ch>
2022-11-23 10:09:29 +0100

    test

--- charts/templates/9575.js
+++ charts/templates/9575.js
@@ -14,16 +14,16 @@
     },
     xAxis: {
       type: "category",
-      
+
       labels: {
         step: 1,
         rotation: -45,
         formatter: function () {
           return this.value
-          .replace('Innenstadt: ', '')
-          .replace('Stadt Basel: ', '')
-          .replace('Innenstadt', '<b>Innenstadt</b>')
-          .replace('Stadt Basel', '<b>Stadt Basel</b>');
+            .replace('Innenstadt: ', '')
+            .replace('Stadt Basel: ', '')
+            .replace('Innenstadt', '<b>_Innenstadt</b>')
+            .replace('Stadt Basel', '<b>_Stadt Basel</b>');
         }
       }
     },
@@ -67,22 +67,22 @@
       { "color": "#C4AB91", index: 0, legendIndex: 14 }, /*hellpink*/
     ],
     "tooltip": {
-     // "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:.1f}%)<br/>',
+      // "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:,.0f}</b> ({point.percentage:.1f}%)<br/>',
       //"footerFormat": 'Total: <b>{point.total:,.0f}</b>',
-       shared: true,
-    useHTML: true, 
-    followPointer: true,
-    formatter: function () {
+      shared: true,
+      useHTML: true,
+      followPointer: true,
+      formatter: function () {
         var s = '<span style="font-size: 10px">' + this.points[0].key + '</span><table>', sum = 0;
         $.each(this.points, function (i, point) {
-            s += '<tr><td><span style="color:' + point.color + '">\u25CF</span> ' + point.series.name + ':</td>'
-                + '<td style="text-align:right">&nbsp;<b>' + point.y + '</b></td>'
-                + '<td style="text-align:right">&nbsp;(' + Highcharts.numberFormat(point.percentage, 1) + '%)</td></tr>';
-            sum += point.y;
+          s += '<tr><td><span style="color:' + point.color + '">\u25CF</span> ' + point.series.name + ':</td>'
+            + '<td style="text-align:right">&nbsp;<b>' + point.y + '</b></td>'
+            + '<td style="text-align:right">&nbsp;(' + Highcharts.numberFormat(point.percentage, 1) + '%)</td></tr>';
+          sum += point.y;
         });
-        s += '<tr><td><span style="color:transparent">\u25CF</span> <b>&nbsp;Total: <b></td><td style="text-align:right">&nbsp;<b>' + Highcharts.numberFormat(sum,0) + '</b></td><td></td></table>'
+        s += '<tr><td><span style="color:transparent">\u25CF</span> <b>&nbsp;Total: <b></td><td style="text-align:right">&nbsp;<b>' + Highcharts.numberFormat(sum, 0) + '</b></td><td></td></table>'
         return s;
-    },
+      },
     },
   }
 }());
