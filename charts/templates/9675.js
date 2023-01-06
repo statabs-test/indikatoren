b674f4afd600ef512322009a094a3e32e4170595
bl99 <bjoern.lietzke@bs.ch>
2023-01-06 17:42:34 +0100

    div

--- charts/templates/9675.js
+++ charts/templates/9675.js
@@ -1,7 +1,55 @@
 (function () {
   return {
     "chart": {
-      "type": "column",
+      "type": "scatter",
+      events: {
+        load: function () {
+          this.credits.element.onclick = function () { };
+
+          //for top-left legends with no x defined: move legend to x position of first yAxis
+          if (this['legend']['options']['align'] == 'left' && this['legend']['options']['verticalAlign'] == 'top' && this['legend']['options']['x'] == 0) {
+            this.update(
+              {
+                legend: {
+                  x: this.yAxis[0].left - this.spacingBox.x - this.legend.padding
+                }
+              }
+            );
+          }
+
+          this.series[1].points[this.series[1].points.length - 1].update({
+            dataLabels: {
+              enabled: true,
+              y: -80,
+              x: -50,
+              format: 'Zielwert {key}: {y:,.1f}',
+              style: {
+                textOutline: false,
+              }
+            },
+            marker: {
+              enabled: true
+            }
+          });
+
+          const chart = this,
+            colors = ['#59fb59', '#fbf659', '#fb9999'],
+            data = chart.series[0].data,
+            assessed = chart.series[1].data;
+          data.forEach(function (element, i) {
+            if (assessed[i].y != null) {
+              element.update({
+                color: colors[assessed[i].y],
+                marker: {
+                  enabled: true,
+                  lineWidth: 1,
+                  lineColor: "#0091f7"
+                }
+              })
+            }
+          });
+        }
+      }
     },
     "plotOptions": {
       series: {
@@ -21,7 +69,7 @@
         dashStyle: 'ShortDash',
         color: "#999999",
         width: 1.5,
-        label:{
+        label: {
           text: "Zielwert: Rang 2",
           rotation: 0,
           y: 12,
@@ -43,60 +91,51 @@
       "verticalAlign": "top",
       "align": "left",
       "x": 35,
-      //"y": 35,    
-      "itemStyle": {
-        "fontWeight": "normal"
-      }
     },
     tooltip: {
       headerFormat: '<span style="font-size: 10px">{series.name}</span><br/>',
       pointFormat: '<span style="color:{point.color}">\u25CF</span> {point.name}: <b>{point.y:,.1f}</b><br/>',
       "shared": false
     },
-    series: [{
-      name: 'Kanton'
-    }],
-
-    /*"series": [
-             function() {
-             var col;
-          if(this.series.data[0]=="BS"){
-         col="#0066FF";
-          }else {
-          col="#0066EE";
-        }
-              }, 
-    {"color": "col", "visible": true}, 
-    ],*/
-
-    "colors": [
-      "#A8C3CA",
-      "#689199",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
-      "#A8C3CA",
+    series: [
+      {
+        name: 'Kanton',
+        color: "#999999",
+        zIndex: 2,
+      },
+      {
+        visible: false,
+        showInLegend: false
+      },
     ],
+  /*"colors": [
+    "#A8C3CA",
+    "#0091f7",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+    "#A8C3CA",
+  ],*/
 
-  }
+}
 }());
