af13658d968a742a92a7dfd8de3da8ed8fe50c96
bl99 <bjoern.lietzke@bs.ch>
2023-03-08 14:07:30 +0100

    6477 Integration

--- charts/templates/5160.js
+++ charts/templates/5160.js
@@ -20,29 +20,24 @@
       tickAmount: 6,
       title: ""
     },
-      /*{
-        "title": {
-          "style": {
-            "color": "#000000",
-            "fontSize": null
-          },
-          "text": null
-        },
-        "labels": {
-          "format": "{value:,.f}",
-          "style": {
-            "color": "#000000"
-          }
-        },
-        "min": -6000,
-        "max": 24000,
-        tickAmount: 6,
-        "gridLineWidth": 0,
-        "opposite": true
-      }*/
     ],
     xAxis: {
       tickInterval: 2,
+    },
+    legend: {
+      enabled: true,
+      //"x": 25,
+      //"y": 55,
+      //itemWidth: 170,
+      layout: "horizontal",
+      verticalAlign: "top",
+      align: "left",
+      itemMarginBottom: 5,
+      alignColumns: false,
+      itemDistance: 10,
+      labelFormatter: function () {
+        return this.name.replace(" bis 2009","");
+      },
     },
     tooltip: {
       pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}‰</b><br/>',
@@ -67,21 +62,6 @@
       }
     ],
 
-    legend: {
-      enabled: true,
-      //"x": 25,
-      //"y": 55,
-      //itemWidth: 170,
-      layout: "horizontal",
-      verticalAlign: "top",
-      itemMarginBottom: 5,
-      align: "left",
-      alignColumns: false,
-      itemDistance: 10,
-      itemStyle: {
-        fontWeight: "normal"
-      }
-    },
   }
 }());
 
