137c12063200bb0b4c5ebf0178a49918e2127751
Sanest <esteban.sanjuan@bs.ch>
2023-11-28 16:54:39 +0100

    Änderungen an den Tooltips der GSV-Indis

--- charts/templates/11172.js
+++ charts/templates/11172.js
@@ -37,7 +37,7 @@
         opposite: true, // Rechte Achse aktivieren
         min: 1,
         max: 12,
-//        tickAmount: 12,
+        //        tickAmount: 12,
         title: {
           text: "", // Beschriftung rechte Achse
         },
@@ -49,15 +49,25 @@
       startOnTick: false,
     },
     tooltip: {
-      pointFormatter: function () {
-        if (this.series.yAxis.userOptions.opposite) {
-          return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 1) + '</b><br/><b>';
-        } else {
-          return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 1) + '%' + '</b><br/><b>';
-        }
-      },
-      "shared": false
+      shared: true,
+      useHTML: true,
+      followPointer: true,
+      headerFormat: '<span style="font-size: 10px"> {point.x} </span> <table>',
+      pointFormat: '<tr><td><span style="color:{series.color}">\u25CF</span> {series.name}: &nbsp;</td>'
+        + '<td style="text-align:right">&nbsp;<b>{point.y:,.1f}%</b></td></tr>',
     },
+    /*
+      tooltip: {
+        pointFormatter: function () {
+          if (this.series.yAxis.userOptions.opposite) {
+            return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 1) + '</b><br/><b>';
+          } else {
+            return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 1) + '%' + '</b><br/><b>';
+          }
+        },
+        "shared": false
+      },
+  */
     legend: {
       itemWidth: 75,
       itemStyle: {
@@ -80,46 +90,74 @@
     },
     series: [
       {
-        color: '#68ab2b', index: 12, legendIndex: 1,
+        color: '#68ab2b', index: 12,
+        legendIndex: 1,
       },
       {
-        color: '#007a2f', index: 11, legendIndex: 2,
+        color: '#007a2f', index: 11,
+        legendIndex: 2,
       },
       {
-        color: '#ffda80', index: 10, legendIndex: 3,
+        color: '#ffda80', index: 10,
+        legendIndex: 3,
       },
       {
-        color: '#fabd24', index: 9, legendIndex: 4,
+        color: '#fabd24', index: 9,
+        legendIndex: 4,
       },
       {
-        color: '#cd9c00', index: 8, legendIndex: 5,
+        color: '#cd9c00', index: 8,
+        legendIndex: 5,
       },
       {
-        color: '#7f5f1a', index: 7, legendIndex: 6,
+        color: '#7f5f1a', index: 7,
+        legendIndex: 6,
       },
       {
-        color: '#45381d', index: 6, legendIndex: 7,
+        color: '#45381d', index: 6,
+        legendIndex: 7,
       },
       {
-        color: '#e7cee2', index: 5, legendIndex: 8,
+        color: '#e7cee2', index: 5,
+        legendIndex: 8,
       },
       {
-        color: '#b375ab', index: 4, legendIndex: 9,
+        color: '#b375ab', index: 4,
+        legendIndex: 9,
       },
       {
-        color: '#923f8d', index: 3, legendIndex: 10,
+        color: '#923f8d', index: 3,
+        legendIndex: 10,
       },
       {
-        color: '#662673', index: 2, legendIndex: 11,
+        color: '#662673', index: 2,
+        legendIndex: 11,
       },
       {
-        color: '#2e1435', index: 1, legendIndex: 12,
+        color: '#2e1435', index: 1,
+        legendIndex: 12,
       },
       {
         type: 'line',
         color: '#010101',
         yAxis: 1,  //Diagramm für durchschnittliche Pflegestufe auf rechte Achse
-        legendIndex: 13
+        legendIndex: 13,
+        tooltip: {
+          shared: true,
+          useHTML: true,
+          followPointer: true,
+          headerFormat: '<span style="font-size: 10px"> {point.x} </span> <table>',
+          pointFormat: '<tr><td><span style="color:{series.color}">\u25CF</span> {series.name}: &nbsp;</td>'
+            + '<td style="text-align:right">&nbsp;<b>{point.y:,.1f}&nbsp;&nbsp;&nbsp;</b></td></tr>',
+        },
+/*
+        tooltip: {
+          headerFormat: '<table>',
+          pointFormatter: function () {
+            return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat(this.y, 1) + '</b><br/><b> </table>';
+          }
+        }
+*/
       },
     ],
   };
--- charts/templates/11178.js
+++ charts/templates/11178.js
@@ -2,10 +2,8 @@
   return {
     chart: {
       type: "column",
-      //type: "area",
     },
     plotOptions: {
-      //area: {
       column: {
         stacking: 'normal',
         //lineWidth: 0,
@@ -16,18 +14,9 @@
       }
     },
     series: [
-      {
-        "color": "#fabd24",
-        legendIndex: 1,
-      },
-      {
-        "color": "#246370",
-        legendIndex: 2,
-      },
-      {
-        "color": "#68ab2b",
-        legendIndex: 3,
-      },
+      { "color": "#fabd24" }, // Kanton/Gemeinde: Restfinanzierung
+      { "color": "#246370" }, // Beiträge Krankenversicherer
+      { "color": "#68ab2b" }, // Eigenbetrag Klient
       /*
       {
         type: 'line',
@@ -39,18 +28,29 @@
       */
     ],
     xAxis: {
-      type: "category"
+      tickInterval: 1,
     },
     yAxis: {
       reversedStacks: false,
     },
     tooltip: {
       shared: true,
-      pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f} Mio. CHF</b> ({point.percentage:,.1f}%)<br/>',
-      footerFormat: 'Total: <b>{point.total:,.1f} Mio. CHF</b>',
+      useHTML: true,
+      followPointer: true,
+      headerFormat: '<span style="font-size: 10px"> {point.x} </span><table>',
+      pointFormat: '<tr><td><span style="color:{series.color}">\u25CF</span> {series.name}: &nbsp;</td>'
+        + '<td style="text-align:right">&nbsp;<b>{point.y:,.1f} Mio. CHF</b></td>'
+        + '<td style="text-align:right">&nbsp;({point.percentage:,.1f}%)</td></tr>',
+      footerFormat: '<tr><td>Total: </td><td><b>{point.total:,.1f} Mio. CHF</b></td><td></td></tr></table>',
     },
+    /*
+        tooltip: {
+          shared: true,
+          pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f} Mio. CHF</b> ({point.percentage:,.1f}%)<br/>',
+          footerFormat: 'Total: <b>{point.total:,.1f} Mio. CHF</b>',
+        },
+    */
     legend: {
-      //    itemWidth: 150,
       enabled: true,
       layout: "horizontal",
       verticalAlign: "top",
