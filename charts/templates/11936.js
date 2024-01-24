f721f8cc7e603d270ef8ea12288fa20ca1fa3a0f
fabienne213 <fabienne.hofer@bs.ch>
2024-01-24 16:27:49 +0100

    11936

--- charts/templates/11936.js
+++ charts/templates/11936.js
@@ -1,58 +1,58 @@
 (function(){
-    return {
-  plotOptions: {
-        series: {
-        	"stacking": "normal",
-             groupPadding: 0,
+  return {
+    "plotOptions": {
+        column: {
+            stacking: 'normal'
+        },
+      "series": {  
+          pointPadding: 0,
+          borderWidth: 0,
+          //pointWidth: 10, 
+        "dataLabels": {
+          "style": {
+            "fontSize": "10px"
+          }
         }
-  },
-  "yAxis": {
-    "labels": {
-      "format": "{value:,.0f}",
-    }
-  },
-  "xAxis": {
-  	"type": "category",
-    "tickInterval": 4,
-     labels:{
-         x: 15,
-     		"formatter": function() {
-   			 return this.value.split("-").slice(0, -1);
-		},
-     }  
-  },
- "legend": { 
-     //y: 45,
-    "enabled": true,
-    "layout": "horizontal",
-    "verticalAlign": "top",
-    "align": "left",
-    "itemStyle": {
-    "fontWeight": "normal"
-    }
-  },
-
-
- "series": [
-    {"color": "#008AC3", index: 1, legendIndex: 0}, /* */
-    {"color": "#8B2223", index: 0, legendIndex: 1}, /* */
-	/* {"color": "#689199", index: 3, legendIndex: 2},  /* */
-	/* {"color": "#DC440E", index: 2, legendIndex: 3},  /* */
-	 /* {"color": "#A8C3CA", index: 1, legendIndex: 4}, /* */
-	/*  {"color": "#FF8028", index: 0, legendIndex: 5}, /* */
-  ],
- tooltip: {
-  "pointFormatter": function(){
-      return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y),0) + ' </b><br/><b>' 
+      },
     },
-  },  
-  "chart": {
-  	//height: 600,
-  	type: "column",
-    "inverted": false,
-  }
+    "xAxis": {
+    },
+    "yAxis": {
+        reversedStacks: false, 
+        tickAmount: 7
+    },
+    "series": [
+      {
+          stack: 1,
+          color: 'rgb(176, 0, 0)'
+      },
+      {
+          stack: 1,
+          color: 'rgb(105, 146, 155)'
+      }
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+      itemWidth: 180, 
+      itemMarginBottom: 5,
+      //"y": 35,    
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    "chart": {      
+      "type": "column"
+    },
+tooltip: {
+         //headerFormat: '<span style="font-size: 10px"> {point.key}</span><br/>',
+         pointFormat: '<span style="color:{point.color}">●</span> {series.name}: <b>{point.y:,.0f}</b><br/>'    
+          }
 };
 }());
+
 
 
 
