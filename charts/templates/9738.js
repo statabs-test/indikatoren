53e86fffa945b79b2c0d6557dfd64c5a80f30e12
bl99 <bjoern.lietzke@bs.ch>
2024-03-06 11:17:59 +0100

    9738

--- charts/templates/9738.js
+++ charts/templates/9738.js
@@ -3,22 +3,32 @@
     chart: {
       height: 415,
     },
-    "series": [
-      { "color": "#71A3B5" },
-      { "color": "#FFBB58" },
-      { "color": "#C8C8C8" },
-      { "color": "#6F6F6F" }, // grau
-      // reversedStacks: true,       
-    ],
-    "xAxis": {
-      "type": "category"
+    title:{
+      useHTML: true
+    },
+    subtitle:{
+      useHTML: true
+    },
+    xAxis: {
+      type: "category"
     },
     yAxis: {
       reversedStacks: false,
     },
     legend: {
       reversed: false,
-      "itemMarginBottom": 10,
-    }
+      itemMarginBottom: 10,
+    },
+    tooltip: {
+      useHTML: true,
+      outside: true
+    },
+    series: [
+      { color: "#71A3B5" },
+      { color: "#FFBB58" },
+      { color: "#C8C8C8" },
+      { color: "#6F6F6F" }, // grau
+      // reversedStacks: true,       
+    ],
   }
 }());
