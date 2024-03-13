cfbb40b0d1bacce13c5954f932d4c5439fe1dfdf
bl99 <bjoern.lietzke@bs.ch>
2024-03-13 18:37:16 +0100

    build

--- charts/templates/4909.js
+++ charts/templates/4909.js
@@ -1,27 +1,31 @@
-(function(){
-    return {
-  "yAxis": {
-    "min": 0,
-    "labels": {
-      "format": "{value:,.0f}"
-	  }
-  },
-  "xAxis": {
-      "min": 1982,
-	    "tickInterval": 5
-  },
-  tooltip: {
-    valueDecimals: 1,
-  },
-  "series": [
-    {
-      "marker": {
-        "symbol": "circle",
-        "enabled": false
-      },
-      "index": 0,
-      "color": "#000000"
-    }
-  ],
-	};
+(function () {
+  return {
+    chart: {
+      spacingBottom: 40
+    },
+    title:{
+      margin: 30
+    },
+    "yAxis": {
+      "min": 0,
+      "labels": {
+        "format": "{value:,.0f}"
+      }
+    },
+    "xAxis": {
+      "tickInterval": 5
+    },
+    tooltip: {
+      valueDecimals: 1,
+    },
+    "series": [
+      {
+        "marker": {
+          "symbol": "circle",
+          "enabled": false
+        },
+        "color": "#000000"
+      }
+    ],
+  };
 }());
