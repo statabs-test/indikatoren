caee153e86713426508da35e5c21c1f5665254c3
bl99 <bjoern.lietzke@bs.ch>
2024-03-19 16:58:51 +0100

    6574

--- charts/templates/6574.js
+++ charts/templates/6574.js
@@ -6,11 +6,10 @@
       "spacingBottom": 40
     },
     plotOptions: {
-      series: {
-        //        pointPadding: 0,
-        //        itemWidth: 10,
+      column: {
+          stacking: 'normal'
       }
-    },
+  },
     "xAxis": {
       type: "category"
     },
@@ -21,12 +20,6 @@
       "min": 0,
       "max": 100,
     },
-    "series": [
-      { "color": "#672773" }, /* hellbraun*/
-      { "color": "#fabd24" }, /* dunkelbraun */
-      { "color": "#73ba7c" }, /* dunkelbraun */
-      { "color": "#b475ab" } /* dunkelbraun */
-    ],
     "legend": {
       "enabled": true,
       "layout": "horizontal",
@@ -34,8 +27,20 @@
       "align": "left",
     },
     tooltip: {
-      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>',
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>'+
+                     '<span style="color:transparent">\u25CF</span> Total: <b>{point.stackTotal:,.2f}%</b>',
       "shared": false
-    }
+    },
+    "series": [
+      { "color": "#672773", stack: "BS", index: 2, legendIndex: 1 }, /* hellbraun*/
+      { "color": "#d095db", stack: "BS", index: 1, legendIndex: 2 }, /* hellbraun*/
+      { "color": "#fabd24", stack: "Ba", index: 4, legendIndex: 3 }, /* dunkelbraun */
+      { "color": "#fde5a7", stack: "Ba", index: 3, legendIndex: 4 }, /* dunkelbraun */
+      { "color": "#73ba7c", stack: "Ri", index: 6, legendIndex: 5  }, /* dunkelbraun */
+      { "color": "#c7e3cb", stack: "Ri", index: 5, legendIndex: 6  }, /* dunkelbraun */
+      { "color": "#b475ab", stack: "Be", index: 8, legendIndex: 7  }, /* dunkelbraun */
+      { "color": "#e1c8dd", stack: "Be", index: 7, legendIndex: 8  } /* dunkelbraun */
+    ]
+  
   }
 }());
