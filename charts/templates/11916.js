2de297fb48c0bddb429bbbfcbec35246af308809
bl99 <bjoern.lietzke@bs.ch>
2024-03-26 14:43:30 +0100

    11916

--- charts/templates/11916.js
+++ charts/templates/11916.js
@@ -1,5 +1,8 @@
 (function () {
 	return {
+		subtitle:{
+			useHTML: true,
+		},
 		"xAxis": {
 			type: "category", /* ausprobieren, ob "category" oder nicht - hat Auswirkung auf den Abstand der letzten Jahreszahl zu rechten Rand */
 			tickPositioner: function () {
@@ -11,6 +14,10 @@
 				while (i >= ext.min) pos.unshift(i = i - interval);
 				return pos;
 			}
+		},
+		"tooltip": {
+			useHTML: true,
+			"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} Mio m<sup>3</sup></b><br/>'
 		},
 		"series": [
 			{ "color": "#662673" },
