2253dd35ba6b6302c16a9543386cc800a83cf769
bl99 <bjoern.lietzke@bs.ch>
2024-04-12 14:31:45 +0200

    12554

--- charts/templates/12554.js
+++ charts/templates/12554.js
@@ -1,7 +1,20 @@
 (function () {
   return {
+    chart: {
+marginRight: 10,
+    },
     "yAxis": {
       tickInterval: 25000
+    },
+    xAxis: {
+      tickPositioner: function () {
+        var interval = 2,
+          ext = this.getExtremes(),
+          i = ext.dataMax,
+          pos = [i];
+        while (i >= ext.dataMin) pos.unshift(i = i - interval);
+        return pos;
+      }
     },
     "series": [
       {
