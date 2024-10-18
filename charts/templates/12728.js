68bb814f4be09738bef2c0a866578e78062f31ef
Sanest <esteban.sanjuan@bs.ch>
2024-10-18 14:11:20 +0200

    GSV-Indis

---
+++ charts/templates/12728.js
@@ -0,0 +1,70 @@
+(function(){
+  return {
+"yAxis": {
+  tickAmount: 10,
+  "labels": {
+     "formatter": function(){
+      return Highcharts.numberFormat((this.value),0)+'';
+    } 
+  },
+  
+},
+"xAxis": {
+  "tickInterval": 1
+},
+"legend": {
+  itemDistance: 8,
+  "enabled": true,
+  "layout": "horizontal",
+  "verticalAlign": "top",
+  "itemMarginBottom": 5,
+  "align": "left"
+},
+
+"series": [
+  {
+    index: 0,
+    color: "#68ab2b"
+  },
+  {
+    index: 1,
+    color: "#006400"
+  },
+  {
+    index: 1,
+    color: "#010101"
+  },
+  {
+    index: 1,
+    color: "#010101"
+  },
+  {
+    "index": 1,
+    color: "#010101"
+  },
+  {
+    "index": 1,
+    color: "#010101"
+  }
+],
+"tooltip": {
+ "pointFormatter": function(){
+    return '<span style="color:' + this.series.color + '">\u25CF</span> ' + this.series.name + ': <b>' + Highcharts.numberFormat((this.y),0) + '</b><br/><b>'
+  },
+  "shared": false
+},  
+"chart": {
+},  
+"plotOptions": {
+  "series": {
+    "marker": {
+      "enabled": false,
+      "symbol": "circle"
+    },
+    "dataLabels": {
+      "enabled": false
+    }
+  }
+}
+}
+}());
