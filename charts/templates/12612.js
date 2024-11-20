91ee0191168aa18048d2bcc5df5057620af54a3b
bl99 <bjoern.lietzke@bs.ch>
2024-11-20 14:16:53 +0100

    12612

--- charts/templates/12612.js
+++ charts/templates/12612.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
   return {
     "xAxis": {
       type: "category", /* ausprobieren, ob "category" oder nicht - hat Auswirkung auf den Abstand der letzten Jahreszahl zu rechten Rand */
@@ -6,67 +6,70 @@
         var maxlabels = 10,
           ext = this.getExtremes(),
           i = Math.round(ext.max),
-          interval = Math.round((i - ext.min)/maxlabels),
+          interval = Math.round((i - ext.min) / maxlabels),
           pos = [i];
         while (i >= ext.min) pos.unshift(i = i - interval);
         return pos;
       }
     },
-"yAxis": {
-"labels": {
-  "format": "{value:,.0f}"
-}
-},	
-"tooltip": {
-  "shared": false, 
-"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .1f}</b><br/>'
-},
-"series": [
-  { 
-    "color": "#9E7C59" /*"#cd9c00" Pestalozzi*/ 
-  },
-  {
-    "color": "#FF8028" /* "#689199" Kannenfeld*/
-  },
-  {
-    "color": "#923F8D" /* "#662673" Landskron*/
-  },
-  {
-    "color": "#689199" /* "#999" Lysbüchel*/
-  },
-  {
-    "color": "#FABD24" /* "#b375ab" Friedmatt*/,
-    visible: true,
-  },
-],
-"legend": {
-  "enabled": true,
-  "layout": "horizontal",
-  "verticalAlign": "top",
-  "itemMarginBottom": 5,
-  "align": "left",
-  alignColumns: false,
- // x: 35,
-},
-"plotOptions": {
-  "line": {
-  "connectNulls": true,
-    "marker":{
-      "enabled": false,
-      "symbol": "circle",
-    }
-  }
-},
-chart: {
-  events: {
-      load: function() {
+    "yAxis": {
+      "labels": {
+        "format": "{value:,.0f}"
+      }
+    },
+    "tooltip": {
+      "shared": false,
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y: .1f}</b><br/>'
+    },
+    "series": [
+      {
+        "color": "#9E7C59", legendIndex: 2 /*"#cd9c00" Pestalozzi*/
+      },
+      {
+        "color": "#FF8028", legendIndex: 3 /* "#689199" Kannenfeld*/
+      },
+      {
+        "color": "#923F8D", legendIndex: 4 /* "#662673" Landskron*/
+      },
+      {
+        "color": "#689199", legendIndex: 5 /* "#999" Lysbüchel*/
+      },
+      {
+        "color": "#FABD24", legendIndex: 6 /* "#b375ab" Friedmatt*/,
+        visible: true,
+      },
+      {
+        "color": "#8A8A8A", legendIndex: 1, dashStyle: 'shortDash' /*"#8A8A8A" Stadt Basel*/,
+      },
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "itemMarginBottom": 5,
+      "align": "left",
+      alignColumns: true,
+      // x: 35,
+    },
+    "plotOptions": {
+      "line": {
+        "connectNulls": true,
+        "marker": {
+          "enabled": false,
+          "symbol": "circle",
+        }
+      }
+    },
+    chart: {
+      events: {
+        load: function () {
           // `this` refers to the chart instance
           this.series[4].update({
-              name: "Friedmatt"
+            name: "Friedmatt"
           });
 
           this.credits.element.onclick = function () { };
- 
+
           //for top-left legends with no x defined: move legend to x position of first yAxis
           if (this['legend']['options']['align'] == 'left' && this['legend']['options']['verticalAlign'] == 'top' && this['legend']['options']['x'] == 0) {
             this.update(
@@ -79,6 +82,6 @@ chart: {
           }
         }
       },
-      }
-}
+    }
+  }
 }());
