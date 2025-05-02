e07395d0293aaee3383c469ee9e4d7c9be990c3d
jvelv <joelle.velvart@bs.ch>
2025-05-02 18:02:53 +0200

    Aufsetzen der Indikatoren

---
+++ charts/templates/22297.js
@@ -0,0 +1,100 @@
+(function(){
+  return {
+plotOptions: {
+      series: {
+        "stacking": "normal",
+          pointPadding: 0,
+          groupPadding: 0.5, // exaclty overlaps columns
+          borderWidth: 0
+      }
+},
+"yAxis": {
+  "min": -200,
+  tickInterval: 250,
+  "labels": {
+    "format": "{value:,.0f}",
+  },
+  plotLines: [{
+    color: '#B9CFD7',
+    value: 0,
+    width: 2
+}]
+},
+"xAxis": {
+ "tickInterval": 1,
+},
+"legend": {
+  "enabled": true,
+  "layout": "horizontal",
+  "verticalAlign": "top",
+  "align": "left",
+//"y": 40,
+//"x": 40,
+"itemMarginBottom": 5,
+  "itemStyle": {
+  "fontWeight": "normal"
+  }
+},
+"series": [
+  {
+    "color": "#b475ab",
+    "index": 0,
+    "type": "column",
+    "pointWidth": "15",
+    legendIndex: 0
+  },
+  {
+    "color": "#E7CEE2",
+    "index": 0,
+    "type": "column",
+    "pointWidth": "15",
+    legendIndex: 1
+  },
+  {
+    "color": "#a8c4cb",
+    "index": 0,
+    "type": "column",
+    "pointWidth": "15",
+    legendIndex: 2
+  },
+  {
+    "color": "#fabd24",
+    "index": 0,
+    "type": "column",
+    "pointWidth": "15",
+    legendIndex: 3
+  },
+{
+    "color": "#000000",
+    "index": 1,
+    "type": "line",
+    "marker": {
+      "enabled": false
+    }, 
+
+    //"yAxis": 1,
+    legendIndex: 4,
+    tooltip: {
+      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+  }
+  },
+  {
+    "color": "#67401E",
+    "index": 1,
+    "type": "line",
+    "marker": {
+      "enabled": false
+    }, 
+
+    //"yAxis": 1,
+    legendIndex: 5,
+    tooltip: {
+      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y}</b><br/>'
+  }
+  },
+],  
+"tooltip": {
+  "shared": true
+},
+};
+}());
--- metadata/all/all.md
+++ metadata/all/all.md
@@ -2470,3 +2470,19 @@ id | visible | visibleInPortal | indikatorenset
 12737 | true | true | Gesundheitsversorgung
 21847 | true | true | 
 22143 | true | false | Legislaturplan (Print)
+22288 | true | true | Wohnraumschutz
+22289 | true | true | Wohnraumschutz
+22290 | true | true | Wohnraumschutz
+22291 | true | true | Wohnraumschutz
+22292 | true | true | Wohnraumschutz
+22293 | true | true | Wohnraumschutz
+22294 | true | true | Wohnraumschutz
+22295 | true | true | Wohnraumschutz
+22296 | true | true | Wohnraumschutz
+22297 | true | true | Wohnraumschutz
+22298 | true | true | Wohnraumschutz
+22299 | true | false | Wohnraumschutz
+22300 | true | true | Wohnraumschutz
+22301 | true | true | Wohnraumschutz
+22302 | true | true | Wohnraumschutz
+22303 | true | true | Wohnraumschutz
