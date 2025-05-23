418a766f45e99c3c2b54ccd5dc1afc5c904bac18
jvelv <joelle.velvart@bs.ch>
2025-05-23 16:52:16 +0200

    json 22293 nochmals mit build

--- metadata/all/indikatoren.js
+++ metadata/all/indikatoren.js
@@ -7041,7 +7041,7 @@ var indikatoren = [
 		"filter": "",
 		"parentId": null,
 		"children": [],
-		"title": "Sanierungsvolumen in Franken",
+		"title": "Sanierungsvolumen in Mio. Franken",
 		"subtitle": "Projektgesamtkosten nach Baueingabe",
 		"lesehilfe": "Die gemäss eingereichtem Umbauprojekt angegebenen Projektgesamtkosten sind insbesondere ab 2021 deutlich angestiegen, ehe sie ab der zweiten Hälfte 2022 wieder deutlich zurückgingen. Die Projektgesamtkosten nicht von der geänderter Gesetzgebung betroffener Umbauprojekte blieben in diesem Zeitraum stabil.",
 		"erlaeuterungen": "Bewilligungspflichte Umbauprojekte (Baubegehren sowie vereinfachte und technische Baubegehren) mit Schlagwort \"Umbau/Sanierung/Renovation\" in Zusammenhang mit Wohngebäuden nach Datum der Baueingabe, ohne Baunebenkosten. Unter \"betroffenen Projekten\" versteht man Projekte, die potentiell unter den Wohnraumschutz fallen können (oder hätten fallen können), also Projekte mit mehr als 3 Wohnungen pro Gebäude mit mehrheitlicher Wohnnutzung. Dabei ausgeschlossen sind Projekte von Genossenschaften.",
