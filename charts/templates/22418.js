f193fab5d47248910d0d695f787f769859f021ce
bl99 <bjoern.lietzke@bs.ch>
2025-09-24 18:35:39 +0200

    22418

--- charts/templates/22418.js
+++ charts/templates/22418.js
@@ -22,23 +22,43 @@
     "yAxis": {
       "min": 0,
       "labels": {
-        formatter: function () {
-          //const total = this.series.data.reduce((sum, point) => sum + point.y, 0); // Calculate the total sum of all data points
-          //const percentage = ((this.y / total) * 100);
-          return Highcharts.numberFormat(100/21008925*this.value, 2) + '%'; 
-        }
+        format: '{value}%',
       }
     },
     "tooltip": {
       "shared": false,
-      //"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f} ha</b> ({point.percentage:.1f}%)<br/>',
-      formatter: function () {
+      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f}%</b><br/>',
+      /*formatter: function () {
         const total = this.series.data.reduce((sum, point) => sum + point.y, 0); // Calculate the total sum of all data points
         const percentage = ((this.y / total) * 100);
-        return `${this.key}<br><span style="color:${this.point.color}">\u25CF</span> ${this.series.name}: <b>`+ Highcharts.numberFormat(percentage,2) +`%</b> (` + Highcharts.numberFormat(this.y/1000000,2) + ` km²)`;
-      }
+        return `${this.key}<br><span style="color:${this.point.color}">\u25CF</span> ${this.series.name}: <b>` + Highcharts.numberFormat(percentage, 2) + `%</b> (` + Highcharts.numberFormat(this.y / 1000000, 2) + ` km²)`;
+      }*/
     },
     "series": [
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
       {
         "color": "#68ab2b",
         visible: false,
@@ -47,14 +67,14 @@
       },
       {
         "color": "#67401E",
-        visible: false,
-        legendIndex: 2,
-        index: 2
+        legendIndex: 1,
+        index: 1
       }, 
       {
         "color": "#933F8D",
-        	legendIndex: 1,
-          index: 1
+        visible: false,
+        legendIndex: 2,
+        index: 2
       }, 
       {
         "color": "#008AC3",
@@ -67,7 +87,7 @@
         visible: false,
         legendIndex: 5,
         index: 5
-      }, 
+      },
     ],
     "legend": {
       "enabled": true,
--- charts/templates/22419.js
+++ charts/templates/22419.js
@@ -22,21 +22,43 @@
     "yAxis": {
       "min": 0,
       "labels": {
-        formatter: function () {
-          return Highcharts.numberFormat(this.value / 1000000, 0) + ' km²';
-        }
+        format: '{value}%',
       }
     },
     "tooltip": {
       "shared": false,
-      //"pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f} ha</b> ({point.percentage:.1f}%)<br/>',
-      formatter: function () {
+      "pointFormat": '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y:.2f}%</b><br/>',
+      /*formatter: function () {
         const total = this.series.data.reduce((sum, point) => sum + point.y, 0); // Calculate the total sum of all data points
         const percentage = ((this.y / total) * 100);
-        return `${this.key}<br><span style="color:${this.point.color}">\u25CF</span> ${this.series.name}: <b>` + Highcharts.numberFormat(percentage, 1) + `%</b> (` + Highcharts.numberFormat(this.y / 1000000, 2) + ` km²)`;
-      }
+        return `${this.key}<br><span style="color:${this.point.color}">\u25CF</span> ${this.series.name}: <b>` + Highcharts.numberFormat(percentage, 2) + `%</b> (` + Highcharts.numberFormat(this.y / 1000000, 2) + ` km²)`;
+      }*/
     },
     "series": [
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
+      {
+        visible: false,
+        showInLegend: false,
+      },
       {
         "color": "#68ab2b",
         visible: false,
@@ -45,21 +67,21 @@
       },
       {
         "color": "#67401E",
-        legendIndex: 1,
-        index: 1
-      }, 
-      {
-        "color": "#933F8D",
         visible: false,
         legendIndex: 2,
         index: 2
-      }, 
+      },
+      {
+        "color": "#933F8D",
+        legendIndex: 1,
+        index: 1
+      },
       {
         "color": "#008AC3",
         visible: false,
         legendIndex: 4,
         index: 4
-      }, 
+      },
       {
         "color": "#474747",
         visible: false,
