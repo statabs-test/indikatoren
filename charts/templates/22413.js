e7f791487c8d64977db6d97313edc6f1edc351e6
bl99 <bjoern.lietzke@bs.ch>
2025-10-14 16:46:10 +0200

    22413

--- charts/templates/22413.js
+++ charts/templates/22413.js
@@ -1,3 +1,41 @@
 (function () {
-  return {}
-}());
+  return {
+    plotOptions: {
+      series: {
+        pointPadding: 0,
+        borderWidth: 0,
+        //itemWidth: 10,
+      }
+    },
+    "xAxis": {
+      "type": "category",
+      "labels": {
+        "rotation": 0
+      }
+    },
+    "yAxis": {
+      "labels": {
+        "format": "{value:,.0f}"
+      }
+    },
+    "series": [
+      { "color": "#ffbb58" } 
+    ],
+    "legend": {
+      "enabled": false,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "align": "left",
+    },
+    /*tooltip: {
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>',
+      "shared": false
+    },*/
+    "chart": {
+      "type": "column",
+      "inverted": true,
+      marginRight: 30,
+      //"spacingTop": 100
+    }
+  }
+}()); 
