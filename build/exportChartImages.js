c0439f8c87e204fea49d4872e5257dd36fb0cd31
Marcel Colomb <marcel.colomb@bs.ch>
2025-11-14 13:06:00 +0100

    create images not adding quelle and title

--- build/exportChartImages.js
+++ build/exportChartImages.js
@@ -1,7 +1,7 @@
-const exporter = require('highcharts-export-server');
+const exporter = require("highcharts-export-server");
 //var execfile = require("execfile");
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 /*
 //Hack to re-use existing web js code from within node.js, see http://stackoverflow.com/a/8808162
@@ -29,111 +29,155 @@ rimraf('images/indikatorenset/*', function(error) {
 
 go();
 
-
-function go(){
-    var views = ['portal'];
-    views.forEach(function(view){
-        console.log('Creating array entries for indikatorensetView=' + view);
-        var files = JSON.parse(fs.readFileSync('tmp/chartsToBuild.json'));
-        files.forEach(function(chartId) {
-            var pathArray = createPathArray(chartId, view);
-            //only add if not null
-            if (pathArray) {chartDetails.push(pathArray)}
-        });
+function go() {
+  var views = ["portal"];
+  views.forEach(function (view) {
+    console.log("Creating array entries for indikatorensetView=" + view);
+    var files = JSON.parse(fs.readFileSync("tmp/chartsToBuild.json"));
+    files.forEach(function (chartId) {
+      var pathArray = createPathArray(chartId, view);
+      //only add if not null
+      if (pathArray) {
+        chartDetails.push(pathArray);
+      }
     });
-    
-    //exporter.logLevel(4);
-    exporter.initPool(
-        {
-            maxWorkers: 1,
-            initialWorkers: 1,
-            workLimit: 10
-        });
-        
-    createSvgImages(chartDetails);
+  });
+
+  //exporter.logLevel(4);
+  exporter.initPool({
+    maxWorkers: 1,
+    initialWorkers: 1,
+    workLimit: 10,
+  });
+
+  createSvgImages(chartDetails);
 }
 
+function createPathArray(chartId, view) {
+  var imagePath = "images/" + view + "/";
+  var configPath = "charts/configs/" + view + "/";
+  var singlePath = "metadata/single/";
+  var infilePath = path.join(__dirname, "../" + configPath + chartId + ".json");
+  var additionalConfigPath = path.join(
+    __dirname,
+    "../" + singlePath + chartId + ".json"
+  );
+  var outfilePath = path.join(__dirname, "../" + imagePath + chartId + ".svg");
 
-function createPathArray(chartId, view){
-    var imagePath = 'images/' + view + '/';
-    var configPath = 'charts/configs/' + view + '/';
-    var singlePath = 'metadata/single/';
-    var infilePath = path.join(__dirname, '../' + configPath + chartId + '.json');
-    var additionalConfigPath = path.join(__dirname, '../' + singlePath + chartId + '.json');
-    var outfilePath = path.join(__dirname, '../' + imagePath + chartId + '.svg');
-    
-    try{
-        var additionalConfigFile = fs.readFileSync(additionalConfigPath, 'utf8');
-        var additionalConfig = deserialize(additionalConfigFile);
-        if(!additionalConfig.kennzahlenset.toLowerCase().includes('print'))
-        {
-            var configFile = fs.readFileSync(infilePath, 'utf8');
-            var config = deserialize(configFile);
-            
-            //decide if stockchart, map, or chart
-            var constr = config.isStock ? 'StockChart': (config.chart.type === 'map' ? 'Map' : 'Chart');
-            return {
-                config: config,
-                additionalConfig: additionalConfig, 
-                infilePath: infilePath, 
-                outfilePath: outfilePath,
-                constr: constr
-            };
-        }
-        else
-        {
-            console.log('File for Chart-Id ' + chartId + ' not created (is Print-Indikator) ');
-            return null;
-        }
+  try {
+    var additionalConfigFile = fs.readFileSync(additionalConfigPath, "utf8");
+    var additionalConfig = deserialize(additionalConfigFile);
+    if (!additionalConfig.kennzahlenset.toLowerCase().includes("print")) {
+      var configFile = fs.readFileSync(infilePath, "utf8");
+      var config = deserialize(configFile);
+
+      //decide if stockchart, map, or chart
+      var constr = config.isStock
+        ? "StockChart"
+        : config.chart.type === "map"
+        ? "Map"
+        : "Chart";
+      return {
+        config: config,
+        additionalConfig: additionalConfig,
+        infilePath: infilePath,
+        outfilePath: outfilePath,
+        constr: constr,
+      };
+    } else {
+      console.log(
+        "File for Chart-Id " + chartId + " not created (is Print-Indikator) "
+      );
+      return null;
     }
-    catch(err){
-        console.log('Exception when reading json file ' + infilePath + ': ' + err);
-    }
+  } catch (err) {
+    console.log("Exception when reading json file " + infilePath + ": " + err);
+  }
 }
 
-
-//alternatively use looping style used here: https://github.com/highcharts/node-export-server/issues/41 
-function createSvgImages(chartDetails){
-    if (chartDetails.length > 0){
-        var chartEntry = chartDetails.pop();   
-        //console.log('Current infile: ' + chart.infile);
-        if (!chartEntry.additionalConfig.kennzahlenset.toLowerCase().includes('print')){
-            var exportSettings = {
-                type: 'svg',
-                infile: chartEntry.infilePath,
-                constr: chartEntry.constr,
-                outfile: chartEntry.outfilePath,
-                allowCodeExecution: 1,
-                //define empty mappie here to satisfy export server, same as in options001.js
-                customCode: "function(){Highcharts.seriesType('mappie', 'pie', {}, {});}()", 
-                //add proj4 and jQuery to export server's dependencies
-                resources: {
-                    files: "node_modules/proj4/dist/proj4.js,node_modules/jquery/dist/jquery.min.js,assets/js/customFunctions.js"
+//alternatively use looping style used here: https://github.com/highcharts/node-export-server/issues/41
+function createSvgImages(chartDetails) {
+  if (chartDetails.length > 0) {
+    var chartEntry = chartDetails.pop();
+    //console.log('Current infile: ' + chart.infile);
+    if (
+      !chartEntry.additionalConfig.kennzahlenset.toLowerCase().includes("print")
+    ) {
+      var exportSettings = {
+        type: "svg",
+        infile: chartEntry.infilePath,
+        constr: chartEntry.constr,
+        outfile: chartEntry.outfilePath,
+        allowCodeExecution: 1,
+        //define empty mappie here to satisfy export server, same as in options001.js
+        customCode: `
+        function () {
+            // Fix für mappie Charts
+            Highcharts.seriesType('mappie', 'pie', {}, {});
+    
+            // Credits entfernen
+            Highcharts.addEvent(Highcharts.Chart, 'load', function () {
+                if (this.credits && this.credits.element) {
+                    this.credits.element.remove();
                 }
-            };
-    
-            exporter.export(exportSettings, function (err, res) {
-                if (err) {throw err}
-                //The export result is now in res.
-                //If the output is not PDF or SVG, it will be base64 encoded (res.data).
-                //If the output is a PDF or SVG, it will contain a filename (res.filename).
-                console.log('File created: ' + res.filename + ', ' + chartDetails.length + ' to go...');
-                createSvgImages(chartDetails);
             });
+    
+            // Control-Points entfernen
+            Highcharts.addEvent(Highcharts.Chart, 'load', function () {
+                const cp = this.container.querySelector('.highcharts-control-points');
+                if (cp) cp.remove();
+            });
+    
+            // Titel & Untertitel entfernen
+            Highcharts.addEvent(Highcharts.Chart, 'load', function () {
+                const title = this.container.querySelector('.highcharts-title');
+                if (title) title.remove();
+    
+                const subtitle = this.container.querySelector('.highcharts-subtitle');
+                if (subtitle) subtitle.remove();
+            });
+        }()
+        `, //add proj4 and jQuery to export server's dependencies
+        resources: {
+          files:
+            "node_modules/proj4/dist/proj4.js,node_modules/jquery/dist/jquery.min.js,assets/js/customFunctions.js",
+        },
+      };
+
+      exporter.export(exportSettings, function (err, res) {
+        if (err) {
+          throw err;
         }
-        else{
-            console.log('File not created (is Print-Indikator): ' + chartEntry.outfilePath + ', ' + chartDetails.length + ' to go...');
-            createSvgImages(chartDetails);
-        }
+        //The export result is now in res.
+        //If the output is not PDF or SVG, it will be base64 encoded (res.data).
+        //If the output is a PDF or SVG, it will contain a filename (res.filename).
+        console.log(
+          "File created: " +
+            res.filename +
+            ", " +
+            chartDetails.length +
+            " to go..."
+        );
+        createSvgImages(chartDetails);
+      });
+    } else {
+      console.log(
+        "File not created (is Print-Indikator): " +
+          chartEntry.outfilePath +
+          ", " +
+          chartDetails.length +
+          " to go..."
+      );
+      createSvgImages(chartDetails);
     }
-    else {
-        console.log('...done!');
-        exporter.killPool();
-        process.exit();
-    }
+  } else {
+    console.log("...done!");
+    exporter.killPool();
+    process.exit();
+  }
 }
 
 //from https://github.com/yahoo/serialize-javascript
-function deserialize(serializedJavascript){
-  return eval('(' + serializedJavascript + ')');
+function deserialize(serializedJavascript) {
+  return eval("(" + serializedJavascript + ")");
 }
