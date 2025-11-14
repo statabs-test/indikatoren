81b12ec0968106a8bd70965f6e568c60f212ff88
Sanest <esteban.sanjuan@bs.ch>
2025-11-14 11:44:09 +0100

    12729

---
+++ charts/templates/12729.js
@@ -0,0 +1,44 @@
+(function () {
+  return {
+    "xAxis": {
+      "tickInterval": 1
+    },
+    "yAxis": {
+      "min": undefined,
+      "labels": {
+        "format": "{value:,.1f}"
+      }
+    },
+    "tooltip": {
+      "shared": false,
+      "pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y:,.1f}</b><br/>'
+    },
+    "series": [
+      { "color": "#FFBB58" },
+      { "color": "#FF8028" },
+      { "color": "#B00000" },
+      { "color": "#B375AB" },
+      { "color": "#662673" }
+    ],
+    "legend": {
+      "enabled": true,
+      "layout": "horizontal",
+      "verticalAlign": "top",
+      "itemMarginBottom": 5,
+      "align": "left",
+      "itemStyle": {
+        "fontWeight": "normal"
+      }
+    },
+    "plotOptions": {
+      "line": {
+        "connectNulls": true,
+        "marker": {
+          "enabled": false,
+          "symbol": "circle",
+          "radius": 3
+        }
+      }
+    }
+  };
+}());
