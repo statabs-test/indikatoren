e9e665b7f7fb16d41d44598d64ae066c8a17e8ac
bl99 <bjoern.lietzke@bs.ch>
2026-09-07 17:26:33 +0200

    21847

--- charts/templates/21847.js
+++ charts/templates/21847.js
@@ -26,6 +26,9 @@
         {
           "x": 0
         },
+        {
+          "x": 0
+        },
       ]
     },
     plotOptions: {
@@ -35,43 +38,88 @@
         borderWidth: 0,
         groupPadding: 0.1,
         minPointLength: 0.5
-      }
+      },
+      line: {
+        index: 1,
+        yAxis: 1,
+        threshold: 0,
+      },
+      column: {
+        pointPadding: 0,
+        borderWidth: 0,
+        index: 0,
+        yAxis: 0,
+        threshold: 0,
+      },
     },
+
     tooltip: {
       shared: true,
       useHTML: true,
       followPointer: true,
       formatter: function () {
-        var s = '<span style="font-size: 10px">' + this.points[0].key + '</span><table>', sum = 0, ki='';
+        var s = '<span style="font-size: 10px">' + this.points[0].key + '</span><table>', sum = 0, ki = '';
         $.each(this.points, function (i, point) {
           if (this.series.userOptions.type != 'errorbar') { //nur für series mit stacking: true
             s += '<tr><td><span style="color:' + point.color + '">\u25CF</span> ' + point.series.name + ': </td>'
-              + '<td style="text-align:right">&nbsp;<b>' + Highcharts.numberFormat(point.y,1) + '</b></td>'
+              + '<td style="text-align:right">&nbsp;<b>' + Highcharts.numberFormat(point.y, 1) + '</b></td>'
               + '<td style="text-align:right">&nbsp;(' + Highcharts.numberFormat(point.percentage, 1) + '%)</td></tr>';
             sum += point.y;
-          }else{
+          } else {
             ki = '<tr><td><span style="color:transparent">\u25CF</span> 95%-Konfidenzintervall:&nbsp;</td>'
-            + '<td style="text-align:left" colspan=2>&nbsp;<b>' + Highcharts.numberFormat(point.point.low, 1) + '</b> - <b>' + Highcharts.numberFormat(point.point.high,1) + '</b></td>'
-            + '</tr>';
+              + '<td style="text-align:left" colspan=2>&nbsp;<b>' + Highcharts.numberFormat(point.point.low, 1) + '</b> - <b>' + Highcharts.numberFormat(point.point.high, 1) + '</b></td>'
+              + '</tr>';
           }
         });
         s += '<tr><td style="padding-top: 7px;"><span style="color:transparent">\u25CF</span> Total: </td>'
-        + '<td style="text-align:right; padding-top: 7px;">&nbsp;<b>' + Highcharts.numberFormat(sum, 1) + '</b></td>'
-        + '<td></td></tr>'+ki+'</table>';
+          + '<td style="text-align:right; padding-top: 7px;">&nbsp;<b>' + Highcharts.numberFormat(sum, 1) + '</b></td>'
+          + '<td></td></tr>' + ki + '</table>';
         return s;
       },
     },
     "xAxis": {
       "type": "category"
     },
-    "yAxis": {
-      reversedStacks: false,
-      /*"labels": {
-        "formatter": function () {
-          return Highcharts.numberFormat((this.value), 0) + '%';
+     yAxis: [
+      {
+        reversedStacks: false,
+        title: {
+          text: null,
         },
-      }*/
-    },
+        labels: {
+          format: "{value:,.0f}",
+          style: {
+            color: "black",
+          },
+        },
+        plotLines: [
+          {
+            color: "#B9CFD7",
+            value: 0,
+            width: 2,
+          },
+        ],
+      },
+      {
+        opposite: true,
+        title: {
+          text: null,
+        },
+        labels: {
+          format: "{value:,.0f}",
+          style: {
+            color: "black",
+          },
+        },
+        plotLines: [
+          {
+            color: "#B9CFD7",
+            value: 0,
+            width: 2,
+          },
+        ],
+      },
+    ],
     "legend": {
       itemDistance: 15,
       "enabled": true,
@@ -81,6 +129,15 @@
       alignColumns: false,
     },
     "series": [
+      {
+        "index": 0,
+        color: "#5555ff",
+        type: "line",
+        marker: {
+          enabled: false,
+        },
+        index:99
+      },
       {
         "index": 0,
         color: "#facd5a",
