5eb7e124c5001c0952c88c0c2b5c3b01927f5d66
Sanest <esteban.sanjuan@bs.ch>
2026-09-10 12:23:08 +0200

    Anpassung 8995: Zwei neue Spalten, Legende bearbeitet

--- charts/templates/8995.js
+++ charts/templates/8995.js
@@ -193,33 +193,34 @@ Highcharts.dateFormats = {
             enabled: true,
             layout: "horizontal",
             verticalAlign: "top",
-            align: "right",
-            x: -38,
-            y: -35,
+            align: "left",
+            //x: -38,
+            //y: -35,
             alignColumns: true,
-            width: 240,
-            itemWidth: 110,
+            width: 400,
+            //itemWidth: 110,
             itemStyle: {
                 textOverflow: undefined,
                 whiteSpace: 'nowrap',
             },
+/*
             labelFormatter: function () {
                 return this.name
                     //.replace('65+ Jahre', '')
                     //.replace('0 - 64 Jahre', '')
                     .replace('Gemeldete Todesfälle ', '')
-                    //.replace('Erwartete Todesfälle ', '')
+                    .replace('Erwartete Todesfälle ', '')
                     ;
                 //.replace('Hochgerechnete Todesfälle', 'hochgerechnete');
             },
-
+/*
             title: {
                 text: 'Gemeldete Todesfälle: ',
                 style: {
                     fontWeight: 'normal'
                 }
             },
-            
+*/            
 
             /*
             x: -38,
